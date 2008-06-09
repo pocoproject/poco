9f751f5e9a006ceb4b61f8acfea8b9a3bc28b592
Guenter Obiltschnig <guenter.obiltschnig@appinf.com>
2008-06-09 18:48:09 +0000

    added release build scripts

---
+++ dist/build_vs71.cmd
@@ -0,0 +1,50 @@
+@echo off
+
+rem
+rem build.cmd
+rem
+rem $Id: //poco/1.3/dist/build_vs71.cmd#1 $
+rem
+rem command-line build script for VS 7.1
+rem
+
+cd CppUnit
+devenv /useenv /rebuild debug_shared CppUnit_vs71.sln
+devenv /useenv /rebuild release_shared CppUnit_vs71.sln
+cd ..
+
+cd Foundation
+devenv /useenv /rebuild debug_shared Foundation_vs71.sln
+devenv /useenv /rebuild release_shared Foundation_vs71.sln
+cd samples
+devenv /useenv /rebuild debug_shared samples_vs71.sln
+devenv /useenv /rebuild release_shared samples_vs71.sln
+cd ..
+cd ..
+
+cd XML
+devenv /useenv /rebuild debug_shared XML_vs71.sln
+devenv /useenv /rebuild release_shared XML_vs71.sln
+cd samples
+devenv /useenv /rebuild debug_shared samples_vs71.sln
+devenv /useenv /rebuild release_shared samples_vs71.sln
+cd ..
+cd ..
+
+cd Util
+devenv /useenv /rebuild debug_shared Util_vs71.sln
+devenv /useenv /rebuild release_shared Util_vs71.sln
+cd samples
+devenv /useenv /rebuild debug_shared samples_vs71.sln
+devenv /useenv /rebuild release_shared samples_vs71.sln
+cd ..
+cd ..
+
+cd Net
+devenv /useenv /rebuild debug_shared Net_vs71.sln
+devenv /useenv /rebuild release_shared Net_vs71.sln
+cd samples
+devenv /useenv /rebuild debug_shared samples_vs71.sln
+devenv /useenv /rebuild release_shared samples_vs71.sln
+cd ..
+cd ..
