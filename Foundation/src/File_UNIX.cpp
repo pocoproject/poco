c186d5da981855bd10b0fc9d125dee060522e42e
Sergey Kholodilov <serghol@gmail.com>
2008-08-05 15:08:39 +0000

    Misprint in createdImpl fixed

--- Foundation/src/File_UNIX.cpp
+++ Foundation/src/File_UNIX.cpp
@@ -205,7 +205,7 @@ Timestamp FileImpl::createdImpl() const
 
 	struct stat st;
 	if (stat(_path.c_str(), &st) == 0)
-		return Timestamp::fromEpochTime(st.st_mtime);
+		return Timestamp::fromEpochTime(st.st_birthtime);
 	else
 		handleLastErrorImpl(_path);
 	return 0;
