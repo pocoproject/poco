56c8ac698ee84840e569ac54dff81a2e85b77246
Marian Krivos <nezmar@tutok.sk>
2009-06-22 18:41:44 +0000

    bugfix: #2809750 (iodbc on OSX)

--- contrib/cmake/FindODBC.cmake
+++ contrib/cmake/FindODBC.cmake
@@ -1,37 +1,35 @@
 # check for location of odbc_config
-find_program(ODBC_CONFIG odbc_config 
+find_program(UNIX_ODBC_CONFIG odbc_config 
 	$ENV{ODBC_PATH}/bin 
 	/usr/bin 
 	/usr/local/bin 
 	PATHS)
 
-if(${ODBC_CONFIG} MATCHES "ODBC_CONFIG-NOTFOUND")
+if(${UNIX_ODBC_CONFIG} MATCHES "UNIX_ODBC_CONFIG-NOTFOUND")
 	message(STATUS "Couldn't find unixODBC")
-else(${ODBC_CONFIG} MATCHES "ODBC_CONFIG-NOTFOUND")
-	message(STATUS "Found unixODBC: odbc_config in ${ODBC_CONFIG}")
-	exec_program(${ODBC_CONFIG} ARGS "--include-prefix" OUTPUT_VARIABLE ODBC_INCLUDE_DIR)
+else(${UNIX_ODBC_CONFIG} MATCHES "UNIX_ODBC_CONFIG-NOTFOUND")
+	message(STATUS "Found unixODBC: odbc_config in ${UNIX_ODBC_CONFIG}")
+	exec_program(${UNIX_ODBC_CONFIG} ARGS "--include-prefix" OUTPUT_VARIABLE ODBC_INCLUDE_DIR)
 	include_directories(${ODBC_INCLUDE_DIR})
-	exec_program(${ODBC_CONFIG} ARGS "--libs" OUTPUT_VARIABLE ODBC_LINK_FLAGS)
+	exec_program(${UNIX_ODBC_CONFIG} ARGS "--libs" OUTPUT_VARIABLE ODBC_LINK_FLAGS)
 	add_definitions(-DPOCO_UNIXODBC)
-endif(${ODBC_CONFIG} MATCHES "ODBC_CONFIG-NOTFOUND")
+endif(${UNIX_ODBC_CONFIG} MATCHES "UNIX_ODBC_CONFIG-NOTFOUND")
 
-if(${ODBC_CONFIG} MATCHES "ODBC_CONFIG-NOTFOUND")
-	find_program(ODBC_CONFIG iodbc-config 
-		$ENV{ODBC_PATH}/bin 
-		/usr/bin 
-		/usr/local/bin 
-		PATHS)
-	if(${ODBC_CONFIG} MATCHES "ODBC_CONFIG-NOTFOUND")
-		message(STATUS "Couldn't find iODBC")
-	else(${ODBC_CONFIG} MATCHES "ODBC_CONFIG-NOTFOUND")
-		message(STATUS "Found iODBC: iodbc-config in ${ODBC_CONFIG}")
-		exec_program(${ODBC_CONFIG} ARGS "--cflags" OUTPUT_VARIABLE ODBC_CFLAGS)
-		add_definitions( ${ODBC_CFLAGS} )
-		exec_program(${ODBC_CONFIG} ARGS "--libs" OUTPUT_VARIABLE ODBC_LINK_FLAGS)
-		add_definitions(-DPOCO_IODBC)
-	endif(${ODBC_CONFIG} MATCHES "ODBC_CONFIG-NOTFOUND")
-endif(${ODBC_CONFIG} MATCHES "ODBC_CONFIG-NOTFOUND")
+find_program(IODBC_CONFIG iodbc-config 
+	$ENV{ODBC_PATH}/bin 
+	/usr/bin 
+	/usr/local/bin 
+	PATHS)
 
-if(${ODBC_CONFIG} MATCHES "ODBC_CONFIG-NOTFOUND")
-	#try odbc32.lib on windows
-endif(${ODBC_CONFIG} MATCHES "ODBC_CONFIG-NOTFOUND")
+if(${IODBC_CONFIG} MATCHES "IODBC_CONFIG-NOTFOUND")
+	message(STATUS "Couldn't find iODBC")
+else(${IODBC_CONFIG} MATCHES "IODBC_CONFIG-NOTFOUND")
+	message(STATUS "Found iODBC: iodbc-config in ${IODBC_CONFIG}")
+	exec_program(${IODBC_CONFIG} ARGS "--cflags" OUTPUT_VARIABLE ODBC_CFLAGS)
+	add_definitions( ${ODBC_CFLAGS} )
+	exec_program(${IODBC_CONFIG} ARGS "--libs" OUTPUT_VARIABLE ODBC_LINK_FLAGS)
+	add_definitions(-DPOCO_IODBC)
+endif(${IODBC_CONFIG} MATCHES "IODBC_CONFIG-NOTFOUND")
+
+message(STATUS "UNIX ODBC: ${UNIX_ODBC_CONFIG}")
+message(STATUS "IODBC: ${IODBC_CONFIG}")
