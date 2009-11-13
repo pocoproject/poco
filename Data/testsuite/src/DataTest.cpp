4856f84b21cbc7c4ac400b0ac9be46732c83b243
Aleksandar Fabijanic <alex@pocoproject.org>
2009-11-13 13:47:57 +0000

    Sun CC wants explicit cast &^(

--- Data/testsuite/src/DataTest.cpp
+++ Data/testsuite/src/DataTest.cpp
@@ -323,12 +323,12 @@ void DataTest::testCLOB()
 
 	assert (blobChrStr != blobNumStr);
 	Var vLOB = blobNumStr;
-	blobChrStr = vLOB;
+	blobChrStr = (CLOB) vLOB;
 	assert (blobChrStr == blobNumStr);
 
 	std::string xyz = "xyz";
 	vLOB = xyz;
-	blobChrStr = vLOB;
+	blobChrStr = (CLOB) vLOB;
 	assert (0 == std::strncmp(xyz.c_str(), blobChrStr.rawContent(), blobChrStr.size()));
 }
 
