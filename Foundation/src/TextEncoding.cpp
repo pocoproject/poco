3b38af0942c82a55bbed20ae22f19ac76c0680d5
Marian Krivos <nezmar@tutok.sk>
2011-05-08 12:40:36 +0000

    added support for Latin2, Windows1250 and Windows1251

--- Foundation/src/TextEncoding.cpp
+++ Foundation/src/TextEncoding.cpp
@@ -39,9 +39,12 @@
 #include "Poco/String.h"
 #include "Poco/ASCIIEncoding.h"
 #include "Poco/Latin1Encoding.h"
+#include "Poco/Latin2Encoding.h"
 #include "Poco/Latin9Encoding.h"
 #include "Poco/UTF16Encoding.h"
 #include "Poco/UTF8Encoding.h"
+#include "Poco/Windows1250Encoding.h"
+#include "Poco/Windows1251Encoding.h"
 #include "Poco/Windows1252Encoding.h"
 #include "Poco/RWLock.h"
 #include "Poco/SingletonHolder.h"
@@ -66,9 +69,12 @@ public:
 
 		add(new ASCIIEncoding);
 		add(new Latin1Encoding);
+		add(new Latin2Encoding);
 		add(new Latin9Encoding);
 		add(pUtf8Encoding);
 		add(new UTF16Encoding);
+		add(new Windows1250Encoding);
+		add(new Windows1251Encoding);
 		add(new Windows1252Encoding);
 	}
 	
