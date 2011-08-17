b496c4cbce0df6f3e8b9137d9fd3b53e7c1e84c6
Marian Krivos <nezmar@tutok.sk>
2011-08-17 06:56:00 +0000

    SF# 3379935

--- Foundation/src/ThreadPool.cpp
+++ Foundation/src/ThreadPool.cpp
@@ -464,8 +464,8 @@ PooledThread* ThreadPool::getThread()
 		if (_threads.size() < _maxCapacity)
 		{
 			pThread = createThread();
-			_threads.push_back(pThread);
 			pThread->start();
+			_threads.push_back(pThread);
 		}
 		else throw NoThreadAvailableException();
 	}
