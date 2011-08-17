499802cf6a7ab8afd603c155e50413b4b5370e6b
Marian Krivos <nezmar@tutok.sk>
2011-08-17 07:16:54 +0000

    SF# 3379935 - fixed memory leak

--- Foundation/src/ThreadPool.cpp
+++ Foundation/src/ThreadPool.cpp
@@ -463,9 +463,17 @@ PooledThread* ThreadPool::getThread()
 	{
 		if (_threads.size() < _maxCapacity)
 		{
-			pThread = createThread();
-			pThread->start();
-			_threads.push_back(pThread);
+            pThread = createThread();
+            try
+            {
+                pThread->start();
+                _threads.push_back(pThread);
+            }
+            catch(SystemException& e)
+            {
+                delete pThread;
+                throw;
+            }
 		}
 		else throw NoThreadAvailableException();
 	}
