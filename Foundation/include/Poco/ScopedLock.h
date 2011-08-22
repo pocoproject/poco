f215fb195a58449e3f016998fa64654eaa260d79
Marian Krivos <nezmar@tutok.sk>
2011-08-22 17:52:08 +0000

    trunk/branch integration: ScopedLockWithUnlock

--- Foundation/include/Poco/ScopedLock.h
+++ Foundation/include/Poco/ScopedLock.h
@@ -72,6 +72,44 @@ private:
 };
 
 
+template <class M>
+class ScopedLockWithUnlock
+	/// A class that simplifies thread synchronization
+	/// with a mutex.
+	/// The constructor accepts a Mutex and locks it.
+	/// The destructor unlocks the mutex.
+	/// The unlock() member function allows for manual
+	/// unlocking of the mutex.
+{
+public:
+	ScopedLockWithUnlock(M& mutex): _pMutex(&mutex)
+	{
+		_pMutex->lock();
+	}
+	
+	~ScopedLockWithUnlock()
+	{
+		unlock();
+	}
+	
+	void unlock()
+	{
+		if (_pMutex)
+		{
+			_pMutex->unlock();
+			_pMutex = 0;
+		}
+	}
+
+private:
+	M* _pMutex;
+
+	ScopedLockWithUnlock();
+	ScopedLockWithUnlock(const ScopedLockWithUnlock&);
+	ScopedLockWithUnlock& operator = (const ScopedLockWithUnlock&);
+};
+
+
 } // namespace Poco
 
 
