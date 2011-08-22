76216a6d553a08509f22133850c4e84e3422a887
Marian Krivos <nezmar@tutok.sk>
2011-08-22 16:57:05 +0000

    trunk/branch integration: VxWorks & Wince

--- Foundation/src/DirectoryIterator_WIN32U.cpp
+++ Foundation/src/DirectoryIterator_WIN32U.cpp
@@ -35,7 +35,11 @@
 
 
 #include "Poco/DirectoryIterator_WIN32U.h"
+#if defined(_WIN32_WCE)
+#include "Poco/File_WINCE.h"
+#else
 #include "Poco/File_WIN32U.h"
+#endif
 #include "Poco/Path.h"
 #include "Poco/UnicodeConverter.h"
 #include <cstring>
