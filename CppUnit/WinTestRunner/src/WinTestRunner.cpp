c13df6f828c5fadd8b5c4d4c98b53fec3b3019cc
Marian Krivos <nezmar@tutok.sk>
2011-09-14 13:03:06 +0000

    cppunit: fix WinTestRunner.cpp

--- CppUnit/WinTestRunner/src/WinTestRunner.cpp
+++ CppUnit/WinTestRunner/src/WinTestRunner.cpp
@@ -1,7 +1,7 @@
 //
 // WinTestRunner.cpp
 //
-// $Id: //poco/Main/CppUnit/WinTestRunner/src/WinTestRunner.cpp#6 $
+// $Id: //poco/1.4/CppUnit/WinTestRunner/src/WinTestRunner.cpp#1 $
 //
 
 
@@ -28,47 +28,47 @@ WinTestRunner::~WinTestRunner()
 
 void WinTestRunner::run()
 {
-        // Note: The following code is some evil hack to
-        // add batch capability to the MFC based WinTestRunner.
-        
-        std::string cmdLine(AfxGetApp()->m_lpCmdLine);
-        if (cmdLine.size() >= 2 && cmdLine[0] == '/' && (cmdLine[1] == 'b' || cmdLine[1] == 'B'))
-        {
-                // We're running in batch mode.
-                std::string outPath;
-                if (cmdLine.size() > 4 && cmdLine[2] == ':')
-                        outPath = cmdLine.substr(3);
-                else
-                        outPath = "CON";
-                std::ofstream ostr(outPath.c_str());
-                if (ostr.good())
-                {
-                        TestRunner runner(ostr);
-                        for (std::vector<Test*>::iterator it = _tests.begin(); it != _tests.end(); ++it)
-                                runner.addTest((*it)->toString(), *it);
-                        _tests.clear();
-                        std::vector<std::string> args;
-                        args.push_back("WinTestRunner");
-                        args.push_back("-all");
-                        bool success = runner.run(args);
-                        ExitProcess(success ? 0 : 1);
-                }
-                else ExitProcess(2);
-        }
-        else
-        {
-                // We're running in interactive mode.
-                TestRunnerDlg dlg;
-                dlg.setTests(_tests);
-                dlg.DoModal();
-        }
-                dlg.setTests(_tests);
-                dlg.DoModal();
-        }
+	// Note: The following code is some evil hack to
+	// add batch capability to the MFC based WinTestRunner.
+	
+	std::string cmdLine(AfxGetApp()->m_lpCmdLine);
+	if (cmdLine.size() >= 2 && cmdLine[0] == '/' && (cmdLine[1] == 'b' || cmdLine[1] == 'B'))
+	{
+		// We're running in batch mode.
+		std::string outPath;
+		if (cmdLine.size() > 4 && cmdLine[2] == ':')
+			outPath = cmdLine.substr(3);
+		else
+			outPath = "CON";
+		std::ofstream ostr(outPath.c_str());
+		if (ostr.good())
+		{
+			TestRunner runner(ostr);
+			for (std::vector<Test*>::iterator it = _tests.begin(); it != _tests.end(); ++it)
+				runner.addTest((*it)->toString(), *it);
+			_tests.clear();
+			std::vector<std::string> args;
+			args.push_back("WinTestRunner");
+			args.push_back("-all");
+			bool success = runner.run(args);
+			ExitProcess(success ? 0 : 1);
+		}
+		else ExitProcess(2);
+	}
+	else
+	{
+		// We're running in interactive mode.
+		TestRunnerDlg dlg;
+		dlg.setTests(_tests);
+		dlg.DoModal();
+	}
 }
 
 
 void WinTestRunner::addTest(Test* pTest)
+{
+	_tests.push_back(pTest);
+}
 
 
 BEGIN_MESSAGE_MAP(WinTestRunnerApp, CWinApp)
