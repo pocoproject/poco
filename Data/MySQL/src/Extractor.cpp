69d57a724ae1c40a09e4d53fce118b66746d6f69
Franky Braem <franky.braem@gmail.com>
2012-02-22 22:26:21 +0000

    Get the length of the BLOB by retrieving the column first with a zero length buffer. Now we know the length and we can fetch it with the correct buffer size.

--- Data/MySQL/src/Extractor.cpp
+++ Data/MySQL/src/Extractor.cpp
@@ -33,7 +33,6 @@
 // DEALINGS IN THE SOFTWARE.
 //
 
-
 #include "Poco/Data/MySQL/Extractor.h"
 
 
@@ -151,8 +150,21 @@ bool Extractor::extract(std::size_t pos, Poco::Data::BLOB& val)
 	if (_metadata.metaColumn(static_cast<Poco::UInt32>(pos)).type() != Poco::Data::MetaColumn::FDT_BLOB)
 		throw MySQLException("Extractor: not a blob");
 
+  unsigned long length= 0;
+  MYSQL_BIND bind = { 0 };
+  bind.buffer= 0;
+  bind.buffer_length= 0;
+  bind.length= &length;
+  if ( ! _stmt.fetchColumn(pos, &bind) )
+    return false;
 
-	val.assignRaw(_metadata.rawData(pos), _metadata.length(pos));
+  std::vector<char> data(length);
+  bind.buffer = &data[0];
+  bind.buffer_length= length;
+  if ( ! _stmt.fetchColumn(pos, &bind) )
+    return false;
+
+  val.assignRaw(&data[0], length);
 	return true;
 }
 
