a4931bb9af0ba5bf7a171805b4d90ccff74920c6
Marian Krivos <nezmar@tutok.sk>
2012-04-28 09:32:25 +0000

    added DigestEngine::digestFromHex(const std::string& digest) method

--- Foundation/src/DigestEngine.cpp
+++ Foundation/src/DigestEngine.cpp
@@ -35,6 +35,7 @@
 
 
 #include "Poco/DigestEngine.h"
+#include "Poco/Exception.h"
 
 
 namespace Poco {
@@ -60,8 +61,43 @@ std::string DigestEngine::digestToHex(const Digest& bytes)
 		unsigned char c = *it;
 		result += digits[(c >> 4) & 0xF];
 		result += digits[c & 0xF];
-	}
-	return result;
+        }
+        return result;
+}
+
+
+DigestEngine::Digest DigestEngine::digestFromHex(const std::string& digest)
+{
+        if (digest.size() % 2 != 0)
+                throw DataFormatException();
+        Digest result;
+        result.reserve(digest.size()/2);
+        for (std::size_t i = 0; i < digest.size(); ++i)
+        {
+                int c = 0;
+                // first upper 4 bits
+                if (digest[i] >= '0' && digest[i] <= '9')
+                        c = digest[i] - '0';
+                else if (digest[i] >= 'a' && digest[i] <= 'f')
+                        c = digest[i] - 'a'+10;
+                else if (digest[i] >= 'A' && digest[i] <= 'F')
+                        c = digest[i] - 'A'+10;
+                else
+                        throw DataFormatException();
+                c <<= 4;
+                ++i;
+                if (digest[i] >= '0' && digest[i] <= '9')
+                        c += digest[i] - '0';
+                else if (digest[i] >= 'a' && digest[i] <= 'f')
+                        c += digest[i] - 'a'+10;
+                else if (digest[i] >= 'A' && digest[i] <= 'F')
+                        c += digest[i] - 'A'+10;
+                else
+                        throw DataFormatException();
+
+                result.push_back(static_cast<unsigned char>(c));
+        }
+        return result;
 }
 
 
