03b7dcd3e13ed1c96d6b8f890c379a73d7cd61d6
Franky Braem <franky.braem@gmail.com>
2012-09-06 17:37:22 +0000

    Some comments were pointing to Util mode instead of JSON

--- JSON/include/Poco/JSON/JSON.h
+++ JSON/include/Poco/JSON/JSON.h
@@ -7,8 +7,8 @@
 // Package: JSON
 // Module:  JSON
 //
-// Basic definitions for the Poco Util library.
-// This file must be the first file included by every other Util
+// Basic definitions for the Poco JSON library.
+// This file must be the first file included by every other JSON
 // header file.
 //
 // Copyright (c) 2012, Applied Informatics Software Engineering GmbH.
@@ -47,10 +47,10 @@
 
 //
 // The following block is the standard way of creating macros which make exporting
-// from a DLL simpler. All files within this DLL are compiled with the Util_EXPORTS
+// from a DLL simpler. All files within this DLL are compiled with the JSON_EXPORTS
 // symbol defined on the command line. this symbol should not be defined on any project
 // that uses this DLL. This way any other project whose source files include this file see
-// Util_API functions as being imported from a DLL, wheras this DLL sees symbols
+// JSON_API functions as being imported from a DLL, wheras this DLL sees symbols
 // defined with this macro as being exported.
 //
 #if defined(_WIN32) && defined(POCO_DLL)
