7f026267241df323ab13cc653ed35f33d2e0bcd0
Aleksandar Fabijanic <alex@pocoproject.org>
2012-09-25 07:28:38 +0000

    FreeBSD fix for DirectoryWatcher

--- Foundation/src/DirectoryWatcher.cpp
+++ Foundation/src/DirectoryWatcher.cpp
@@ -53,7 +53,7 @@
 	#include <sys/types.h>
 	#include <sys/event.h>
 	#include <sys/time.h>
-	#if POCO_OS == POCO_OS_FREE_BSD
+	#if (POCO_OS == POCO_OS_FREE_BSD) && !defined(O_EVTONLY)
 		#define O_EVTONLY 0x8000
 	#endif
 #endif
