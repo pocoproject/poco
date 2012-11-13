a367294de3e2b3446bf9a72664d97caf44d88cfc
Mike Naquin <mike.naquin@ncr.com>
2012-11-13 15:31:08 -0600

    Add JSONConfiguration to Poco::Util::Application::loadConfiguration.

--- Foundation/include/Poco/Config.h
+++ Foundation/include/Poco/Config.h
@@ -120,6 +120,11 @@
 // #define POCO_UTIL_NO_INIFILECONFIGURATION
 
 
+// No support for JSON configuration in 
+// Poco::Util::Application.
+// #define POCO_UTIL_NO_JSONCONFIGURATION
+
+
 // No support for XML configuration in 
 // Poco::Util::Application. Avoids linking of XML
 // library and saves a few 100 Kbytes.
