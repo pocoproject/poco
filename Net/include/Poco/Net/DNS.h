22b658a83852920429e269ffa4d9df23aa58871c
Aleksandar Fabijanic <alex@pocoproject.org>
2013-03-24 14:36:28 -0500

    GH #68: DNS::hostByAddress can be slow
    GH #68: DNS::hostByAddress can be slow - added parameters in the
    interface to pass hint flags to hostByAddress() and hostByName() calls

--- Net/include/Poco/Net/DNS.h
+++ Net/include/Poco/Net/DNS.h
@@ -57,9 +57,31 @@ class Net_API DNS
 	/// An internal DNS cache is used to speed up name lookups.
 {
 public:
-	static HostEntry hostByName(const std::string& hostname);
+
+	enum HintFlag
+	{
+		DNS_HINT_NONE = 0,
+#ifdef POCO_HAVE_ADDRINFO
+		DNS_HINT_AI_PASSIVE = AI_PASSIVE,  // Socket address will be used in bind() call
+		DNS_HINT_AI_CANONNAME = AI_CANONNAME, // Return canonical name in first ai_canonname
+		DNS_HINT_AI_NUMERICHOST = AI_NUMERICHOST, // Nodename must be a numeric address string
+		DNS_HINT_AI_NUMERICSERV = AI_NUMERICSERV, // Servicename must be a numeric port number
+		DNS_HINT_AI_ALL = AI_ALL, // Query both IP6 and IP4 with AI_V4MAPPED
+		DNS_HINT_AI_ADDRCONFIG = AI_ADDRCONFIG, // Resolution only if global address configured
+		DNS_HINT_AI_V4MAPPED = AI_V4MAPPED, // On v6 failure, query v4 and convert to V4MAPPED format	
+#endif
+	};
+
+	static HostEntry hostByName(const std::string& hostname, unsigned hintFlags =
+#ifdef POCO_HAVE_ADDRINFO
+		DNS_HINT_AI_CANONNAME | DNS_HINT_AI_ADDRCONFIG
+#else
+		DNS_HINT_NONE
+#endif		
+		);
 		/// Returns a HostEntry object containing the DNS information
-		/// for the host with the given name.
+		/// for the host with the given name. HintFlag argument is only
+		/// used on platforms that have getaddrinfo().
 		///
 		/// Throws a HostNotFoundException if a host with the given
 		/// name cannot be found.
@@ -71,9 +93,16 @@ public:
 		///
 		/// Throws an IOException in case of any other error.
 		
-	static HostEntry hostByAddress(const IPAddress& address);
+	static HostEntry hostByAddress(const IPAddress& address, unsigned hintFlags =
+#ifdef POCO_HAVE_ADDRINFO
+		DNS_HINT_AI_CANONNAME | DNS_HINT_AI_ADDRCONFIG
+#else
+		DNS_HINT_NONE
+#endif		
+		);
 		/// Returns a HostEntry object containing the DNS information
-		/// for the host with the given IP address.
+		/// for the host with the given IP address. HintFlag argument is only
+		/// used on platforms that have getaddrinfo().
 		///
 		/// Throws a HostNotFoundException if a host with the given
 		/// name cannot be found.
