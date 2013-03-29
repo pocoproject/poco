ad5361ede1d9baf5581f0ad1b229de12ef02cb99
Aleksandar Fabijanic <alex@pocoproject.org>
2013-03-28 23:49:52 -0500

    Util alignment with JSON changes

--- Util/src/JSONConfiguration.cpp
+++ Util/src/JSONConfiguration.cpp
@@ -45,7 +45,6 @@
 #include "Poco/StringTokenizer.h"
 #include "Poco/JSON/Parser.h"
 #include "Poco/JSON/Query.h"
-#include "Poco/JSON/DefaultHandler.h"
 #include "Poco/RegularExpression.h"
 #include "Poco/NumberParser.h"
 
@@ -91,10 +90,8 @@ void JSONConfiguration::load(const std::string& path)
 void JSONConfiguration::load(std::istream& istr)
 {
 	JSON::Parser parser;
-	JSON::DefaultHandler handler;
-	parser.setHandler(&handler);
 	parser.parse(istr);
-	DynamicAny result = handler.result();
+	DynamicAny result = parser.result();
 	if ( result.type() == typeid(JSON::Object::Ptr) )
 	{
 		_object = result.extract<JSON::Object::Ptr>();
