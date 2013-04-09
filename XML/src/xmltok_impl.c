94352e18c1dd01c6b55ffab7f6f85a2c3b69cb30
Guenter Obiltschnig <guenter.obiltschnig@appinf.com>
2013-04-09 09:12:48 +0200

    upgraded expat to release 2.1.0

--- XML/src/xmltok_impl.c
+++ XML/src/xmltok_impl.c
@@ -885,7 +885,7 @@ PREFIX(scanPercent)(const ENCODING *enc, const char *ptr, const char *end,
                     const char **nextTokPtr)
 {
   if (ptr == end)
-    return -XML_TOK_PERCENT;
+    return XML_TOK_PARTIAL;
   switch (BYTE_TYPE(enc, ptr)) {
   CHECK_NMSTRT_CASES(enc, ptr, end, nextTokPtr)
   case BT_S: case BT_LF: case BT_CR: case BT_PERCNT:
