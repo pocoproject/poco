25d8bdcf1bcbd5497ac410f10cdc84eba045c61e
Guenter Obiltschnig <guenter.obiltschnig@appinf.com>
2013-05-25 00:33:49 +0200

    fix std::ios::internal usage in Poco::format()

--- Foundation/src/Format.cpp
+++ Foundation/src/Format.cpp
@@ -1,7 +1,7 @@
 //
 // Format.cpp
 //
-// $Id: //poco/1.4/Foundation/src/Format.cpp#6 $
+// $Id: //poco/1.4/Foundation/src/Format.cpp#7 $
 //
 // Library: Foundation
 // Package: Core
@@ -57,8 +57,8 @@ namespace
 			switch (*itFmt)
 			{
 			case '-': str.setf(std::ios::left); ++itFmt; break;
-			case '+': str.setf(std::ios::showpos | std::ios::internal); ++itFmt; break;
-			case '0': str.fill('0'); ++itFmt; break;
+			case '+': str.setf(std::ios::showpos); ++itFmt; break;
+			case '0': str.fill('0'); str.setf(std::ios::internal); ++itFmt; break;
 			case '#': str.setf(std::ios::showpoint | std::ios::showbase); ++itFmt; break;
 			default:  isFlag = false; break;
 			}
