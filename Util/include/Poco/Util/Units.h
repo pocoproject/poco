448d4f3da37acbb2e15b6aea6fbd0524a2e29235
Guenter Obiltschnig <guenter.obiltschnig@appinf.com>
2013-07-25 13:23:56 +0200

    Units.h fix

--- Util/include/Poco/Util/Units.h
+++ Util/include/Poco/Util/Units.h
@@ -328,12 +328,6 @@ Value<V, Power<U, Num, Den> > raise(const Value<V, U>& a)
 
 namespace Internal
 {
-#ifndef POCO_ENABLE_CPP11
-	template <bool> struct static_assert;
-	template <> struct static_assert<true> { };
-		/// Ensures (at compile-time) that the template argument is true.
-#endif
-
 	template <typename T1, typename T2>
 	struct Convertible;
 
@@ -376,12 +370,8 @@ namespace Internal
 		/// specialize this template.
 		/// The default implementation falls through to Convert2.
 	{
-#ifdef POCO_ENABLE_CPP11
-		static_assert(Convertible<T1,T2>::Value, "Error: Value not convertible.");
-#else
-		static_assert<Convertible<T1,T2>::Value> checkConvertible;
-#endif
-			/// If this fails, then T1 is not Convertible to T2:
+		/// If this fails, then T1 is not Convertible to T2:
+		poco_static_assert ((Convertible<T1,T2>::Value));
 
 		template <typename V>
 		static V fn(const V& v)
