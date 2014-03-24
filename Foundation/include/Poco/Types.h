1e80c649b86435643c81fb8b719cf0047efc6716
Luigi 'Comio' Mantellini <luigi.mantellini@gmail.com>
2014-03-24 08:31:34 +0100

    mingw/win64: check for WIN64 and enable LLP64 schema.

--- Foundation/include/Poco/Types.h
+++ Foundation/include/Poco/Types.h
@@ -42,6 +42,9 @@
 
 #include "Poco/Foundation.h"
 
+#if defined(_WIN64)
+#define __LLP64__
+#endif
 
 namespace Poco {
 
@@ -77,14 +80,22 @@ namespace Poco {
 	typedef unsigned short         UInt16;
 	typedef signed int             Int32;
 	typedef unsigned int           UInt32;
-	typedef signed long            IntPtr;
-	typedef unsigned long          UIntPtr;
 	#if defined(__LP64__)
 		#define POCO_PTR_IS_64_BIT 1
 		#define POCO_LONG_IS_64_BIT 1
+		typedef signed long        IntPtr;
+		typedef unsigned long      UIntPtr;
 		typedef signed long        Int64;
 		typedef unsigned long      UInt64;
+	#elif defined(__LLP64__)
+		#define POCO_PTR_IS_64_BIT 1
+		typedef signed long long   IntPtr;
+		typedef unsigned long long UIntPtr;
+		typedef signed long long   Int64;
+		typedef unsigned long long UInt64;
 	#else
+		typedef signed long        IntPtr;
+		typedef unsigned long      UIntPtr;
 		typedef signed long long   Int64;
 		typedef unsigned long long UInt64;
 	#endif
