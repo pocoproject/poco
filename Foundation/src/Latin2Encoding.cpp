f586911b8f78fb463cdd46cfdb6f9ea6d2e8f340
Guenter Obiltschnig <guenter.obiltschnig@appinf.com>
2014-05-15 14:32:24 +0200

    TextEncoding fixes, added reverse encodings

--- Foundation/src/Latin2Encoding.cpp
+++ Foundation/src/Latin2Encoding.cpp
@@ -1,7 +1,7 @@
 //
 // Latin2Encoding.cpp
 //
-// $Id: //poco/Main/Foundation/src/Latin2Encoding.cpp#12 $
+// $Id: //poco/1.4/Foundation/src/Latin2Encoding.cpp#1 $
 //
 // Library: Foundation
 // Package: Text
@@ -32,6 +32,7 @@ const char* Latin2Encoding::_names[] =
 
 const TextEncoding::CharacterMap Latin2Encoding::_charMap = 
 {
+	/*          00      01      02      03      04      05      06      07      08      09      0a      0b      0c      0d      0e      0f   */
 	/* 00 */	0x0000, 0x0001, 0x0002, 0x0003, 0x0004, 0x0005, 0x0006, 0x0007, 0x0008, 0x0009, 0x000a, 0x000b, 0x000c, 0x000d, 0x000e, 0x000f, 
 	/* 10 */	0x0010, 0x0011, 0x0012, 0x0013, 0x0014, 0x0015, 0x0016, 0x0017, 0x0018, 0x0019, 0x001a, 0x001b, 0x001c, 0x001d, 0x001e, 0x001f, 
 	/* 20 */	0x0020, 0x0021, 0x0022, 0x0023, 0x0024, 0x0025, 0x0026, 0x0027, 0x0028, 0x0029, 0x002a, 0x002b, 0x002c, 0x002d, 0x002e, 0x002f, 
@@ -98,8 +99,68 @@ int Latin2Encoding::convert(int ch, unsigned char* bytes, int length) const
 			*bytes = (unsigned char) ch;
 		return 1;
 	}
-	return 0;
+	switch(ch)
+	{
+	case 0x0104: if (bytes && length >= 1) *bytes = 0xa1; return 1;
+	case 0x02d8: if (bytes && length >= 1) *bytes = 0xa2; return 1;
+	case 0x0141: if (bytes && length >= 1) *bytes = 0xa3; return 1;
+	case 0x013d: if (bytes && length >= 1) *bytes = 0xa5; return 1;
+	case 0x015a: if (bytes && length >= 1) *bytes = 0xa6; return 1;
+	case 0x0160: if (bytes && length >= 1) *bytes = 0xa9; return 1;
+	case 0x015e: if (bytes && length >= 1) *bytes = 0xaa; return 1;
+	case 0x0164: if (bytes && length >= 1) *bytes = 0xab; return 1;
+	case 0x0179: if (bytes && length >= 1) *bytes = 0xac; return 1;
+	case 0x017d: if (bytes && length >= 1) *bytes = 0xae; return 1;
+	case 0x017b: if (bytes && length >= 1) *bytes = 0xaf; return 1;
+	case 0x0105: if (bytes && length >= 1) *bytes = 0xb1; return 1;
+	case 0x02db: if (bytes && length >= 1) *bytes = 0xb2; return 1;
+	case 0x0142: if (bytes && length >= 1) *bytes = 0xb3; return 1;
+	case 0x013e: if (bytes && length >= 1) *bytes = 0xb5; return 1;
+	case 0x015b: if (bytes && length >= 1) *bytes = 0xb6; return 1;
+	case 0x02c7: if (bytes && length >= 1) *bytes = 0xb7; return 1;
+	case 0x0161: if (bytes && length >= 1) *bytes = 0xb9; return 1;
+	case 0x015f: if (bytes && length >= 1) *bytes = 0xba; return 1;
+	case 0x0165: if (bytes && length >= 1) *bytes = 0xbb; return 1;
+	case 0x017a: if (bytes && length >= 1) *bytes = 0xbc; return 1;
+	case 0x02dd: if (bytes && length >= 1) *bytes = 0xbd; return 1;
+	case 0x017e: if (bytes && length >= 1) *bytes = 0xbe; return 1;
+	case 0x017c: if (bytes && length >= 1) *bytes = 0xbf; return 1;
+	case 0x0154: if (bytes && length >= 1) *bytes = 0xc0; return 1;
+	case 0x0102: if (bytes && length >= 1) *bytes = 0xc3; return 1;
+	case 0x0139: if (bytes && length >= 1) *bytes = 0xc5; return 1;
+	case 0x0106: if (bytes && length >= 1) *bytes = 0xc6; return 1;
+	case 0x010c: if (bytes && length >= 1) *bytes = 0xc8; return 1;
+	case 0x0118: if (bytes && length >= 1) *bytes = 0xca; return 1;
+	case 0x011a: if (bytes && length >= 1) *bytes = 0xcc; return 1;
+	case 0x010e: if (bytes && length >= 1) *bytes = 0xcf; return 1;
+	case 0x0110: if (bytes && length >= 1) *bytes = 0xd0; return 1;
+	case 0x0143: if (bytes && length >= 1) *bytes = 0xd1; return 1;
+	case 0x0147: if (bytes && length >= 1) *bytes = 0xd2; return 1;
+	case 0x0150: if (bytes && length >= 1) *bytes = 0xd5; return 1;
+	case 0x0158: if (bytes && length >= 1) *bytes = 0xd8; return 1;
+	case 0x016e: if (bytes && length >= 1) *bytes = 0xd9; return 1;
+	case 0x0170: if (bytes && length >= 1) *bytes = 0xdb; return 1;
+	case 0x0162: if (bytes && length >= 1) *bytes = 0xde; return 1;
+	case 0x0155: if (bytes && length >= 1) *bytes = 0xe0; return 1;
+	case 0x0103: if (bytes && length >= 1) *bytes = 0xe3; return 1;
+	case 0x013a: if (bytes && length >= 1) *bytes = 0xe5; return 1;
+	case 0x0107: if (bytes && length >= 1) *bytes = 0xe6; return 1;
+	case 0x010d: if (bytes && length >= 1) *bytes = 0xe8; return 1;
+	case 0x0119: if (bytes && length >= 1) *bytes = 0xea; return 1;
+	case 0x011b: if (bytes && length >= 1) *bytes = 0xec; return 1;
+	case 0x010f: if (bytes && length >= 1) *bytes = 0xef; return 1;
+	case 0x0111: if (bytes && length >= 1) *bytes = 0xf0; return 1;
+	case 0x0144: if (bytes && length >= 1) *bytes = 0xf1; return 1;
+	case 0x0148: if (bytes && length >= 1) *bytes = 0xf2; return 1;
+	case 0x0151: if (bytes && length >= 1) *bytes = 0xf5; return 1;
+	case 0x0159: if (bytes && length >= 1) *bytes = 0xf8; return 1;
+	case 0x016f: if (bytes && length >= 1) *bytes = 0xf9; return 1;
+	case 0x0171: if (bytes && length >= 1) *bytes = 0xfb; return 1;
+	case 0x0163: if (bytes && length >= 1) *bytes = 0xfe; return 1;
+	default: return 0;
+	}
 }
+
 
 int Latin2Encoding::queryConvert(const unsigned char* bytes, int length) const
 {
