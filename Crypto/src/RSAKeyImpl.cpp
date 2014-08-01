52ba87a6f05fc0151e7cfab35ec7f3487bd2b796
Guenter Obiltschnig <guenter.obiltschnig@appinf.com>
2014-08-01 09:57:41 +0200

    fixed GH #510: Incorrect RSAKey construction from istream

--- Crypto/src/RSAKeyImpl.cpp
+++ Crypto/src/RSAKeyImpl.cpp
@@ -86,7 +86,9 @@ RSAKeyImpl::RSAKeyImpl(
 			RSA* pubKey = PEM_read_bio_RSAPublicKey(bio, &_pRSA, 0, 0);
 			if (!pubKey)
 			{
-				int rc = BIO_seek(bio, 0);
+				int rc = BIO_reset(bio);
+				// BIO_reset() normally returns 1 for success and 0 or -1 for failure. 
+				// File BIOs are an exception, they return 0 for success and -1 for failure.
 				if (rc != 0) throw Poco::FileException("Failed to load public key", publicKeyFile);
 				pubKey = PEM_read_bio_RSA_PUBKEY(bio, &_pRSA, 0, 0);
 			}
@@ -147,8 +149,10 @@ RSAKeyImpl::RSAKeyImpl(std::istream* pPublicKeyStream, std::istream* pPrivateKey
 		RSA* publicKey = PEM_read_bio_RSAPublicKey(bio, &_pRSA, 0, 0);
 		if (!publicKey)
 		{
-			int rc = BIO_seek(bio, 0);
-			if (rc != 0) throw Poco::FileException("Failed to load public key");
+			int rc = BIO_reset(bio);
+			// BIO_reset() normally returns 1 for success and 0 or -1 for failure. 
+			// File BIOs are an exception, they return 0 for success and -1 for failure.
+			if (rc != 1) throw Poco::FileException("Failed to load public key");
 			publicKey = PEM_read_bio_RSA_PUBKEY(bio, &_pRSA, 0, 0);
 		}
 		BIO_free(bio);
