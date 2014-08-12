770a25e8fee718da831302f9793b4f322594af5e
Aleksandar Fabijanic <alex@pocoproject.org>
2014-08-12 18:37:08 -0500

    Update Process_VMS.cpp

--- Foundation/src/Process_VMS.cpp
+++ Foundation/src/Process_VMS.cpp
@@ -139,14 +139,18 @@ void ProcessImpl::killImpl(PIDImpl pid)
 	}
 }
 
+
 bool ProcessImpl::isRunningImpl(const ProcessHandleImpl& handle) 
 {
 	throw Poco::NotImplementedException("Process::is_running()");
 }
+
+
 bool ProcessImpl::isRunningImpl(PIDImpl pid) 
 {
 	throw Poco::NotImplementedException("Process::is_running()");
 }
+
 
 void ProcessImpl::requestTerminationImpl(PIDImpl pid)
 {
