e500d0f31ad5ebde0e428e4ae77bff55fa8c870a
Guenter Obiltschnig <guenter.obiltschnig@appinf.com>
2014-09-19 08:16:47 +0200

    added poco_unexpected() macro invoking Bugcheck::unexpected() to deal with unexpected exceptions in destructors

--- Foundation/src/Bugcheck.cpp
+++ Foundation/src/Bugcheck.cpp
@@ -57,6 +57,35 @@ void Bugcheck::bugcheck(const char* msg, const char* file, int line)
 }
 
 
+void Bugcheck::unexpected(const char* file, int line)
+{
+	try
+	{
+		std::string msg("Caught exception in destructor: ");
+		try
+		{
+			throw;
+		}
+		catch (Poco::Exception& exc)
+		{
+			msg += exc.displayText();
+		}
+		catch (std::exception& exc)
+		{
+			msg += exc.what();
+		}
+		catch (...)
+		{
+			msg += "unknown exception";
+		}
+		Debugger::enter(msg, file, line);
+	}
+	catch (...)
+	{
+	}	
+}
+
+
 void Bugcheck::debugger(const char* file, int line)
 {
 	Debugger::enter(file, line);
@@ -76,8 +105,6 @@ std::string Bugcheck::what(const char* msg, const char* file, int line)
 	str << "in file \"" << file << "\", line " << line;
 	return str.str();
 }
-
-
 
 
 } // namespace Poco
