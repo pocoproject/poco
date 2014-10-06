7d413184e6f8c8c62ed15d11ec1bb9221b8c83e6
Daniel Rosser <danoli3@gmail.com>
2014-10-06 14:37:39 +1100

    zlib patch for DEBUG pre-processor
    Patch as described on zlib:
    https://github.com/madler/zlib/pull/80

--- Foundation/src/deflate.h
+++ Foundation/src/deflate.h
@@ -249,7 +249,7 @@ typedef struct internal_state {
     uInt matches;       /* number of string matches in current block */
     uInt insert;        /* bytes at end of window left to insert */
 
-#ifdef DEBUG
+#ifdef ZLIB_DEBUG
     ulg compressed_len; /* total bit length of compressed file mod 2^32 */
     ulg bits_sent;      /* bit length of compressed data sent mod 2^32 */
 #endif
@@ -309,7 +309,7 @@ void ZLIB_INTERNAL _tr_stored_block OF((deflate_state *s, charf *buf,
  * used.
  */
 
-#ifndef DEBUG
+#ifndef ZLIB_DEBUG
 /* Inline versions of _tr_tally for speed: */
 
 #if defined(GEN_TREES_H) || !defined(STDC)
