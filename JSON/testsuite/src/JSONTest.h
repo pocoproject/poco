102413aed5b4ee3666e59972d52e5543a0200bf3
Alex Fabijanic <alex@pocoproject.org>
2014-10-11 18:48:00 -0500

    clang build fixes

--- JSON/testsuite/src/JSONTest.h
+++ JSON/testsuite/src/JSONTest.h
@@ -18,6 +18,15 @@
 
 #include "Poco/JSON/JSON.h"
 #include "CppUnit/TestCase.h"
+#include "Poco/JSON/Object.h"
+#include "Poco/JSON/Parser.h"
+#include "Poco/JSON/Query.h"
+#include "Poco/JSON/JSONException.h"
+#include "Poco/JSON/Stringifier.h"
+#include "Poco/JSON/ParseHandler.h"
+#include "Poco/JSON/PrintHandler.h"
+#include "Poco/JSON/Template.h"
+#include <sstream>
 
 
 class JSONTest: public CppUnit::TestCase
@@ -83,33 +92,33 @@ private:
 		std::ostringstream os;
 		os << "{ \"test\" : " << number << " }";
 		std::string json = os.str();
-		Parser parser;
-		Var result;
+		Poco::JSON::Parser parser;
+		Poco::Dynamic::Var result;
 
 		try
 		{
 			result = parser.parse(json);
 		}
-		catch (JSONException& jsone)
+		catch (Poco::JSON::JSONException& jsone)
 		{
 			std::cout << jsone.message() << std::endl;
 			assert(false);
 		}
 
-		assert(result.type() == typeid(Object::Ptr));
+		assert(result.type() == typeid(Poco::JSON::Object::Ptr));
 
-		Object::Ptr object = result.extract<Object::Ptr>();
-		Var test = object->get("test");
+		Poco::JSON::Object::Ptr object = result.extract<Poco::JSON::Object::Ptr>();
+		Poco::Dynamic::Var test = object->get("test");
 		assert(test.isNumeric());
 		T value = test;
 		assert(value == number);
 
-		DynamicStruct ds = *object;
+		Poco::DynamicStruct ds = *object;
 		assert(!ds["test"].isEmpty());
 		assert(ds["test"].isNumeric());
 		assert(ds["test"] == number);
 
-		const DynamicStruct& rds = *object;
+		const Poco::DynamicStruct& rds = *object;
 		assert(!rds["test"].isEmpty());
 		assert(rds["test"].isNumeric());
 		assert(rds["test"] == number);
