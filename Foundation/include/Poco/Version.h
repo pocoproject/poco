1e471d734323e5973d5a8d64d54eb0f38857f394
Guenter Obiltschnig <guenter.obiltschnig@appinf.com>
2014-12-11 20:11:20 +0100

    semantic versioning documentation

--- Foundation/include/Poco/Version.h
+++ Foundation/include/Poco/Version.h
@@ -23,16 +23,19 @@
 //
 // Version Information
 //
+// Since 1.6.0, we're using Semantic Versioning 2.0
+// (http://semver.org/spec/v2.0.0.html)
+//
 // Version format is 0xAABBCCDD, where
 //    - AA is the major version number,
 //    - BB is the minor version number,
-//    - CC is the revision number, and
-//    - DD is the patch level number.
-//      Note that some patch level numbers have
-//      a special meaning:
-//      Dx are development releases
-//      Ax are alpha releases
-//      Bx are beta releases
+//    - CC is the patch	version number, and
+//    - DD is the pre-release designation/number.
+//      The pre-release designation hex digits have a special meaning:
+//      00: final/stable releases
+//      Dx: development releases
+//      Ax: alpha releases
+//      Bx: beta releases
 //
 #define POCO_VERSION 0x010600D1
 
