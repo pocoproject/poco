fe313fa278e1e721939901e42f9404ebb0ae1801
Björn Schramke <bjoern@schramke-online.de>
2015-01-28 16:12:39 +0100

    update class documentation for Util::SystemConfiguration and Util::Application

--- Util/include/Poco/Util/SystemConfiguration.h
+++ Util/include/Poco/Util/SystemConfiguration.h
@@ -42,7 +42,9 @@ class Util_API SystemConfiguration: public AbstractConfiguration
 	///   - system.configHomeDir: the base directory relative to which user specific configuration files should be stored
 	///   - system.cacheHomeDir: the base directory relative to which user specific non-essential data files should be stored
 	///   - system.dataHomeDir: the base directory relative to which user specific data files should be stored
+	///   - system.tempHomeDir: the base directory relative to which user-specific temporary files and other file objects should be placed
 	///   - system.tempDir: the system's temporary directory
+	///   - system.configDir: the system's configuration directory
 	///   - system.dateTime: the current UTC date and time, formatted in ISO 8601 format.
 	///   - system.pid: the current process ID.
 	///   - system.env.<NAME>: the environment variable with the given <NAME>.
