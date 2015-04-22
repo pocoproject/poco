62bcc5c6edbbb67388232f03fc36c17730f5c138
Alex Fabijanic <alex@pocoproject.org>
2015-04-22 18:36:12 -0500

    GCC_DIAG_* definitions are missing for Windows (MinGW) #785

--- Foundation/include/Poco/Bugcheck.h
+++ Foundation/include/Poco/Bugcheck.h
@@ -148,9 +148,7 @@ protected:
 //
 
 
-#if defined(POCO_COMPILER_GCC) && (((__GNUC__ * 100) + __GNUC_MINOR__) >= 408) && !defined(POCO_ANDROID)
-	GCC_DIAG_OFF(unused-local-typedefs) // supress numerous gcc warnings
-#endif // POCO_COMPILER_GCC && (((__GNUC__ * 100) + __GNUC_MINOR__) >= 408)
+GCC_DIAG_OFF(unused-local-typedefs) // supress numerous gcc warnings
 
 
 template <bool x>
