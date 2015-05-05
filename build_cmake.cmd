a58bb475a659fe1b5b5171f22f8e43b98b324e0a
Marian Krivos <nezmar@tutok.sk>
2015-05-05 18:26:03 +0200

    win build script

--- build_cmake.cmd
+++ build_cmake.cmd
@@ -1,14 +1,15 @@
-# POCO_STATIC=1 - for static build
-# POCO_UNBUNDLED - for no built-in version of libs
-# CMAKE_INSTALL_PREFIX=path - for install path
+rem POCO_STATIC=1 - for static build
+rem POCO_UNBUNDLED - for no built-in version of libs
+rem CMAKE_INSTALL_PREFIX=path - for install path
+
 
 mkdir cmake-build
+
 
 cd cmake-build
 
 
-cmake .. -DCMAKE_BUILD_TYPE=Debug -G"NMake Makefiles JOM" %1 %2 %3 %4 %5
-
+cmake .. -DCMAKE_BUILD_TYPE=Debug -G"NMake Makefiles JOM" -DCMAKE_INSTALL_PREFIX=/opt/rsys/platform -DREMOTING=1
 jom /j3
 
 jom install
@@ -18,7 +19,7 @@ del CMakeCache.txt
 
 
 
-cmake .. -DCMAKE_BUILD_TYPE=Release -G"NMake Makefiles JOM" %1 %2 %3 %4 %5
+cmake .. -DCMAKE_BUILD_TYPE=Release -G"NMake Makefiles JOM" -DCMAKE_INSTALL_PREFIX="/opt/rsys/platform" -DREMOTING=1
 
 jom /j3
 
