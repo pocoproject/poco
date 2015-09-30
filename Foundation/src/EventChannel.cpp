92e2a19661b3b9b722136e8ab6453b8651b989ec
Guenter Obiltschnig <guenter.obiltschnig@appinf.com>
2015-09-30 11:03:03 +0200

    added EventChannel class (from macchina.io)

---
+++ Foundation/src/EventChannel.cpp
@@ -0,0 +1,39 @@
+//
+// EventChannel.cpp
+//
+// $Id$
+//
+// Library: Foundation
+// Package: Logging
+// Module:  EventChannel
+//
+// Copyright (c) 2015, Applied Informatics Software Engineering GmbH.
+// and Contributors.
+//
+// SPDX-License-Identifier:	BSL-1.0
+//
+
+
+#include "Poco/EventChannel.h"
+
+
+namespace Poco {
+
+
+EventChannel::EventChannel()
+{
+}
+
+
+EventChannel::~EventChannel()
+{
+}
+
+
+void EventChannel::log(const Message& msg)
+{
+	messageLogged(this, msg);
+}
+
+
+} // namespace Poco
