4257a20536b1fc5930c80b5f71c45626e465e9ff
FrancisANDRE <zosrothko@orange.fr>
2015-10-03 10:36:52 +0200

    Change CYGWIN to Cygwin. Add LD_LIBRARY_PATH to testing dynamic shared library load on Linux.
    Signed-off-by: FrancisANDRE <zosrothko@orange.fr>

--- build/script/runtests.sh
+++ build/script/runtests.sh
@@ -82,7 +82,7 @@ do
 				echo ""
 
 				runs=`expr $runs + 1`
-				sh -c "cd $POCO_BASE/$comp/testsuite/$BINDIR && LD_LIBRARY_PATH=. $TESTRUNNER $TESTRUNNERARGS"
+				sh -c "cd $POCO_BASE/$comp/testsuite/$BINDIR && LD_LIBRARY_PATH=$LD_LIBRARY_PATH:. $TESTRUNNER $TESTRUNNERARGS"
 				if [ $? -ne 0 ] ; then
 					failures=`expr $failures + 1`
 					failedTests="$failedTests $comp"
