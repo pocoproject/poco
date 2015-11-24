0edf68d620dbed49cf27c430e83716b2a7801716
FrancisANDRE <zosrothko@orange.fr>
2015-11-24 09:22:47 +0100

    Remove jom for now.
    Signed-off-by: FrancisANDRE <zosrothko@orange.fr>

--- build_cmake.cmd
+++ build_cmake.cmd
@@ -6,13 +6,15 @@ mkdir cmake-build
 cd cmake-build
 
 del CMakeCache.txt
-cmake ../. -DCMAKE_BUILD_TYPE=Debug -G"NMake Makefiles JOM" %1 %2 %3 %4 %5
-jom /j3
-jom install
+cmake ../. -DCMAKE_BUILD_TYPE=Debug -G"NMake Makefiles" %1 %2 %3 %4 %5
+cmake --build .
+rem jom /j3
+rem jom install
 
 del CMakeCache.txt
-cmake ../. -DCMAKE_BUILD_TYPE=Release -G"NMake Makefiles JOM" %1 %2 %3 %4 %5
-jom /j3
-jom install
+cmake ../. -DCMAKE_BUILD_TYPE=Release -G"NMake Makefiles" %1 %2 %3 %4 %5
+cmake --build .
+rem jom /j3
+rem jom install
 
 cd ..
