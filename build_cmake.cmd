98f054e3d3545d0da665e4d107dd1c97c09b9904
FrancisANDRE <zosrothko@orange.fr>
2015-11-24 19:38:01 +0100

    Use Debug & Release configration.
    Signed-off-by: FrancisANDRE <zosrothko@orange.fr>

--- build_cmake.cmd
+++ build_cmake.cmd
@@ -1,3 +1,6 @@
+rem usage
+rem build_cmake ( Debug | Release )
+
 rem POCO_STATIC=1 - for static build
 rem POCO_UNBUNDLED - for no built-in version of libs
 rem CMAKE_INSTALL_PREFIX=path - for install path
@@ -6,12 +9,7 @@ mkdir cmake-build
 cd cmake-build
 
 del CMakeCache.txt
-cmake ../. -DCMAKE_BUILD_TYPE=Debug -G"NMake Makefiles" -DENABLE_TESTS=ON -DENABLE_SAMPLE=ON %1 %2 %3 %4 %5
-jom /i /j3
-jom /i install
-
-del CMakeCache.txt
-cmake ../. -DCMAKE_BUILD_TYPE=Release -G"NMake Makefiles"  -DENABLE_TESTS=ON -DENABLE_SAMPLE=ON %1 %2 %3 %4 %5
+cmake ../. -DCMAKE_BUILD_TYPE=%1 -G"NMake Makefiles"  -DENABLE_TESTS=ON -DENABLE_SAMPLE=ON %2 %3 %4 %5
 jom /i /j3
 jom /i install
 
