b47f38fc336c6c7a36fe8368423281c3c4067600
Rangel Reale <rangelspam@gmail.com>
2015-12-10 13:16:02 -0200

    * Don't leak <windows.h> (Poco/Unwindows.h) to library users

--- Crypto/src/X509Certificate.cpp
+++ Crypto/src/X509Certificate.cpp
@@ -20,6 +20,10 @@
 #include "Poco/DateTimeParser.h"
 #include <sstream>
 #include <openssl/pem.h>
+#ifdef _WIN32
+// fix for WIN32 header conflict
+#undef X509_NAME 
+#endif
 #include <openssl/x509v3.h>
 #include <openssl/err.h>
 #include <openssl/evp.h>
