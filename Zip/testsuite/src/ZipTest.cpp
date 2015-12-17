5928a19546dac4ed349d58126f7ec43d12461feb
FrancisANDRE <zosrothko@orange.fr>
2015-12-17 08:11:38 +0100

    Use KB instead of MB to avoid cloging AppVeyor & Travis
    Signed-off-by: FrancisANDRE <zosrothko@orange.fr>

--- Zip/testsuite/src/ZipTest.cpp
+++ Zip/testsuite/src/ZipTest.cpp
@@ -211,9 +211,9 @@ void ZipTest::verifyDataFile(const std::string& path, Poco::UInt64 size)
 void ZipTest::testDecompressZip64()
 {
 	std::map<std::string, Poco::UInt64> files;
-	files["data1.bin"] = static_cast<Poco::UInt64>(MB)*4096+1;
-	files["data2.bin"] = static_cast<Poco::UInt64>(MB)*16;
-	files["data3.bin"] = static_cast<Poco::UInt64>(MB)*4096-1;
+	files["data1.bin"] = static_cast<Poco::UInt64>(KB)*4096+1;
+	files["data2.bin"] = static_cast<Poco::UInt64>(KB)*16;
+	files["data3.bin"] = static_cast<Poco::UInt64>(KB)*4096-1;
 	
 	for(std::map<std::string, Poco::UInt64>::const_iterator it = files.begin(); it != files.end(); it++) 
 	{
