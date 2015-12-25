466dbc4f68e77d8c457425adfc41474e595b89e8
FrancisANDRE <zosrothko@orange.fr>
2015-12-25 23:02:20 +0100

    Set proper path
    Signed-off-by: FrancisANDRE <zosrothko@orange.fr>

--- travis/runtests.sh
+++ travis/runtests.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -ev
 export POCO_BASE=`pwd`
-source ignored.sh
-source excluded.sh
+source ./travis/ignored.sh
+source ./travis/excluded.sh
 export PATH=$PATH:.
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:.
 build/script/runtests.sh
