1cbaf38bbc84159b2ffa87ecc5d8be15d943dc31
Guenter Obiltschnig <guenter.obiltschnig@appinf.com>
2016-02-17 20:32:19 +0100

    fixed an issue with DELETE in persistent connections

--- Net/src/HTTPServerRequestImpl.cpp
+++ Net/src/HTTPServerRequestImpl.cpp
@@ -58,7 +58,7 @@ HTTPServerRequestImpl::HTTPServerRequestImpl(HTTPServerResponseImpl& response, H
 #else
 		_pStream = new HTTPFixedLengthInputStream(session, getContentLength());
 #endif
-	else if (getMethod() == HTTPRequest::HTTP_GET || getMethod() == HTTPRequest::HTTP_HEAD)
+	else if (getMethod() == HTTPRequest::HTTP_GET || getMethod() == HTTPRequest::HTTP_HEAD || getMethod() == HTTPRequest::HTTP_DELETE)
 		_pStream = new HTTPFixedLengthInputStream(session, 0);
 	else
 		_pStream = new HTTPInputStream(session);
