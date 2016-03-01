ce4341e96bfc0998f3c11701b7d0cd4882189283
Guenter Obiltschnig <guenter.obiltschnig@appinf.com>
2016-03-01 11:48:14 +0100

    removed automatic registration as it can cause a deadlock in mysql_library_init

--- Data/MySQL/include/Poco/Data/MySQL/Connector.h
+++ Data/MySQL/include/Poco/Data/MySQL/Connector.h
@@ -24,12 +24,6 @@
 #include "Poco/Data/Connector.h"
 
 
-// Note: to avoid static (de)initialization problems,
-// during connector automatic (un)registration, it is 
-// best to have this as a macro.
-#define POCO_DATA_MYSQL_CONNECTOR_NAME "mysql"
-
-
 namespace Poco {
 namespace Data {
 namespace MySQL {
@@ -64,64 +58,6 @@ public:
 
 
 } } } // namespace Poco::Data::MySQL
-
-
-// 
-// Automatic Connector registration
-// 
-
-struct MySQL_API MySQLConnectorRegistrator
-	/// Connector registering class.
-	/// A global instance of this class is instantiated
-	/// with sole purpose to automatically register the 
-	/// MySQL connector with central Poco Data registry.
-{
-	MySQLConnectorRegistrator()
-		/// Calls Poco::Data::MySQL::registerConnector();
-	{
-		Poco::Data::MySQL::Connector::registerConnector();
-	}
-
-	~MySQLConnectorRegistrator()
-		/// Calls Poco::Data::MySQL::unregisterConnector();
-	{
-		try
-		{
-			Poco::Data::MySQL::Connector::unregisterConnector();
-		}
-		catch (...)
-		{
-			poco_unexpected();
-		}
-	}
-};
-
-
-#if !defined(POCO_NO_AUTOMATIC_LIB_INIT)
-	#if defined(POCO_OS_FAMILY_WINDOWS) && !defined(__GNUC__)
-		extern "C" const struct MySQL_API MySQLConnectorRegistrator pocoMySQLConnectorRegistrator;
-		#if defined(MySQL_EXPORTS)
-			#if defined(_WIN64)
-				#define POCO_DATA_MYSQL_FORCE_SYMBOL(s) __pragma(comment (linker, "/export:"#s))
-			#elif defined(_WIN32)
-				#define POCO_DATA_MYSQL_FORCE_SYMBOL(s) __pragma(comment (linker, "/export:_"#s))
-			#endif
-		#else  // !MySQL_EXPORTS
-			#if defined(_WIN64)
-				#define POCO_DATA_MYSQL_FORCE_SYMBOL(s) __pragma(comment (linker, "/include:"#s))
-			#elif defined(_WIN32)
-				#define POCO_DATA_MYSQL_FORCE_SYMBOL(s) __pragma(comment (linker, "/include:_"#s))
-			#endif
-		#endif // MySQL_EXPORTS
-	#else // !POCO_OS_FAMILY_WINDOWS
-			#define POCO_DATA_MYSQL_FORCE_SYMBOL(s) extern "C" const struct MySQLConnectorRegistrator s;
-	#endif // POCO_OS_FAMILY_WINDOWS
-	POCO_DATA_MYSQL_FORCE_SYMBOL(pocoMySQLConnectorRegistrator)
-#endif // POCO_NO_AUTOMATIC_LIB_INIT
-
-// 
-// End automatic Connector registration
-// 
 
 
 #endif // Data_MySQL_Connector_INCLUDED
