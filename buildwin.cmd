6946780baf5b09fd06a6e3af12d399b83d0b19c3
Matt Tucker <tuck@whistlingfish.net>
2016-03-11 11:19:08 -0800

    windows: add jenkins build script, add winres.h (required to compile)

--- buildwin.cmd
+++ buildwin.cmd
@@ -28,7 +28,7 @@ rem
 rem VS_VERSION is required argument. Default is build all.
 
 rem Change OPENSSL_DIR to match your setup
-set OPENSSL_DIR=C:\OpenSSL
+set OPENSSL_DIR=C:\OpenSSL-Win32
 set OPENSSL_INCLUDE=%OPENSSL_DIR%\include
 set OPENSSL_LIB=%OPENSSL_DIR%\lib;%OPENSSL_DIR%\lib\VC
 set INCLUDE=%INCLUDE%;%OPENSSL_INCLUDE%
---
+++ jenkins.cmd
@@ -0,0 +1,20 @@
+@echo off
+
+echo Writing components list ...
+
+echo Foundation> components
+echo XML>> components
+echo JSON>> components
+echo Util>> components
+echo Net>> components
+echo Crypto>> components
+echo NetSSL_OpenSSL>> components
+echo Zip>> components
+
+type components
+
+echo Initializing Visual Studio environment
+call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin\vcvars32.bat"
+
+echo Building poco
+buildwin 100 build all both Win32 nosamples vcexpress
