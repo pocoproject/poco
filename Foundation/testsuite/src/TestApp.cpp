50240732f95207630d678a6d2f0a66fd65bbe34b
Guenter Obiltschnig <guenter.obiltschnig@appinf.com>
2016-04-21 09:25:26 +0200

    pre c++11 fixes

--- Foundation/testsuite/src/TestApp.cpp
+++ Foundation/testsuite/src/TestApp.cpp
@@ -18,6 +18,7 @@
 #include <string>
 #include <iostream>
 #include <cstdlib>
+#include <signal.h>
 
 
 int main(int argc, char** argv)
@@ -48,8 +49,8 @@ int main(int argc, char** argv)
 		}
 		else if (arg == "-raise-int")
 		{
-			std::signal(SIGINT, SIG_DFL);
-			std::raise(SIGINT);
+			signal(SIGINT, SIG_DFL);
+			raise(SIGINT);
 		}
 		else if (arg == "-echo-args")
 		{
