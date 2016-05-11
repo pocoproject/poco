241a9bb8d85d6fa71ce687fb599a444e3b308aca
Vladimir Gamalian <vladimirgamalian@users.noreply.github.com>
2016-05-11 12:25:07 +0700

    Add const modificator to SocketAddress arguments

--- Net/src/ICMPClient.cpp
+++ Net/src/ICMPClient.cpp
@@ -56,7 +56,7 @@ int ICMPClient::ping(const std::string& address, int repeat) const
 }
 
 
-int ICMPClient::ping(SocketAddress& address, int repeat) const
+int ICMPClient::ping(const SocketAddress& address, int repeat) const
 {
 	if (repeat <= 0) return 0;
 
@@ -116,7 +116,7 @@ int ICMPClient::pingIPv4(const std::string& address, int repeat)
 }
 
 
-int ICMPClient::ping(SocketAddress& address, IPAddress::Family family, int repeat)
+int ICMPClient::ping(const SocketAddress& address, IPAddress::Family family, int repeat)
 {
 	if (repeat <= 0) return 0;
 
