28ff1f2530ea9f808b0dbc8bd6eaec70d19de6ae
Guenter Obiltschnig <guenter.obiltschnig@appinf.com>
2016-09-08 09:36:28 +0200

    fixed strerror_r issue

--- Net/src/Socket.cpp
+++ Net/src/Socket.cpp
@@ -148,8 +148,7 @@ int Socket::select(SocketList& readList, SocketList& writeList, SocketList& exce
 		if (epollfd < 0)
 		{
 			char buf[1024];
-			int rc = strerror_r(errno, buf, sizeof(buf));
-			poco_assert (rc == 0);
+			(void) strerror_r(errno, buf, sizeof(buf));
 			SocketImpl::error(std::string("Can't create epoll queue: ") + buf);
 		}
 
@@ -161,9 +160,8 @@ int Socket::select(SocketList& readList, SocketList& writeList, SocketList& exce
 				if (epoll_ctl(epollfd, EPOLL_CTL_ADD, sockfd, e) < 0)
 				{
 					char buf[1024];
-					int rc = strerror_r(errno, buf, sizeof(buf));
+					(void) strerror_r(errno, buf, sizeof(buf));
 					::close(epollfd);
-					poco_assert (rc == 0);
 					SocketImpl::error(std::string("Can't insert socket to epoll queue: ") + buf);
 				}
 			}
