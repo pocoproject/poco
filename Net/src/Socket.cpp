b22501f6926f9fbeaa06e31bc65bc7ba599eabfc
Guenter Obiltschnig <guenter.obiltschnig@appinf.com>
2016-10-18 18:13:52 +0200

    another fix for GH #1458 - handle case when all sockets are invalid

--- Net/src/Socket.cpp
+++ Net/src/Socket.cpp
@@ -144,6 +144,8 @@ int Socket::select(SocketList& readList, SocketList& writeList, SocketList& exce
 		}
 
 		epollSize = eventLast - eventsIn;
+		if (epollSize == 0) return 0;
+		
 		epollfd = epoll_create(1);
 		if (epollfd < 0)
 		{
