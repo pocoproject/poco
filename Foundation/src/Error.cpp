1435c9ad97019d61594918c16655406d8c8fa021
Guenter Obiltschnig <guenter.obiltschnig@appinf.com>
2017-02-14 14:35:56 +0100

    revert changes to Error.cpp; it's hopeless with strerror_r()...

--- Foundation/src/Error.cpp
+++ Foundation/src/Error.cpp
@@ -1,7 +1,7 @@
 //
 // Error.cpp
 //
-// $Id: //poco/1.7/Foundation/src/Error.cpp#3 $
+// $Id: //poco/1.4/Foundation/src/Error.cpp#3 $
 //
 // Library: Foundation
 // Package: Core
@@ -66,11 +66,11 @@ namespace Poco {
 		   without -D_GNU_SOURCE is needed, otherwise the GNU version is
 		   preferred.
 		*/
-#if _POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600 || POCO_ANDROID || __APPLE__
+#if (_XOPEN_SOURCE >= 600) || POCO_ANDROID
 		char errmsg[256] = "";
-		int rc = strerror_r(errorCode, errmsg, 256);
-		return rc == 0 ? std::string(errmsg) : std::string();
-#elif defined(__GLIBC__) && _GNU_SOURCE
+		strerror_r(errorCode, errmsg, 256);
+		return errmsg;
+#elif defined _GNU_SOURCE
 		char errmsg[256] = "";
 		return std::string(strerror_r(errorCode, errmsg, 256));
 #else
