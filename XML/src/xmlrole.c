85879fb02dd89004421daeedba78932896941eab
Guenter Obiltschnig <guenter.obiltschnig@appinf.com>
2017-06-21 15:15:55 +0200

    upgrade bundled expat to 2.2.1; apply POCO-specific changes

--- XML/src/xmlrole.c
+++ XML/src/xmlrole.c
@@ -6,12 +6,6 @@
 
 #ifdef EXPAT_WIN32
 #include "winconfig.h"
-#elif defined(MACOS_CLASSIC)
-#include "macconfig.h"
-#elif defined(__amigaos__)
-#include "amigaconfig.h"
-#elif defined(__WATCOMC__)
-#include "watcomconfig.h"
 #else
 #ifdef HAVE_EXPAT_CONFIG_H
 #include "expat_config.h"
