efe6fb9d921bf164d11f43cfd1c868e010b5d158
Matt Tucker <tuck@whistlingfish.net>
2017-07-06 11:57:12 -0700

    TCPServerDispatcher: fix thread accounting leak
    * TCPServerDispatcher::run: catch errors in connection creation and handling to prevent threads from exiting without accounting for them

--- Net/src/TCPServerDispatcher.cpp
+++ Net/src/TCPServerDispatcher.cpp
@@ -18,6 +18,7 @@
 #include "Poco/Net/TCPServerConnectionFactory.h"
 #include "Poco/Notification.h"
 #include "Poco/AutoPtr.h"
+#include "Poco/ErrorHandler.h"
 #include <memory>
 
 
@@ -104,24 +105,38 @@ void TCPServerDispatcher::run()
 
 	for (;;)
 	{
-		AutoPtr<Notification> pNf = _queue.waitDequeueNotification(idleTime);
-		if (pNf)
-		{
-			TCPConnectionNotification* pCNf = dynamic_cast<TCPConnectionNotification*>(pNf.get());
-			if (pCNf)
+		try {
+			AutoPtr<Notification> pNf = _queue.waitDequeueNotification(idleTime);
+			if (pNf)
 			{
-#if __cplusplus < 201103L
-				std::auto_ptr<TCPServerConnection> pConnection(_pConnectionFactory->createConnection(pCNf->socket()));
-#else
-				std::unique_ptr<TCPServerConnection> pConnection(_pConnectionFactory->createConnection(pCNf->socket()));
-#endif
-				poco_check_ptr(pConnection.get());
-				beginConnection();
-				pConnection->start();
-				endConnection();
+				TCPConnectionNotification* pCNf = dynamic_cast<TCPConnectionNotification*>(pNf.get());
+				if (pCNf)
+				{
+	#if __cplusplus < 201103L
+					std::auto_ptr<TCPServerConnection> pConnection(_pConnectionFactory->createConnection(pCNf->socket()));
+	#else
+					std::unique_ptr<TCPServerConnection> pConnection(_pConnectionFactory->createConnection(pCNf->socket()));
+	#endif
+					poco_check_ptr(pConnection.get());
+					beginConnection();
+					pConnection->start();
+					endConnection();
+				}
 			}
 		}
-	
+		catch (Poco::Exception &exc)
+		{
+			ErrorHandler::handle(exc);
+		}
+		catch (std::exception &exc)
+		{
+			ErrorHandler::handle(exc);
+		}
+		catch (...)
+		{
+			ErrorHandler::handle();
+		}
+
 		FastMutex::ScopedLock lock(_mutex);
 		if (_stopped || (_currentThreads > 1 && _queue.empty()))
 		{
