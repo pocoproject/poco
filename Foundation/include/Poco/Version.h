8ca493bddf9053ba6f6c4bf621c9b680226eec93
Günter Obiltschnig <guenter.obiltschnig@appinf.com>
2017-08-10 16:47:25 +0200

    fixed GH #1813: xmlparse.cpp doesn't compile in WinCE (poco 1.7.8p3)

--- Foundation/include/Poco/Version.h
+++ Foundation/include/Poco/Version.h
@@ -37,7 +37,7 @@
 //      Ax: alpha releases
 //      Bx: beta releases
 //
-#define POCO_VERSION 0x01070803
+#define POCO_VERSION 0x01070804
 
 
 #endif // Foundation_Version_INCLUDED
--- XML/src/xmlparse.cpp
+++ XML/src/xmlparse.cpp
@@ -917,6 +917,7 @@ gather_time_entropy(void)
 
 static unsigned long
 ENTROPY_DEBUG(const char * label, unsigned long entropy) {
+#ifdef ENABLE_EXPAT_ENTROPY_DEBUG
   const char * const EXPAT_ENTROPY_DEBUG = getenv("EXPAT_ENTROPY_DEBUG");
   if (EXPAT_ENTROPY_DEBUG && ! strcmp(EXPAT_ENTROPY_DEBUG, "1")) {
     fprintf(stderr, "Entropy: %s --> 0x%0*lx (%lu bytes)\n",
@@ -924,6 +925,7 @@ ENTROPY_DEBUG(const char * label, unsigned long entropy) {
         (int)sizeof(entropy) * 2, entropy,
         (unsigned long)sizeof(entropy));
   }
+#endif
   return entropy;
 }
 
