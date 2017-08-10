07a00c0da1ee4520e26adf3937bfb5e8349c270d
Günter Obiltschnig <guenter.obiltschnig@appinf.com>
2017-08-10 12:18:44 +0200

    removed pointless statements causing VC++ warnings

--- XML/src/xmlparse.cpp
+++ XML/src/xmlparse.cpp
@@ -6684,8 +6684,6 @@ hash(XML_Parser parser, KEY s)
 {
   struct siphash state;
   struct sipkey key;
-  (void)sip_tobin;
-  (void)sip24_valid;
   copy_salt_to_sipkey(parser, &key);
   sip24_init(&state, &key);
   sip24_update(&state, s, keylen(s) * sizeof(XML_Char));
