1a5d49abe1992c1867bf22dfb10cb86bf0b2e625
Rangel Reale <rangelspam@gmail.com>
2017-08-14 15:53:57 -0300

    * Emscripten build support

--- Util/src/ServerApplication.cpp
+++ Util/src/ServerApplication.cpp
@@ -65,7 +65,7 @@ SERVICE_STATUS        ServerApplication::_serviceStatus;
 SERVICE_STATUS_HANDLE ServerApplication::_serviceStatusHandle = 0; 
 #endif
 #endif
-#if defined(POCO_VXWORKS) || defined(POCO_ANDROID) || defined(__NACL__)
+#if defined(POCO_VXWORKS) || defined(POCO_ANDROID) || defined(__NACL__) ||  defined(__EMSCRIPTEN__)
 Poco::Event ServerApplication::_terminate;
 #endif
 
@@ -103,7 +103,7 @@ void ServerApplication::terminate()
 {
 #if defined(POCO_OS_FAMILY_WINDOWS)
 	_terminate.set();
-#elif defined(POCO_VXWORKS) || defined(POCO_ANDROID) || defined(__NACL__)
+#elif defined(POCO_VXWORKS) || defined(POCO_ANDROID) || defined(__NACL__) || defined(__EMSCRIPTEN__)
 	_terminate.set();
 #else
 	Poco::Process::requestTermination(Process::id());
@@ -593,7 +593,7 @@ void ServerApplication::defineOptions(OptionSet& options)
 //
 void ServerApplication::waitForTerminationRequest()
 {
-#if !defined(POCO_ANDROID) && !defined(__NACL__)
+#if !defined(POCO_ANDROID) && !defined(__NACL__) && !defined(__EMSCRIPTEN__)
 	sigset_t sset;
 	sigemptyset(&sset);
 	if (!std::getenv("POCO_ENABLE_DEBUGGER"))
@@ -605,7 +605,7 @@ void ServerApplication::waitForTerminationRequest()
 	sigprocmask(SIG_BLOCK, &sset, NULL);
 	int sig;
 	sigwait(&sset, &sig);
-#else // POCO_ANDROID || __NACL__
+#else // POCO_ANDROID || __NACL__ || __EMSCRIPTEN__
 	_terminate.wait();
 #endif
 }
