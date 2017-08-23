0f6c528435cec67b23e75da8a31410643bd7026c
zosrothko <zosrothko@orange.fr>
2017-08-23 16:52:00 +0200

    PocoDoc: support generation on Windows (#1854)
    * PocoDoc: support generation on Windows
    
    * Replace hard code path with variables:
    VC should be set to the path of the C++ compiler (cl.exe)
    WDK should be set to the Windows Development Kit
    
    * Update CHANGELOG with the latest 1.7.8p4 one

--- Foundation/src/Environment.cpp
+++ Foundation/src/Environment.cpp
@@ -130,5 +130,41 @@ Poco::UInt32 Environment::libraryVersion()
 	return POCO_VERSION;
 }
 
+Poco::Int32 Environment::os()
+{
+	return POCO_OS;
+}
+
+Poco::Int32 Environment::cpu()
+{
+	return POCO_ARCH;
+}
+
+bool Environment::osFamilyUnix()
+{
+#if defined(POCO_OS_FAMILY_UNIX)
+	return true;
+#else
+	return false;
+#endif
+}
+
+bool Environment::osFamilyWindows()
+{
+#if defined(POCO_OS_FAMILY_WINDOWS)
+	return true;
+#else
+	return false;
+#endif
+}
+
+bool Environment::osFamilyVms()
+{
+#if defined(POCO_OS_FAMILY_VMS)
+	return true;
+#else
+	return false;
+#endif
+}
 
 } // namespace Poco
