76dcf5bed0e4b130b0c033d1525d68f74897b455
zosrothko <zosrothko@orange.fr>
2017-08-26 17:03:11 +0200

    Use Mutex_STD except for Cygwin

--- Foundation/include/Poco/Mutex.h
+++ Foundation/include/Poco/Mutex.h
@@ -25,18 +25,10 @@
 #include "Poco/ScopedLock.h"
 
 
-#if (POCO_OS != POCO_OS_CYGWIN)
-#include "Poco/Mutex_STD.h"
-#elif defined(POCO_OS_FAMILY_WINDOWS)
-#if defined(_WIN32_WCE)
-#include "Poco/Mutex_WINCE.h"
-#else
-#include "Poco/Mutex_WIN32.h"
-#endif
-#elif defined(POCO_VXWORKS)
-#include "Poco/Mutex_VX.h"
-#else
+#if (POCO_OS == POCO_OS_CYGWIN)
 #include "Poco/Mutex_POSIX.h"
+#else
+#include "Poco/Mutex_STD.h"
 #endif
 
 
