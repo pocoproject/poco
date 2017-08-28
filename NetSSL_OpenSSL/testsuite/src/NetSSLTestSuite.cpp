afd72d8c87ab923796476e59c98d28c5bafce7a6
Michele Pradella <michele.pradella@selea.com>
2017-08-28 17:36:11 +0200

    comment misspelled
    checked misspelled comment, and re-add WebSocket testsuite

--- NetSSL_OpenSSL/testsuite/src/NetSSLTestSuite.cpp
+++ NetSSL_OpenSSL/testsuite/src/NetSSLTestSuite.cpp
@@ -15,6 +15,7 @@
 #include "HTTPSClientTestSuite.h"
 #include "TCPServerTestSuite.h"
 #include "HTTPSServerTestSuite.h"
+#include "WebSocketTestSuite.h"
 #include "FTPSClientTestSuite.h"
 
 
@@ -26,6 +27,7 @@ CppUnit::Test* NetSSLTestSuite::suite()
 	pSuite->addTest(HTTPSClientTestSuite::suite());
 	pSuite->addTest(TCPServerTestSuite::suite());
 	pSuite->addTest(HTTPSServerTestSuite::suite());
+	pSuite->addTest(WebSocketTestSuite::suite());
 	pSuite->addTest(FTPSClientTestSuite::suite());
 
 	return pSuite;
