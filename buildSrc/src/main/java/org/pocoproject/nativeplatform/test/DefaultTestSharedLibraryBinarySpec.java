085f81a21cdf923c9616a7ce0f99539bb69a3f36
zosrothko <francis.andre@metrixware.com>
2017-09-07 22:24:41 +0200

    WIP

--- buildSrc/src/main/java/org/pocoproject/nativeplatform/test/DefaultTestSharedLibraryBinarySpec.java
+++ buildSrc/src/main/java/org/pocoproject/nativeplatform/test/DefaultTestSharedLibraryBinarySpec.java
@@ -2,6 +2,6 @@ package org.pocoproject.nativeplatform.test;
 
 import org.gradle.nativeplatform.internal.DefaultSharedLibraryBinarySpec;
 
-public class DefaultSharedLibraryTestSuiteBinarySpec extends DefaultSharedLibraryBinarySpec implements SharedLibraryTestSuiteBinarySpec {
+public class DefaultTestSharedLibraryBinarySpec extends DefaultSharedLibraryBinarySpec implements TestSharedLibraryBinarySpec {
 
 }
