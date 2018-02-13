6ed085f288abfc6545e9285e4b6599529108d5f4
Günter Obiltschnig <guenter.obiltschnig@appinf.com>
2018-02-13 21:32:17 +0100

    fixed doc typos; changed maximum sequence length to 4

--- Foundation/testsuite/src/UTF8StringTest.cpp
+++ Foundation/testsuite/src/UTF8StringTest.cpp
@@ -31,32 +31,32 @@ void UTF8StringTest::testCompare()
 {
 	std::string a1("aaaaa");
 	std::string b1("bbbbb");
-	
+
 	assert (UTF8::icompare(a1, b1) < 0);
 
 	std::string a2("aaaaa");
 	std::string b2("BBBBB");
-	
+
 	assert (UTF8::icompare(a2, b2) < 0);
 
 	std::string a3("AAAAA");
 	std::string b3("bbbbb");
-	
+
 	assert (UTF8::icompare(a3, b3) < 0);
 
 	std::string a4("aaaaa");
 	std::string b4("AAAAA");
-	
+
 	assert (UTF8::icompare(a4, b4) == 0);
-	
+
 	std::string a5("AAAAA");
 	std::string b5("bbbbb");
-	
+
 	assert (UTF8::icompare(a5, b5) < 0);
 
 	std::string a6("\303\274\303\266\303\244"); // "u"o"a
 	std::string b6("\303\234\303\226\303\204"); // "U"O"A
-	
+
 	assert (UTF8::icompare(a6, b6) == 0);
 }
 
@@ -72,14 +72,14 @@ void UTF8StringTest::testTransform()
 	assert (s2 == "ABCDE123");
 
 	std::string s3("\303\274\303\266\303\244"); // "u"o"a
-	UTF8::toUpperInPlace(s3);	
+	UTF8::toUpperInPlace(s3);
 	assert (s3 == "\303\234\303\226\303\204"); // "U"O"A
 	UTF8::toLowerInPlace(s3);
 	assert (s3 == "\303\274\303\266\303\244"); // "u"o"a
 
 	// a mix of invalid sequences
 	std::string str = "\xC2\xE5\xF0\xF8\xE8\xED\xFB+-++";
-	assert ("???" == UTF8::toLower(str));
+	assert ("???+-++" == UTF8::toLower(str));
 }
 
 
