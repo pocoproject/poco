f9031e5e2906e921fce39d03ea7f6a0b4d4f0c91
Aleksandar Fabijanic <aleks-f@users.noreply.github.com>
2018-02-24 09:30:11 -0600

    add license header

--- PDF/src/BmpUtil.cpp
+++ PDF/src/BmpUtil.cpp
@@ -1,6 +1,15 @@
 //
 // BmpUtil.cpp
 //
+// Library: PDF
+// Package: PDFCore
+// Module:  BmpUtil
+//
+// Copyright (c) 2006, Applied Informatics Software Engineering GmbH.
+// and Contributors.
+//
+// SPDX-License-Identifier:	BSL-1.0
+//
 
 #include "Poco/PDF/BmpUtil.h"
 #include "bmpread.h"
