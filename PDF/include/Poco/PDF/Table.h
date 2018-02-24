18e11c95cf58e4b43012d6a127bd3c9589986aa2
Aleksandar Fabijanic <aleks-f@users.noreply.github.com>
2018-02-24 09:38:49 -0600

    Update Table.h

--- PDF/include/Poco/PDF/Table.h
+++ PDF/include/Poco/PDF/Table.h
@@ -1,6 +1,17 @@
 //
 // Table.h
 //
+// Library: PDF
+// Package: PDFCore
+// Module:  Table
+//
+// Definition of the Table class.
+//
+// Copyright (c) 2006, Applied Informatics Software Engineering GmbH.
+// and Contributors.
+//
+// SPDX-License-Identifier:	BSL-1.0
+//
 
 
 #ifndef PDF_Table_INCLUDED
