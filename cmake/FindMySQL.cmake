13a2a49640b6d3317bf28b58addee1def036333a
Steve Langasek <steve.langasek@ubuntu.com>
2018-02-28 22:38:04 +0100

    Find libmysqlclient instead of the obsolete libmysqlclient_r
    Bug-Debian: https://bugs.debian.org/889625

--- cmake/FindMySQL.cmake
+++ cmake/FindMySQL.cmake
@@ -50,7 +50,7 @@ if (WIN32)
 				 ${BINDIR32}/MySQL/*/lib/vs10
 				 $ENV{SystemDrive}/MySQL/*/lib/${libsuffixDist})
 else (WIN32)
-	find_library(MYSQL_LIB NAMES mysqlclient mysqlclient_r
+	find_library(MYSQL_LIB NAMES mysqlclient mysqlclient
 				 PATHS
 				 /usr/lib/mysql
 				 /usr/local/lib/mysql
