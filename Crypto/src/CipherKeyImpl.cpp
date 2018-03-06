e19f33351ddd630351f391a37ad98a5d03519bed
Günter Obiltschnig <guenter.obiltschnig@appinf.com>
2018-03-06 19:46:24 +0100

    add additional cipher modes

--- Crypto/src/CipherKeyImpl.cpp
+++ Crypto/src/CipherKeyImpl.cpp
@@ -25,8 +25,8 @@ namespace Poco {
 namespace Crypto {
 
 
-CipherKeyImpl::CipherKeyImpl(const std::string& name, 
-	const std::string& passphrase, 
+CipherKeyImpl::CipherKeyImpl(const std::string& name,
+	const std::string& passphrase,
 	const std::string& salt,
 	int iterationCount,
 	const std::string& digest):
@@ -54,8 +54,8 @@ CipherKeyImpl::CipherKeyImpl(const std::string& name,
 }
 
 
-CipherKeyImpl::CipherKeyImpl(const std::string& name, 
-	const ByteVec& key, 
+CipherKeyImpl::CipherKeyImpl(const std::string& name,
+	const ByteVec& key,
 	const ByteVec& iv):
 	_pCipher(0),
 	_pDigest(0),
@@ -71,7 +71,7 @@ CipherKeyImpl::CipherKeyImpl(const std::string& name,
 		throw Poco::NotFoundException("Cipher " + name + " was not found");
 }
 
-	
+
 CipherKeyImpl::CipherKeyImpl(const std::string& name):
 	_pCipher(0),
 	_pDigest(0),
@@ -114,6 +114,15 @@ CipherKeyImpl::Mode CipherKeyImpl::mode() const
 
 	case EVP_CIPH_OFB_MODE:
 		return MODE_OFB;
+
+	case EVP_CIPH_CTR_MODE:
+		return MODE_CTR;
+
+	case EVP_CIPH_GCM_MODE:
+		return MODE_GCM;
+
+	case EVP_CIPH_CCM_MODE:
+		return MODE_CCM;
 	}
 	throw Poco::IllegalStateException("Unexpected value of EVP_CIPHER_mode()");
 }
@@ -125,7 +134,7 @@ void CipherKeyImpl::generateKey()
 
 	getRandomBytes(vec, keySize());
 	setKey(vec);
-	
+
 	getRandomBytes(vec, ivSize());
 	setIV(vec);
 }
@@ -134,7 +143,7 @@ void CipherKeyImpl::generateKey()
 void CipherKeyImpl::getRandomBytes(ByteVec& vec, std::size_t count)
 {
 	Poco::RandomInputStream random;
-	
+
 	vec.clear();
 	vec.reserve(count);
 
