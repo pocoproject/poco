242a6b75161c92d6c6e6d1ab5f34b4e610ed84ee
Marian Krivos <marian.krivos@gmail.com>
2018-05-02 07:02:04 +0200

    Fixed warnings for JSON module

--- JSON/src/pdjson.c
+++ JSON/src/pdjson.c
@@ -158,7 +158,7 @@ static int pushchar(json_stream *json, int c)
             json->data.string = buffer;
         }
     }
-    json->data.string[json->data.string_fill++] = c;
+    json->data.string[json->data.string_fill++] = (char)(c);
     return 0;
 }
 
@@ -440,7 +440,7 @@ is_legal_utf8(const unsigned char *bytes, int length)
 static int
 read_utf8(json_stream* json, int next_char)
 {
-    int count = utf8_seq_length(next_char);
+    int count = utf8_seq_length((char)(next_char));
     if (!count)
     {
         json_error(json, "%s", "Bad character.");
@@ -448,10 +448,10 @@ read_utf8(json_stream* json, int next_char)
     }
 
     char buffer[4];
-    buffer[0] = next_char;
+    buffer[0] = (char)(next_char);
     for (int i = 1; i < count; ++i)
     {
-        buffer[i] = json->source.get(&json->source);;
+        buffer[i] = (char)(json->source.get(&json->source));
     }
 
     if (!is_legal_utf8((unsigned char*) buffer, count))
