cf1cca4d67ad0bcf343ce3c97549030bf8492af5
Francis ANDRE <francis.andre.kampbell@orange.fr>
2018-05-16 10:17:20 +0200

    Move cygserver.exe to appveyor/Cygwin/runtest.sh

--- appveyor/Cygwin/runtests.sh
+++ appveyor/Cygwin/runtests.sh
@@ -5,4 +5,5 @@ export PATH=$PATH:.
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:.
 source ./appveyor/ignored.sh
 source ./appveyor/Cygwin/excluded.sh
+/usr/sbin/cygserver.exe &
 build/script/runtests.sh
