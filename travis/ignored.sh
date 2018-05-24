48ba1f5a64a991655d0f8731482a6728d2aa72d1
Alex Fabijanic <alex@pocoproject.org>
2018-05-23 22:48:15 -0500

    add testMTU to ignore list

--- travis/ignored.sh
+++ travis/ignored.sh
@@ -2,4 +2,5 @@ export CPPUNIT_IGNORE="\
   N7CppUnit10TestCallerI8PathTestEE.testExpand \
   N7CppUnit10TestCallerI22HTTPSClientSessionTestEE.testProxy \
   N7CppUnit10TestCallerI22HTTPSStreamFactoryTestEE.testProxy \
+  N7CppUnit10TestCallerI14ICMPSocketTestEE.testMTU \
   N7CppUnit10TestCallerI9TimerTestEE.testTimer"
