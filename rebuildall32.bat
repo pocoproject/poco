6f9185ae62949d0e5f7368cb3e01552ed54bac3c
Francis ANDRE <francis.andre.kampbell@orange.fr>
2018-06-16 07:59:22 +0200

    Move rebuildall* to ProGen

--- rebuildall32.bat
+++
@@ -1,8 +0,0 @@
-setlocal EnableDelayedExpansion EnableExtensions        
-rem rd /q /s lib bin     & buildwin  90 rebuild all both Win32 samples tests devenv  > log\90.x32.log 2>&1
-rem rd /q /s lib bin     & buildwin 100 rebuild all both Win32 samples tests msbuild > log\100.x32.log 2>&1
-rd /q /s lib bin     & buildwin 110 rebuild all both Win32 samples tests msbuild > log\110.x32.log 2>&1
-rd /q /s lib bin     & buildwin 120 rebuild all both Win32 samples tests msbuild > log\120.x32.log 2>&1
-rd /q /s lib bin     & buildwin 140 rebuild all both Win32 samples tests msbuild > log\140.x32.log 2>&1
-rd /q /s lib bin     & buildwin 150 rebuild all both Win32 samples tests msbuild > log\150.x32.log 2>&1
-endlocal
--- rebuildall64.bat
+++
@@ -1,8 +0,0 @@
-setlocal EnableExtensions EnableDelayedExpansion
-rem rd /q /s lib64 bin64 & buildwin  90 rebuild all both x64   samples tests devenv  > log\90.x64.log 2>&1
-rd /q /s lib64 bin64 & buildwin 100 rebuild all both x64   samples tests msbuild > log\100.x64.log 2>&1
-rd /q /s lib64 bin64 & buildwin 110 rebuild all both x64   samples tests msbuild > log\110.x64.log 2>&1
-rd /q /s lib64 bin64 & buildwin 120 rebuild all both x64   samples tests msbuild > log\120.x64.log 2>&1
-rd /q /s lib64 bin64 & buildwin 140 rebuild all both x64   samples tests msbuild > log\140.x64.log 2>&1
-rd /q /s lib64 bin64 & buildwin 150 rebuild all both x64   samples tests msbuild > log\150.x64.log 2>&1
-endlocal
