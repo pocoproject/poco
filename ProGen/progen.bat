5dbb9cc3de7682730175b0d849cf3f8d22edc40b
Francis ANDRE <francis.andre.kampbell@orange.fr>
2018-06-20 09:47:40 +0200

    Regenerating VisualStudio projects

--- ProGen/progen.bat
+++ ProGen/progen.bat
@@ -1,45 +1,55 @@
-ProGen\bin\static_md\progen.exe CppUnit\CppUnit.progen
-ProGen\bin\static_md\progen.exe Foundation\Foundation.progen
-ProGen\bin\static_md\progen.exe Encodings\Encodings.progen
-ProGen\bin\static_md\progen.exe XML\XML.progen
-ProGen\bin\static_md\progen.exe JSON\JSON.progen
-ProGen\bin\static_md\progen.exe Util\Util.progen
-ProGen\bin\static_md\progen.exe Net\Net.progen
-ProGen\bin\static_md\progen.exe Crypto\Crypto.progen
-ProGen\bin\static_md\progen.exe NetSSL_OpenSSL\NetSSL_OpenSSL.progen
-ProGen\bin\static_md\progen.exe NetSSL_Win\NetSSL_Win.progen
-ProGen\bin\static_md\progen.exe Data\Data.progen
-ProGen\bin\static_md\progen.exe Data\SQLite\SQLite.progen
-ProGen\bin\static_md\progen.exe Data\ODBC\ODBC.progen
-ProGen\bin\static_md\progen.exe Data\MySQL\MySQL.progen
-ProGen\bin\static_md\progen.exe Data\PostgreSQL\PostgreSQL.progen
-ProGen\bin\static_md\progen.exe Zip\Zip.progen
-ProGen\bin\static_md\progen.exe PageCompiler\PageCompiler.progen
-ProGen\bin\static_md\progen.exe PageCompiler\File2Page\File2Page.progen
-ProGen\bin\static_md\progen.exe PDF\PDF.progen
-ProGen\bin\static_md\progen.exe CppParser\CppParser.progen
-ProGen\bin\static_md\progen.exe MongoDB\MongoDB.progen
-ProGen\bin\static_md\progen.exe Redis\Redis.progen
-ProGen\bin\static_md\progen.exe ProGen\ProGen.progen
+@echo off
+setlocal EnableDelayedExpansion EnableExtensions        
+if "%POCO_BASE%"=="" (
+	echo POCO_BASE not set
+	goto :exit
+)
+pushd %POCO_BASE%
+ProGen\bin\static_md\progen.exe  /platform=%1 CppUnit\CppUnit.progen
+ProGen\bin\static_md\progen.exe  /platform=%1 Foundation\Foundation.progen
+ProGen\bin\static_md\progen.exe  /platform=%1 Encodings\Encodings.progen
+ProGen\bin\static_md\progen.exe  /platform=%1 XML\XML.progen
+ProGen\bin\static_md\progen.exe  /platform=%1 JSON\JSON.progen
+ProGen\bin\static_md\progen.exe  /platform=%1 Util\Util.progen
+ProGen\bin\static_md\progen.exe  /platform=%1 Net\Net.progen
+ProGen\bin\static_md\progen.exe  /platform=%1 Crypto\Crypto.progen
+ProGen\bin\static_md\progen.exe  /platform=%1 NetSSL_OpenSSL\NetSSL_OpenSSL.progen
+ProGen\bin\static_md\progen.exe  /platform=%1 NetSSL_Win\NetSSL_Win.progen
+ProGen\bin\static_md\progen.exe  /platform=%1 Data\Data.progen
+ProGen\bin\static_md\progen.exe  /platform=%1 Data\SQLite\SQLite.progen
+ProGen\bin\static_md\progen.exe  /platform=%1 Data\ODBC\ODBC.progen
+ProGen\bin\static_md\progen.exe  /platform=%1 Data\MySQL\MySQL.progen
+ProGen\bin\static_md\progen.exe  /platform=%1 Data\PostgreSQL\PostgreSQL.progen
+ProGen\bin\static_md\progen.exe  /platform=%1 Zip\Zip.progen
+ProGen\bin\static_md\progen.exe  /platform=%1 PageCompiler\PageCompiler.progen
+ProGen\bin\static_md\progen.exe  /platform=%1 PageCompiler\File2Page\File2Page.progen
+ProGen\bin\static_md\progen.exe  /platform=%1 PDF\PDF.progen
+ProGen\bin\static_md\progen.exe  /platform=%1 CppParser\CppParser.progen
+ProGen\bin\static_md\progen.exe  /platform=%1 MongoDB\MongoDB.progen
+ProGen\bin\static_md\progen.exe  /platform=%1 Redis\Redis.progen
+ProGen\bin\static_md\progen.exe  /platform=%1 ProGen\ProGen.progen
 
 
-ProGen\bin\static_md\progen.exe Foundation\testsuite\TestSuite.progen
-ProGen\bin\static_md\progen.exe Encodings\testsuite\TestSuite.progen
-ProGen\bin\static_md\progen.exe XML\testsuite\TestSuite.progen
-ProGen\bin\static_md\progen.exe JSON\testsuite\TestSuite.progen
-ProGen\bin\static_md\progen.exe Util\testsuite\TestSuite.progen
-ProGen\bin\static_md\progen.exe Net\testsuite\TestSuite.progen
-ProGen\bin\static_md\progen.exe Crypto\testsuite\TestSuite.progen
-ProGen\bin\static_md\progen.exe NetSSL_OpenSSL\testsuite\TestSuite.progen
-ProGen\bin\static_md\progen.exe NetSSL_Win\testsuite\TestSuite.progen
-ProGen\bin\static_md\progen.exe Data\testsuite\TestSuite.progen
-ProGen\bin\static_md\progen.exe Data\SQLite\testsuite\TestSuite.progen
-ProGen\bin\static_md\progen.exe Data\ODBC\testsuite\TestSuite.progen
-ProGen\bin\static_md\progen.exe Data\MySQL\testsuite\TestSuite.progen
-ProGen\bin\static_md\progen.exe Data\PostgreSQL\testsuite\TestSuite.progen
-ProGen\bin\static_md\progen.exe Zip\testsuite\TestSuite.progen
-ProGen\bin\static_md\progen.exe PageCompiler\testsuite\TestSuite.progen
-ProGen\bin\static_md\progen.exe PDF\testsuite\TestSuite.progen
-ProGen\bin\static_md\progen.exe CppParser\testsuite\TestSuite.progen
-ProGen\bin\static_md\progen.exe MongoDB\testsuite\TestSuite.progen
-ProGen\bin\static_md\progen.exe Redis\testsuite\TestSuite.progen
+ProGen\bin\static_md\progen.exe  /platform=%1 Foundation\testsuite\TestSuite.progen
+ProGen\bin\static_md\progen.exe  /platform=%1 Encodings\testsuite\TestSuite.progen
+ProGen\bin\static_md\progen.exe  /platform=%1 XML\testsuite\TestSuite.progen
+ProGen\bin\static_md\progen.exe  /platform=%1 JSON\testsuite\TestSuite.progen
+ProGen\bin\static_md\progen.exe  /platform=%1 Util\testsuite\TestSuite.progen
+ProGen\bin\static_md\progen.exe  /platform=%1 Net\testsuite\TestSuite.progen
+ProGen\bin\static_md\progen.exe  /platform=%1 Crypto\testsuite\TestSuite.progen
+ProGen\bin\static_md\progen.exe  /platform=%1 NetSSL_OpenSSL\testsuite\TestSuite.progen
+ProGen\bin\static_md\progen.exe  /platform=%1 NetSSL_Win\testsuite\TestSuite.progen
+ProGen\bin\static_md\progen.exe  /platform=%1 Data\testsuite\TestSuite.progen
+ProGen\bin\static_md\progen.exe  /platform=%1 Data\SQLite\testsuite\TestSuite.progen
+ProGen\bin\static_md\progen.exe  /platform=%1 Data\ODBC\testsuite\TestSuite.progen
+ProGen\bin\static_md\progen.exe  /platform=%1 Data\MySQL\testsuite\TestSuite.progen
+ProGen\bin\static_md\progen.exe  /platform=%1 Data\PostgreSQL\testsuite\TestSuite.progen
+ProGen\bin\static_md\progen.exe  /platform=%1 Zip\testsuite\TestSuite.progen
+ProGen\bin\static_md\progen.exe  /platform=%1 PageCompiler\testsuite\TestSuite.progen
+ProGen\bin\static_md\progen.exe  /platform=%1 PDF\testsuite\TestSuite.progen
+ProGen\bin\static_md\progen.exe  /platform=%1 CppParser\testsuite\TestSuite.progen
+ProGen\bin\static_md\progen.exe  /platform=%1 MongoDB\testsuite\TestSuite.progen
+ProGen\bin\static_md\progen.exe  /platform=%1 Redis\testsuite\TestSuite.progen
+popd
+:exit
+endlocal
--- ProGen/rebuildall64.bat
+++ ProGen/rebuildall64.bat
@@ -1,8 +1,19 @@
+@echo off
 setlocal EnableExtensions EnableDelayedExpansion
-rd /q /s ..\lib64 ..\bin64 & call buildwin  90 ..\rebuild all bot..\h x64   samples tests devenv  > log\9..\0.x64.log 2>&1
-rd /q /s ..\lib64 ..\bin64 & call buildwin 100 ..\rebuild all bot..\h x64   samples tests msbuild > log\10..\0.x64.log 2>&1
-rd /q /s ..\lib64 ..\bin64 & call buildwin 110 ..\rebuild all bot..\h x64   samples tests msbuild > log\11..\0.x64.log 2>&1
-rd /q /s ..\lib64 ..\bin64 & call buildwin 120 ..\rebuild all bot..\h x64   samples tests msbuild > log\12..\0.x64.log 2>&1
-rd /q /s ..\lib64 ..\bin64 & call buildwin 140 ..\rebuild all bot..\h x64   samples tests msbuild > log\14..\0.x64.log 2>&1
-rd /q /s ..\lib64 ..\bin64 & call buildwin 150 ..\rebuild all bot..\h x64   samples tests msbuild > log\15..\0.x64.log 2>&1
+if "%POCO_BASE%"=="" (
+	echo POCO_BASE not set
+	goto :exit
+)
+pushd %POCO_BASE%
+mkdir ProGen\log
+
+call ProGen\progen x64 >ProGen\log\progen64.log 2>&1
+rd /q /s lib64 bin64 & call buildwin  90 rebuild all both x64   samples tests devenv  > ProGen\log\90.x64.log 2>&1
+rd /q /s lib64 bin64 & call buildwin 100 rebuild all both x64   samples tests msbuild > ProGen\log\100.x64.log 2>&1
+rd /q /s lib64 bin64 & call buildwin 110 rebuild all both x64   samples tests msbuild > ProGen\log\110.x64.log 2>&1
+rd /q /s lib64 bin64 & call buildwin 120 rebuild all both x64   samples tests msbuild > ProGen\log\120.x64.log 2>&1
+rd /q /s lib64 bin64 & call buildwin 140 rebuild all both x64   samples tests msbuild > ProGen\log\140.x64.log 2>&1
+rd /q /s lib64 bin64 & call buildwin 150 rebuild all both x64   samples tests msbuild > ProGen\log\150.x64.log 2>&1
+popd
+:exit
 endlocal
