06d984673c846e053d9393616b993bfcda17d295
dudym <dudy.margalit@gmail.com>
2018-08-13 23:51:02 +1000

    Added missing IPv6 methods to SecureSocketImpl (#2417)

--- NetSSL_OpenSSL/src/SecureServerSocketImpl.cpp
+++ NetSSL_OpenSSL/src/SecureServerSocketImpl.cpp
@@ -68,7 +68,12 @@ void SecureServerSocketImpl::bind(const SocketAddress& address, bool reuseAddres
 	reset(_impl.sockfd());
 }
 
-	
+void SecureServerSocketImpl::bind6(const SocketAddress& address, bool reuseAddress, bool ipV6Only)
+{
+	_impl.bind6(address, reuseAddress, ipV6Only);
+	reset(_impl.sockfd());
+}
+
 void SecureServerSocketImpl::listen(int backlog)
 {
 	_impl.listen(backlog);
