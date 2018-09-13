fe8bda7720b45d8eba5d07cac243c798f025c243
Francis ANDRE <francis.andre.kampbell@orange.fr>
2018-09-13 08:26:00 +0200

    Fix for issue #2462 by bakkiraju@logitech.com
    QNX7.0 seems to be using header <sys/statvfs.h> instead of <sys/statfs.h>

--- Foundation/src/File_UNIX.cpp
+++ Foundation/src/File_UNIX.cpp
@@ -23,7 +23,7 @@
 #if defined(POCO_OS_FAMILY_BSD)
 #include <sys/param.h>
 #include <sys/mount.h>
-#elif (POCO_OS == POCO_OS_SOLARIS)
+#elif (POCO_OS == POCO_OS_SOLARIS) || (POCO_OS == POCO_OS_QNX)
 #include <sys/statvfs.h>
 #else
 #include <sys/statfs.h>
