e9fcf42693b3ef2252eadbde820a2190a7fc642a
Jörg-Christian Böhme <joerg@chaosdorf.de>
2018-12-28 11:31:54 +0100

    Add ChannelTest::testAsync to cppunit ignore

--- Foundation/testsuite/src/ChannelTest.cpp
+++ Foundation/testsuite/src/ChannelTest.cpp
@@ -70,7 +70,6 @@ void ChannelTest::testSplitter()
 void ChannelTest::testAsync()
 {
 	AutoPtr<TestChannel> pChannel = new TestChannel;
-#ifndef POCO_OS_FAMILY_WINDOWS
 	AutoPtr<AsyncChannel> pAsync = new AsyncChannel(pChannel); // FIXME It is crashing here on Windows!
 	pAsync->open();
 	Message msg;
@@ -78,7 +77,6 @@ void ChannelTest::testAsync()
 	pAsync->log(msg);
 	pAsync->close();
 	assertTrue (pChannel->list().size() == 2);
-#endif
 }
 
 
