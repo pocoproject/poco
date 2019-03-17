b3e25e5ce252e75cfc30f33084e31d4d49a30001
Vitaly Baranov <vitbar@yandex-team.ru>
2019-03-17 22:29:45 +0300

    Implement DataURIStream for extracting data from data URIs.

---
+++ Foundation/include/Poco/DataURIStream.h
@@ -0,0 +1,78 @@
+﻿//
+// DataURIStream.h
+//
+// Library: Foundation
+// Package: Streams
+// Module:  DataURIStreamFactory
+//
+// Definition of the DataURIStream class.
+//
+// Copyright (c) 2019, Applied Informatics Software Engineering GmbH.
+// and Contributors.
+//
+// SPDX-License-Identifier:	BSL-1.0
+//
+
+
+#ifndef Foundation_DataURIStream_INCLUDED
+#define Foundation_DataURIStream_INCLUDED
+
+
+#include "Poco/Foundation.h"
+#include <istream>
+#include <memory>
+
+
+namespace Poco {
+
+
+class Base64Decoder;
+class MemoryInputStream;
+class URI;
+
+
+class Foundation_API DataURIStreamIOS: public virtual std::ios
+	/// The base class for DataURIStream.
+	///
+	/// This class is needed to ensure the correct initialization
+	/// order of the stream buffer and base classes.
+{
+public:
+	DataURIStreamIOS(const URI& uri);
+	~DataURIStreamIOS();
+	std::streambuf* rdbuf();
+
+protected:
+	std::streambuf* _buf;
+
+private:
+	DataURIStreamIOS(const DataURIStreamIOS&);
+	DataURIStreamIOS& operator = (const DataURIStreamIOS&);
+	std::string _data;
+	std::unique_ptr<MemoryInputStream> _memoryStream;
+	std::unique_ptr<Base64Decoder> _base64Decoder;
+};
+
+
+class Foundation_API DataURIStream: public DataURIStreamIOS, public std::istream
+	/// An input stream for reading data from a data URI.
+	/// For example, when constructed from  "data:text/plain;base64,SGVsbG8sIFdvcmxkIQ%3D%3D" it reads "Hello, World!".
+{
+public:
+	DataURIStream(const URI& uri);
+		/// Creates a DataURIStream for the given data URI,
+		/// ready for reading data.
+		/// Throws a DataFormatException exception if the data is incorrect format.
+	~DataURIStream();
+		/// Destroys the DataURIStream.
+
+private:
+	DataURIStream(const DataURIStream&);
+	DataURIStream& operator = (const DataURIStream&);
+};
+
+
+} // namespace Poco
+
+
+#endif // Foundation_DataURIStream_INCLUDED
---
+++ Foundation/src/DataURIStream.cpp
@@ -0,0 +1,72 @@
+﻿//
+// DataURIStream.cpp
+//
+// Library: Foundation
+// Package: Streams
+// Module:  DataURIStreamFactory
+//
+// Copyright (c) 2019, Applied Informatics Software Engineering GmbH.
+// and Contributors.
+//
+// SPDX-License-Identifier:	BSL-1.0
+//
+
+
+#include "Poco/DataURIStream.h"
+#include "Poco/Base64Decoder.h"
+#include "Poco/Exception.h"
+#include "Poco/MemoryStream.h"
+#include "Poco/Path.h"
+#include "Poco/StreamUtil.h"
+#include "Poco/URI.h"
+#include <string.h>
+
+
+namespace Poco {
+
+
+DataURIStreamIOS::DataURIStreamIOS(const URI& uri)
+{
+	poco_assert (uri.getScheme() == "data");
+
+	const std::string& path = uri.getPath();
+	size_t comma = path.find(',');
+	if (comma == std::string::npos)
+		throw DataFormatException();
+	_data = path.substr(comma + 1);
+	_memoryStream.reset(new MemoryInputStream(_data.data(), _data.length()));
+	constexpr char base64[] = ";base64";
+	const size_t base64Len = strlen(base64);
+	if ((comma >= base64Len) && !path.compare(comma - base64Len, base64Len, base64, base64Len))
+	{
+		_base64Decoder.reset(new Base64Decoder(*_memoryStream, 0));
+		_buf = _base64Decoder->rdbuf();
+	}
+	else
+		_buf = _memoryStream->rdbuf();
+	poco_ios_init(_buf);
+}
+
+
+DataURIStreamIOS::~DataURIStreamIOS()
+{
+}
+
+
+std::streambuf* DataURIStreamIOS::rdbuf()
+{
+	return _buf;
+}
+
+
+DataURIStream::DataURIStream(const URI& uri): DataURIStreamIOS(uri), std::istream(_buf)
+{
+}
+
+
+DataURIStream::~DataURIStream()
+{
+}
+
+
+} // namespace Poco
