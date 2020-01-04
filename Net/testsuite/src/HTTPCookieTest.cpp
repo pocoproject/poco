243a5dd8f83ab32b5b60ac1784189a9bcdfbab25
Günter Obiltschnig <guenter.obiltschnig@appinf.com>
2020-01-04 10:15:41 +0100

    added support for SameSite cookie attribute
    (cherry picked from commit 4ec5d35060b3d4eae1a31dfb27fb2f4bd0fd94df)
    
    Conflicts:
    	Net/testsuite/src/HTTPCookieTest.cpp

--- Net/testsuite/src/HTTPCookieTest.cpp
+++ Net/testsuite/src/HTTPCookieTest.cpp
@@ -70,11 +70,11 @@ void HTTPCookieTest::testCookie()
 
 	cookie.setVersion(1);
 	assert (cookie.toString() == "name=\"value\"; Comment=\"comment\"; Domain=\"appinf.com\"; Path=\"/\"; secure; Version=\"1\"");
-	
+
 	cookie.setSecure(false);
 	cookie.setMaxAge(100);
 	assert (cookie.toString() == "name=\"value\"; Comment=\"comment\"; Domain=\"appinf.com\"; Path=\"/\"; Max-Age=\"100\"; Version=\"1\"");
-	
+
 	cookie.setHttpOnly(true);
 	assert (cookie.toString() == "name=\"value\"; Comment=\"comment\"; Domain=\"appinf.com\"; Path=\"/\"; Max-Age=\"100\"; HttpOnly; Version=\"1\"");
 
@@ -83,7 +83,17 @@ void HTTPCookieTest::testCookie()
 	cookie.setPriority("Medium");
 	assert (cookie.toString() == "name=\"value\"; Comment=\"comment\"; Domain=\"appinf.com\"; Path=\"/\"; Priority=\"Medium\"; Max-Age=\"100\"; HttpOnly; Version=\"1\"");
 	cookie.setPriority("High");
-	assert (cookie.toString() == "name=\"value\"; Comment=\"comment\"; Domain=\"appinf.com\"; Path=\"/\"; Priority=\"High\"; Max-Age=\"100\"; HttpOnly; Version=\"1\"");	
+	assert (cookie.toString() == "name=\"value\"; Comment=\"comment\"; Domain=\"appinf.com\"; Path=\"/\"; Priority=\"High\"; Max-Age=\"100\"; HttpOnly; Version=\"1\"");
+
+	cookie.setPriority("");
+	cookie.setSameSite(HTTPCookie::SAME_SITE_NONE);
+	assert (cookie.toString() == "name=\"value\"; Comment=\"comment\"; Domain=\"appinf.com\"; Path=\"/\"; Max-Age=\"100\"; SameSite=None; HttpOnly; Version=\"1\"");
+	cookie.setSameSite(HTTPCookie::SAME_SITE_LAX);
+	assert (cookie.toString() == "name=\"value\"; Comment=\"comment\"; Domain=\"appinf.com\"; Path=\"/\"; Max-Age=\"100\"; SameSite=Lax; HttpOnly; Version=\"1\"");
+	cookie.setSameSite(HTTPCookie::SAME_SITE_STRICT);
+	assert (cookie.toString() == "name=\"value\"; Comment=\"comment\"; Domain=\"appinf.com\"; Path=\"/\"; Max-Age=\"100\"; SameSite=Strict; HttpOnly; Version=\"1\"");
+	cookie.setSameSite(HTTPCookie::SAME_SITE_NOT_SPECIFIED);
+	assert (cookie.toString() == "name=\"value\"; Comment=\"comment\"; Domain=\"appinf.com\"; Path=\"/\"; Max-Age=\"100\"; HttpOnly; Version=\"1\"");
 }
 
 
