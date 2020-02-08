3629a896366b188871c471b41a74b2f78131e8b1
Günter Obiltschnig <guenter.obiltschnig@appinf.com>
2020-02-08 10:49:42 +0100

    fix tests

--- travis/runtests.sh
+++ travis/runtests.sh
@@ -1,6 +1,8 @@
 set -ev
+osname=`uname`
+osarch=`uname -m`
 export POCO_BASE=`pwd`
 export EXCLUDE_TESTS="Data/MySQL Data/ODBC MongoDB PDF"
 export PATH=$PATH:.
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:.
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:.:$POCO_BASE/lib/$osname/$osarch
 build/script/runtests.sh
