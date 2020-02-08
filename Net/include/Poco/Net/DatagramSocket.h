137c6ad136cc743edd6b457e4ac3c820cdcd3640
Günter Obiltschnig <guenter.obiltschnig@appinf.com>
2020-02-08 16:29:01 +0100

    clarify documentation of flags parameter

--- Net/include/Poco/Net/DatagramSocket.h
+++ Net/include/Poco/Net/DatagramSocket.h
@@ -69,7 +69,7 @@ public:
 		///
 		/// Releases the socket's SocketImpl and
 		/// attaches the SocketImpl from the other socket and
-		/// increments the reference count of the SocketImpl.	
+		/// increments the reference count of the SocketImpl.
 
 	void connect(const SocketAddress& address);
 		/// Restricts incoming and outgoing
@@ -81,7 +81,7 @@ public:
 		/// Bind a local address to the socket.
 		///
 		/// This is usually only done when establishing a server
-		/// socket. 
+		/// socket.
 		///
 		/// If reuseAddress is true, sets the SO_REUSEADDR
 		/// socket option.
@@ -92,7 +92,7 @@ public:
 		/// Bind a local address to the socket.
 		///
 		/// This is usually only done when establishing a server
-		/// socket. 
+		/// socket.
 		///
 		/// If reuseAddress is true, sets the SO_REUSEADDR
 		/// socket option.
@@ -108,6 +108,9 @@ public:
 		///
 		/// Returns the number of bytes sent, which may be
 		/// less than the number of bytes specified.
+		///
+		/// The flags parameter can be used to pass system-defined flags
+		/// for send() like MSG_DONTROUTE.
 
 	int sendBytes(const SocketBufVec& buffer, int flags = 0);
 		/// Sends the contents of the given buffers through
@@ -115,22 +118,34 @@ public:
 		///
 		/// Returns the number of bytes sent, which may be
 		/// less than the number of bytes specified.
+		///
+		/// The flags parameter can be used to pass system-defined flags
+		/// for send() like MSG_DONTROUTE.
 
 	int receiveBytes(void* buffer, int length, int flags = 0);
 		/// Receives data from the socket and stores it
 		/// in buffer. Up to length bytes are received.
 		///
 		/// Returns the number of bytes received.
+		///
+		/// The flags parameter can be used to pass system-defined flags
+		/// for recv() like MSG_PEEK.
 
 	int receiveBytes(SocketBufVec& buffer, int flags = 0);
 		/// Receives data from the socket and stores it in buffers.
 		///
 		/// Returns the number of bytes received.
+		///
+		/// The flags parameter can be used to pass system-defined flags
+		/// for recv() like MSG_PEEK.
 
 	int receiveBytes(Poco::Buffer<char>& buffer, int flags = 0, const Poco::Timespan& timeout = 100000);
 		/// Receives data from the socket and stores it in buffers.
 		///
 		/// Returns the number of bytes received.
+		///
+		/// The flags parameter can be used to pass system-defined flags
+		/// for recv() like MSG_PEEK.
 
 	int sendTo(const void* buffer, int length, const SocketAddress& address, int flags = 0);
 		/// Sends the contents of the given buffer through
@@ -138,6 +153,9 @@ public:
 		///
 		/// Returns the number of bytes sent, which may be
 		/// less than the number of bytes specified.
+		///
+		/// The flags parameter can be used to pass system-defined flags
+		/// for sendto() like MSG_DONTROUTE.
 
 	int sendTo(const SocketBufVec& buffers, const SocketAddress& address, int flags = 0);
 		/// Sends the contents of the given buffers through
@@ -145,6 +163,9 @@ public:
 		///
 		/// Returns the number of bytes sent, which may be
 		/// less than the number of bytes specified.
+		///
+		/// The flags parameter can be used to pass system-defined flags
+		/// for sendto() like MSG_DONTROUTE.
 
 	int receiveFrom(void* buffer, int length, SocketAddress& address, int flags = 0);
 		/// Receives data from the socket and stores it
@@ -152,6 +173,9 @@ public:
 		/// Stores the address of the sender in address.
 		///
 		/// Returns the number of bytes received.
+		///
+		/// The flags parameter can be used to pass system-defined flags
+		/// for recvfrom() like MSG_PEEK.
 
 	int receiveFrom(void* buffer, int length, struct sockaddr** ppSA, poco_socklen_t** ppSALen, int flags = 0);
 		/// Receives data from the socket and stores it
@@ -160,6 +184,9 @@ public:
 		/// ppSA, and the length of native address in ppSALen.
 		///
 		/// Returns the number of bytes received.
+		///
+		/// The flags parameter can be used to pass system-defined flags
+		/// for recvfrom() like MSG_PEEK.
 
 	int receiveFrom(SocketBufVec& buffers, SocketAddress& address, int flags = 0);
 		/// Receives data from the socket and stores it
@@ -168,6 +195,9 @@ public:
 		/// Stores the address of the sender in address.
 		///
 		/// Returns the number of bytes received.
+		///
+		/// The flags parameter can be used to pass system-defined flags
+		/// for recvfrom() like MSG_PEEK.
 
 	int receiveFrom(SocketBufVec& buffers, struct sockaddr** ppSA, poco_socklen_t** ppSALen, int flags = 0);
 		/// Receives data from the socket and stores it
@@ -176,13 +206,16 @@ public:
 		/// ppSA, and the length of native address in ppSALen.
 		///
 		/// Returns the number of bytes received.
+		///
+		/// The flags parameter can be used to pass system-defined flags
+		/// for recvfrom() like MSG_PEEK.
 
 	void setBroadcast(bool flag);
 		/// Sets the value of the SO_BROADCAST socket option.
 		///
 		/// Setting this flag allows sending datagrams to
 		/// the broadcast address.
-	
+
 	bool getBroadcast() const;
 		/// Returns the value of the SO_BROADCAST socket option.
 
