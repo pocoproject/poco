8afb1a06e1e2d7ec6414d241a7e1ceb34d3eccb2
Anton Barabanov <beckpost@yandex.ru>
2020-04-28 18:59:57 +0300

    Fix invalid request parsing. Problem is in case, when buffer stops reading in the last symbol of the line. Next time it will read '\r\n' sequence and interpret it as an empty line and set _end to true.

--- Net/src/HTTPHeaderStream.cpp
+++ Net/src/HTTPHeaderStream.cpp
@@ -28,7 +28,8 @@ namespace Net {
 HTTPHeaderStreamBuf::HTTPHeaderStreamBuf(HTTPSession& session, openmode mode):
 	HTTPBasicStreamBuf(HTTPBufferAllocator::BUFFER_SIZE, mode),
 	_session(session),
-	_end(false)
+	_end(false),
+	_line_ended(true)
 {
 }
 
@@ -55,8 +56,9 @@ int HTTPHeaderStreamBuf::readFromDevice(char* buffer, std::streamsize length)
 	if (ch != eof)
 	{
 		*buffer++ = (char) ch; ++n;
-		if (n == 2) _end = true;
+		if (n == 2) _end = _line_ended;
 	}
+	_line_ended = (ch == '\n');
 	return n;
 }
 
