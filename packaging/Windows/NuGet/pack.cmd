e897c8604b6dfffff89be3f442fe4be885bbeb01
Francis ANDRE <francis.andre.kampbell@orange.fr>
2020-12-19 18:16:14 +0100

    reverse parameter order

--- packaging/Windows/NuGet/pack.cmd
+++ packaging/Windows/NuGet/pack.cmd
@@ -7,68 +7,68 @@ rem echo ------
 setlocal
 if "%2"=="" goto usage
 if "%1"=="" goto usage
-nuget pack Pocoproject.Poco.shared.nuspec -symbols -symbolpackageformat snupkg	-p version=%2 -p vs=vs%1 -p id=CppParser
-nuget pack Pocoproject.Poco.shared.nuspec -symbols -symbolpackageformat snupkg	-p version=%2 -p vs=vs%1 -p id=Crypto
-nuget pack Pocoproject.Poco.shared.nuspec -symbols -symbolpackageformat snupkg	-p version=%2 -p vs=vs%1 -p id=Data
-nuget pack Pocoproject.Poco.shared.nuspec -symbols -symbolpackageformat snupkg	-p version=%2 -p vs=vs%1 -p id=DataSQLite
-nuget pack Pocoproject.Poco.shared.nuspec -symbols -symbolpackageformat snupkg	-p version=%2 -p vs=vs%1 -p id=DataMySQL
-nuget pack Pocoproject.Poco.shared.nuspec -symbols -symbolpackageformat snupkg	-p version=%2 -p vs=vs%1 -p id=DataODBC
-nuget pack Pocoproject.Poco.shared.nuspec -symbols -symbolpackageformat snupkg	-p version=%2 -p vs=vs%1 -p id=DataPostgreSQL
-nuget pack Pocoproject.Poco.shared.nuspec -symbols -symbolpackageformat snupkg	-p version=%2 -p vs=vs%1 -p id=Encodings
-nuget pack Pocoproject.Poco.shared.nuspec -symbols -symbolpackageformat snupkg	-p version=%2 -p vs=vs%1 -p id=Foundation
-nuget pack Pocoproject.Poco.shared.nuspec -symbols -symbolpackageformat snupkg	-p version=%2 -p vs=vs%1 -p id=JSON
-nuget pack Pocoproject.Poco.shared.nuspec -symbols -symbolpackageformat snupkg	-p version=%2 -p vs=vs%1 -p id=JWT
-nuget pack Pocoproject.Poco.shared.nuspec -symbols -symbolpackageformat snupkg	-p version=%2 -p vs=vs%1 -p id=MongoDB
-nuget pack Pocoproject.Poco.shared.nuspec -symbols -symbolpackageformat snupkg	-p version=%2 -p vs=vs%1 -p id=Net
-nuget pack Pocoproject.Poco.shared.nuspec -symbols -symbolpackageformat snupkg	-p version=%2 -p vs=vs%1 -p id=NetSSL
-nuget pack Pocoproject.Poco.shared.nuspec -symbols -symbolpackageformat snupkg	-p version=%2 -p vs=vs%1 -p id=NetSSLWin
-nuget pack Pocoproject.Poco.shared.nuspec -symbols -symbolpackageformat snupkg	-p version=%2 -p vs=vs%1 -p id=PDF
-nuget pack Pocoproject.Poco.shared.nuspec 										-p version=%2 -p vs=vs%1 -p id=PageCompiler
-nuget pack Pocoproject.Poco.shared.nuspec -symbols -symbolpackageformat snupkg	-p version=%2 -p vs=vs%1 -p id=Redis
-nuget pack Pocoproject.Poco.shared.nuspec -symbols -symbolpackageformat snupkg	-p version=%2 -p vs=vs%1 -p id=Util
-nuget pack Pocoproject.Poco.shared.nuspec -symbols -symbolpackageformat snupkg	-p version=%2 -p vs=vs%1 -p id=XML
-nuget pack Pocoproject.Poco.shared.nuspec -symbols -symbolpackageformat snupkg	-p version=%2 -p vs=vs%1 -p id=Zip
-nuget pack Pocoproject.Poco.noarch.nuspec -p version=%2 -p vs=vs%1 -p id=CppParser 			-p inc=CppParser
-nuget pack Pocoproject.Poco.noarch.nuspec -p version=%2 -p vs=vs%1 -p id=Crypto 			-p inc=Crypto
-nuget pack Pocoproject.Poco.noarch.nuspec -p version=%2 -p vs=vs%1 -p id=Data 				-p inc=Data
-nuget pack Pocoproject.Poco.noarch.nuspec -p version=%2 -p vs=vs%1 -p id=DataSQLite 		-p inc="Data\SQLite"
-nuget pack Pocoproject.Poco.noarch.nuspec -p version=%2 -p vs=vs%1 -p id=DataMySQL 			-p inc="Data\MySQL"
-nuget pack Pocoproject.Poco.noarch.nuspec -p version=%2 -p vs=vs%1 -p id=DataODBC 			-p inc="Data\ODBC"
-nuget pack Pocoproject.Poco.noarch.nuspec -p version=%2 -p vs=vs%1 -p id=DataPostgreSQL		-p inc="Data\PostgreSQL"
-nuget pack Pocoproject.Poco.noarch.nuspec -p version=%2 -p vs=vs%1 -p id=Encodings 			-p inc=Encodings
-nuget pack Pocoproject.Poco.noarch.nuspec -p version=%2 -p vs=vs%1 -p id=Foundation 		-p inc=Foundation
-nuget pack Pocoproject.Poco.noarch.nuspec -p version=%2 -p vs=vs%1 -p id=JSON 				-p inc=JSON
-nuget pack Pocoproject.Poco.noarch.nuspec -p version=%2 -p vs=vs%1 -p id=JWT 				-p inc=JWT
-nuget pack Pocoproject.Poco.noarch.nuspec -p version=%2 -p vs=vs%1 -p id=MongoDB 			-p inc=MongoDB
-nuget pack Pocoproject.Poco.noarch.nuspec -p version=%2 -p vs=vs%1 -p id=Net 				-p inc=Net
-nuget pack Pocoproject.Poco.noarch.nuspec -p version=%2 -p vs=vs%1 -p id=NetSSL 			-p inc=NetSSL_OpenSSL
-nuget pack Pocoproject.Poco.noarch.nuspec -p version=%2 -p vs=vs%1 -p id=NetSSLWin 			-p inc=NetSSL_Win
-nuget pack Pocoproject.Poco.noarch.nuspec -p version=%2 -p vs=vs%1 -p id=PDF 				-p inc=PDF
-nuget pack Pocoproject.Poco.noarch.nuspec -p version=%2 -p vs=vs%1 -p id=Redis 				-p inc=Redis
-nuget pack Pocoproject.Poco.noarch.nuspec -p version=%2 -p vs=vs%1 -p id=Util 				-p inc=Util
-nuget pack Pocoproject.Poco.noarch.nuspec -p version=%2 -p vs=vs%1 -p id=XML 				-p inc=XML
-nuget pack Pocoproject.Poco.noarch.nuspec -p version=%2 -p vs=vs%1 -p id=Zip 				-p inc=Zip
-nuget pack Pocoproject.Poco.static.nuspec -symbols -symbolpackageformat snupkg	-p version=%2 -p vs=vs%1 -p id=CppParser
-nuget pack Pocoproject.Poco.static.nuspec -symbols -symbolpackageformat snupkg	-p version=%2 -p vs=vs%1 -p id=Crypto
-nuget pack Pocoproject.Poco.static.nuspec -symbols -symbolpackageformat snupkg	-p version=%2 -p vs=vs%1 -p id=Data
-nuget pack Pocoproject.Poco.static.nuspec -symbols -symbolpackageformat snupkg	-p version=%2 -p vs=vs%1 -p id=DataSQLite
-nuget pack Pocoproject.Poco.static.nuspec -symbols -symbolpackageformat snupkg	-p version=%2 -p vs=vs%1 -p id=DataMySQL
-nuget pack Pocoproject.Poco.static.nuspec -symbols -symbolpackageformat snupkg	-p version=%2 -p vs=vs%1 -p id=DataODBC
-nuget pack Pocoproject.Poco.static.nuspec -symbols -symbolpackageformat snupkg	-p version=%2 -p vs=vs%1 -p id=DataPostgreSQL
-nuget pack Pocoproject.Poco.static.nuspec -symbols -symbolpackageformat snupkg	-p version=%2 -p vs=vs%1 -p id=Encodings
-nuget pack Pocoproject.Poco.static.nuspec -symbols -symbolpackageformat snupkg	-p version=%2 -p vs=vs%1 -p id=Foundation
-nuget pack Pocoproject.Poco.static.nuspec -symbols -symbolpackageformat snupkg	-p version=%2 -p vs=vs%1 -p id=JSON
-nuget pack Pocoproject.Poco.static.nuspec -symbols -symbolpackageformat snupkg	-p version=%2 -p vs=vs%1 -p id=JWT
-nuget pack Pocoproject.Poco.static.nuspec -symbols -symbolpackageformat snupkg	-p version=%2 -p vs=vs%1 -p id=MongoDB
-nuget pack Pocoproject.Poco.static.nuspec -symbols -symbolpackageformat snupkg	-p version=%2 -p vs=vs%1 -p id=Net
-nuget pack Pocoproject.Poco.static.nuspec -symbols -symbolpackageformat snupkg	-p version=%2 -p vs=vs%1 -p id=NetSSL
-nuget pack Pocoproject.Poco.static.nuspec -symbols -symbolpackageformat snupkg	-p version=%2 -p vs=vs%1 -p id=NetSSLWin
-nuget pack Pocoproject.Poco.static.nuspec -symbols -symbolpackageformat snupkg	-p version=%2 -p vs=vs%1 -p id=PDF
-nuget pack Pocoproject.Poco.static.nuspec 										-p version=%2 -p vs=vs%1 -p id=PageCompiler
-nuget pack Pocoproject.Poco.static.nuspec -symbols -symbolpackageformat snupkg	-p version=%2 -p vs=vs%1 -p id=Redis
-nuget pack Pocoproject.Poco.static.nuspec -symbols -symbolpackageformat snupkg	-p version=%2 -p vs=vs%1 -p id=Util
-nuget pack Pocoproject.Poco.static.nuspec -symbols -symbolpackageformat snupkg	-p version=%2 -p vs=vs%1 -p id=XML
-nuget pack Pocoproject.Poco.static.nuspec -symbols -symbolpackageformat snupkg	-p version=%2 -p vs=vs%1 -p id=Zip
+nuget pack Pocoproject.Poco.shared.nuspec -symbols -symbolpackageformat snupkg	-p vs=vs%1 -p version=%2 -p id=CppParser
+nuget pack Pocoproject.Poco.shared.nuspec -symbols -symbolpackageformat snupkg	-p vs=vs%1 -p version=%2 -p id=Crypto
+nuget pack Pocoproject.Poco.shared.nuspec -symbols -symbolpackageformat snupkg	-p vs=vs%1 -p version=%2 -p id=Data
+nuget pack Pocoproject.Poco.shared.nuspec -symbols -symbolpackageformat snupkg	-p vs=vs%1 -p version=%2 -p id=DataSQLite
+nuget pack Pocoproject.Poco.shared.nuspec -symbols -symbolpackageformat snupkg	-p vs=vs%1 -p version=%2 -p id=DataMySQL
+nuget pack Pocoproject.Poco.shared.nuspec -symbols -symbolpackageformat snupkg	-p vs=vs%1 -p version=%2 -p id=DataODBC
+nuget pack Pocoproject.Poco.shared.nuspec -symbols -symbolpackageformat snupkg	-p vs=vs%1 -p version=%2 -p id=DataPostgreSQL
+nuget pack Pocoproject.Poco.shared.nuspec -symbols -symbolpackageformat snupkg	-p vs=vs%1 -p version=%2 -p id=Encodings
+nuget pack Pocoproject.Poco.shared.nuspec -symbols -symbolpackageformat snupkg	-p vs=vs%1 -p version=%2 -p id=Foundation
+nuget pack Pocoproject.Poco.shared.nuspec -symbols -symbolpackageformat snupkg	-p vs=vs%1 -p version=%2 -p id=JSON
+nuget pack Pocoproject.Poco.shared.nuspec -symbols -symbolpackageformat snupkg	-p vs=vs%1 -p version=%2 -p id=JWT
+nuget pack Pocoproject.Poco.shared.nuspec -symbols -symbolpackageformat snupkg	-p vs=vs%1 -p version=%2 -p id=MongoDB
+nuget pack Pocoproject.Poco.shared.nuspec -symbols -symbolpackageformat snupkg	-p vs=vs%1 -p version=%2 -p id=Net
+nuget pack Pocoproject.Poco.shared.nuspec -symbols -symbolpackageformat snupkg	-p vs=vs%1 -p version=%2 -p id=NetSSL
+nuget pack Pocoproject.Poco.shared.nuspec -symbols -symbolpackageformat snupkg	-p vs=vs%1 -p version=%2 -p id=NetSSLWin
+nuget pack Pocoproject.Poco.shared.nuspec -symbols -symbolpackageformat snupkg	-p vs=vs%1 -p version=%2 -p id=PDF
+nuget pack Pocoproject.Poco.shared.nuspec 										-p vs=vs%1 -p version=%2 -p id=PageCompiler
+nuget pack Pocoproject.Poco.shared.nuspec -symbols -symbolpackageformat snupkg	-p vs=vs%1 -p version=%2 -p id=Redis
+nuget pack Pocoproject.Poco.shared.nuspec -symbols -symbolpackageformat snupkg	-p vs=vs%1 -p version=%2 -p id=Util
+nuget pack Pocoproject.Poco.shared.nuspec -symbols -symbolpackageformat snupkg	-p vs=vs%1 -p version=%2 -p id=XML
+nuget pack Pocoproject.Poco.shared.nuspec -symbols -symbolpackageformat snupkg	-p vs=vs%1 -p version=%2 -p id=Zip
+nuget pack Pocoproject.Poco.noarch.nuspec -p vs=vs%1 -p version=%2 -p id=CppParser 			-p inc=CppParser
+nuget pack Pocoproject.Poco.noarch.nuspec -p vs=vs%1 -p version=%2 -p id=Crypto 			-p inc=Crypto
+nuget pack Pocoproject.Poco.noarch.nuspec -p vs=vs%1 -p version=%2 -p id=Data 				-p inc=Data
+nuget pack Pocoproject.Poco.noarch.nuspec -p vs=vs%1 -p version=%2 -p id=DataSQLite 		-p inc="Data\SQLite"
+nuget pack Pocoproject.Poco.noarch.nuspec -p vs=vs%1 -p version=%2 -p id=DataMySQL 			-p inc="Data\MySQL"
+nuget pack Pocoproject.Poco.noarch.nuspec -p vs=vs%1 -p version=%2 -p id=DataODBC 			-p inc="Data\ODBC"
+nuget pack Pocoproject.Poco.noarch.nuspec -p vs=vs%1 -p version=%2 -p id=DataPostgreSQL		-p inc="Data\PostgreSQL"
+nuget pack Pocoproject.Poco.noarch.nuspec -p vs=vs%1 -p version=%2 -p id=Encodings 			-p inc=Encodings
+nuget pack Pocoproject.Poco.noarch.nuspec -p vs=vs%1 -p version=%2 -p id=Foundation 		-p inc=Foundation
+nuget pack Pocoproject.Poco.noarch.nuspec -p vs=vs%1 -p version=%2 -p id=JSON 				-p inc=JSON
+nuget pack Pocoproject.Poco.noarch.nuspec -p vs=vs%1 -p version=%2 -p id=JWT 				-p inc=JWT
+nuget pack Pocoproject.Poco.noarch.nuspec -p vs=vs%1 -p version=%2 -p id=MongoDB 			-p inc=MongoDB
+nuget pack Pocoproject.Poco.noarch.nuspec -p vs=vs%1 -p version=%2 -p id=Net 				-p inc=Net
+nuget pack Pocoproject.Poco.noarch.nuspec -p vs=vs%1 -p version=%2 -p id=NetSSL 			-p inc=NetSSL_OpenSSL
+nuget pack Pocoproject.Poco.noarch.nuspec -p vs=vs%1 -p version=%2 -p id=NetSSLWin 			-p inc=NetSSL_Win
+nuget pack Pocoproject.Poco.noarch.nuspec -p vs=vs%1 -p version=%2 -p id=PDF 				-p inc=PDF
+nuget pack Pocoproject.Poco.noarch.nuspec -p vs=vs%1 -p version=%2 -p id=Redis 				-p inc=Redis
+nuget pack Pocoproject.Poco.noarch.nuspec -p vs=vs%1 -p version=%2 -p id=Util 				-p inc=Util
+nuget pack Pocoproject.Poco.noarch.nuspec -p vs=vs%1 -p version=%2 -p id=XML 				-p inc=XML
+nuget pack Pocoproject.Poco.noarch.nuspec -p vs=vs%1 -p version=%2 -p id=Zip 				-p inc=Zip
+nuget pack Pocoproject.Poco.static.nuspec -symbols -symbolpackageformat snupkg	-p vs=vs%1 -p version=%2 -p id=CppParser
+nuget pack Pocoproject.Poco.static.nuspec -symbols -symbolpackageformat snupkg	-p vs=vs%1 -p version=%2 -p id=Crypto
+nuget pack Pocoproject.Poco.static.nuspec -symbols -symbolpackageformat snupkg	-p vs=vs%1 -p version=%2 -p id=Data
+nuget pack Pocoproject.Poco.static.nuspec -symbols -symbolpackageformat snupkg	-p vs=vs%1 -p version=%2 -p id=DataSQLite
+nuget pack Pocoproject.Poco.static.nuspec -symbols -symbolpackageformat snupkg	-p vs=vs%1 -p version=%2 -p id=DataMySQL
+nuget pack Pocoproject.Poco.static.nuspec -symbols -symbolpackageformat snupkg	-p vs=vs%1 -p version=%2 -p id=DataODBC
+nuget pack Pocoproject.Poco.static.nuspec -symbols -symbolpackageformat snupkg	-p vs=vs%1 -p version=%2 -p id=DataPostgreSQL
+nuget pack Pocoproject.Poco.static.nuspec -symbols -symbolpackageformat snupkg	-p vs=vs%1 -p version=%2 -p id=Encodings
+nuget pack Pocoproject.Poco.static.nuspec -symbols -symbolpackageformat snupkg	-p vs=vs%1 -p version=%2 -p id=Foundation
+nuget pack Pocoproject.Poco.static.nuspec -symbols -symbolpackageformat snupkg	-p vs=vs%1 -p version=%2 -p id=JSON
+nuget pack Pocoproject.Poco.static.nuspec -symbols -symbolpackageformat snupkg	-p vs=vs%1 -p version=%2 -p id=JWT
+nuget pack Pocoproject.Poco.static.nuspec -symbols -symbolpackageformat snupkg	-p vs=vs%1 -p version=%2 -p id=MongoDB
+nuget pack Pocoproject.Poco.static.nuspec -symbols -symbolpackageformat snupkg	-p vs=vs%1 -p version=%2 -p id=Net
+nuget pack Pocoproject.Poco.static.nuspec -symbols -symbolpackageformat snupkg	-p vs=vs%1 -p version=%2 -p id=NetSSL
+nuget pack Pocoproject.Poco.static.nuspec -symbols -symbolpackageformat snupkg	-p vs=vs%1 -p version=%2 -p id=NetSSLWin
+nuget pack Pocoproject.Poco.static.nuspec -symbols -symbolpackageformat snupkg	-p vs=vs%1 -p version=%2 -p id=PDF
+nuget pack Pocoproject.Poco.static.nuspec 										-p vs=vs%1 -p version=%2 -p id=PageCompiler
+nuget pack Pocoproject.Poco.static.nuspec -symbols -symbolpackageformat snupkg	-p vs=vs%1 -p version=%2 -p id=Redis
+nuget pack Pocoproject.Poco.static.nuspec -symbols -symbolpackageformat snupkg	-p vs=vs%1 -p version=%2 -p id=Util
+nuget pack Pocoproject.Poco.static.nuspec -symbols -symbolpackageformat snupkg	-p vs=vs%1 -p version=%2 -p id=XML
+nuget pack Pocoproject.Poco.static.nuspec -symbols -symbolpackageformat snupkg	-p vs=vs%1 -p version=%2 -p id=Zip
 goto exit
 :usage
 echo Usage:
