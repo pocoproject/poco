666e71a674a4a1a24be1c46bfa668e8dbef5ada8
Francis ANDRE <francis.andre.kampbell@orange.fr>
2020-12-26 09:11:08 +0100

    comment out nuget push

--- packaging/Windows/NuGet/bap.cmd
+++ packaging/Windows/NuGet/bap.cmd
@@ -13,7 +13,7 @@ buildwin %1 rebuild all both Win32 samples tests msbuild noenv
 buildwin %1 rebuild all both x64   samples tests msbuild noenv
 popd
 pack %1 %2
-nuget push *.nupkg -source nuget.int -symbolsource snuget.int -noninteractive -skipduplicate -timeout 3000
+rem nuget push *.nupkg -source nuget.org -symbolsource snuget.org -noninteractive -skipduplicate -timeout 3000
 goto exit
 :usage
 echo Usage:
