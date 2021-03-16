8a05b671e94afc411ef077fc1b92f47070c5a53a
Laurent Stacul <laurent.stacul@amadeus.com>
2021-03-16 16:21:39 +0000

    Remove SSL23 support from Poco/Crypto
    As per OpenSSL 3 (since alpha 13), the support of SSL23 has been removed
    (https://github.com/openssl/openssl/pull/14248).
    
    Reference: #3223

--- Crypto/include/Poco/Crypto/Crypto.h
+++ Crypto/include/Poco/Crypto/Crypto.h
@@ -49,10 +49,6 @@ enum RSAPaddingMode
 		/// EME-OAEP as defined in PKCS #1 v2.0 with SHA-1, MGF1 and an empty 
 		/// encoding parameter. This mode is recommended for all new applications.
 		
-	RSA_PADDING_SSLV23,
-		/// PKCS #1 v1.5 padding with an SSL-specific modification that denotes 
-		/// that the server is SSL3 capable. 
-		
 	RSA_PADDING_NONE
 		/// Raw RSA encryption. This mode should only be used to implement cryptographically 
 		/// sound padding modes in the application code. Encrypting user data directly with RSA 
