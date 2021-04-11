9f02ea1715bd7bb5287c4f920ea449921daac119
Günter Obiltschnig <guenter.obiltschnig@appinf.com>
2021-04-11 16:15:02 +0200

    #2783: fix Invalid condition [ICMPv4PacketImpl.cpp:234]

--- Net/src/ICMPv4PacketImpl.cpp
+++ Net/src/ICMPv4PacketImpl.cpp
@@ -43,7 +43,7 @@ const UInt8 ICMPv4PacketImpl::TIME_EXCEEDED_TYPE           = 11;
 const Poco::UInt8 ICMPv4PacketImpl::PARAMETER_PROBLEM_TYPE = 12;
 
 
-const std::string ICMPv4PacketImpl::MESSAGE_TYPE[] = 
+const std::string ICMPv4PacketImpl::MESSAGE_TYPE[] =
 {
 	"Echo Reply",
 	"ICMP 1",
@@ -66,7 +66,7 @@ const std::string ICMPv4PacketImpl::MESSAGE_TYPE[] =
 };
 
 
-const std::string ICMPv4PacketImpl::DESTINATION_UNREACHABLE_CODE[] = 
+const std::string ICMPv4PacketImpl::DESTINATION_UNREACHABLE_CODE[] =
 {
 	"Net unreachable",
 	"Host unreachable",
@@ -78,7 +78,7 @@ const std::string ICMPv4PacketImpl::DESTINATION_UNREACHABLE_CODE[] =
 };
 
 
-const std::string ICMPv4PacketImpl::REDIRECT_MESSAGE_CODE[] = 
+const std::string ICMPv4PacketImpl::REDIRECT_MESSAGE_CODE[] =
 {
 	"Redirect datagrams for the network",
 	"Redirect datagrams for the host",
@@ -88,7 +88,7 @@ const std::string ICMPv4PacketImpl::REDIRECT_MESSAGE_CODE[] =
 };
 
 
-const std::string ICMPv4PacketImpl::TIME_EXCEEDED_CODE[] = 
+const std::string ICMPv4PacketImpl::TIME_EXCEEDED_CODE[] =
 {
 	"Time to live exceeded in transit",
 	"Fragment reassembly time exceeded",
@@ -96,7 +96,7 @@ const std::string ICMPv4PacketImpl::TIME_EXCEEDED_CODE[] =
 };
 
 
-const std::string ICMPv4PacketImpl::PARAMETER_PROBLEM_CODE[] = 
+const std::string ICMPv4PacketImpl::PARAMETER_PROBLEM_CODE[] =
 {
 	"Pointer indicates the error",
 	"Unknown code"
@@ -220,31 +220,31 @@ std::string ICMPv4PacketImpl::errorDescription(unsigned char* buffer, int length
 		else
 			err << DESTINATION_UNREACHABLE_CODE[DESTINATION_UNREACHABLE_UNKNOWN];
 		break;
-	
+
 	case SOURCE_QUENCH_TYPE:
 		err << "Source quench";
 		break;
-	
+
 	case REDIRECT_MESSAGE_TYPE:
-		if (code >= REDIRECT_NETWORK && code < REDIRECT_MESSAGE_UNKNOWN) 
+		if (code >= REDIRECT_NETWORK && code < REDIRECT_MESSAGE_UNKNOWN)
 			err << REDIRECT_MESSAGE_CODE[code];
 		else
 			err << REDIRECT_MESSAGE_CODE[REDIRECT_MESSAGE_UNKNOWN];
 		break;
 
 	case TIME_EXCEEDED_TYPE:
-		if (code >= TIME_TO_LIVE || code < TIME_EXCEEDED_UNKNOWN)
+		if (code >= TIME_TO_LIVE && code < TIME_EXCEEDED_UNKNOWN)
 			err << TIME_EXCEEDED_CODE[code];
 		else
 			err << TIME_EXCEEDED_CODE[TIME_EXCEEDED_UNKNOWN];
 		break;
-	
+
 	case PARAMETER_PROBLEM_TYPE:
 		if (POINTER_INDICATES_THE_ERROR != code)
 			code = PARAMETER_PROBLEM_UNKNOWN;
 		err << PARAMETER_PROBLEM_CODE[code] << ": error in octet #" << pointer;
 		break;
-	
+
 	default:
 		err << "Unknown type.";
 		break;
