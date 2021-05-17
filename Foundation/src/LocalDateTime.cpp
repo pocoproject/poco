33811e4fcfbd80583a35e9117a31687838039551
Dan Okken <41077466+okkenator@users.noreply.github.com>
2021-05-17 17:56:03 -0400

    Fix localtime_r for VxWorks 6.9 and later

--- Foundation/src/LocalDateTime.cpp
+++ Foundation/src/LocalDateTime.cpp
@@ -270,14 +270,9 @@ void LocalDateTime::determineTzd(bool adjust)
 		_tzd = (Timezone::utcOffset() + ((broken->tm_isdst == 1) ? 3600 : 0));
 #else
 		std::tm broken;
-#if defined(POCO_VXWORKS)
-#if defined(_WRS_VXWORKS_MAJOR) && ((_WRS_VXWORKS_MAJOR > 6) || ((_WRS_VXWORKS_MAJOR == 6)  && (_WRS_VXWORKS_MINOR >= 9))) && !defined(_VXWORKS_COMPATIBILITY_MODE)
-		if (!localtime_r(&epochTime, &broken))
-			throw Poco::SystemException("cannot get local time");
-#else
+#if defined(POCO_VXWORKS) && (defined(_VXWORKS_COMPATIBILITY_MODE) || (defined(_WRS_VXWORKS_MAJOR) && ((_WRS_VXWORKS_MAJOR < 6) || ((_WRS_VXWORKS_MAJOR == 6)  && (_WRS_VXWORKS_MINOR < 9))))
 		if (localtime_r(&epochTime, &broken) != OK)
 			throw Poco::SystemException("cannot get local time");
-#endif
 #else
 		if (!localtime_r(&epochTime, &broken))
 			throw Poco::SystemException("cannot get local time");
