37f7315bc43aa5ff56de8c25736a1e75faccf4d5
Günter Obiltschnig <guenter.obiltschnig@appinf.com>
2021-06-15 13:46:19 +0200

    #3191: Fixing a bug in the NetSSL_Win module (Host name verification failed error)

--- NetSSL_Win/src/SecureSocketImpl.cpp
+++ NetSSL_Win/src/SecureSocketImpl.cpp
@@ -726,7 +726,7 @@ void SecureSocketImpl::connectSSL(bool completeHandshake)
 
 	if (_peerHostName.empty())
 	{
-		_peerHostName = _pSocket->address().host().toString();
+		_peerHostName = _pSocket->peerAddress().host().toString();
 	}
 
 	initClientContext();
@@ -1548,7 +1548,7 @@ void SecureSocketImpl::stateIllegal()
 
 void SecureSocketImpl::stateConnected()
 {
-	_peerHostName = _pSocket->address().host().toString();
+	_peerHostName = _pSocket->peerAddress().host().toString();
 	initClientContext();
 	performInitialClientHandshake();
 }
