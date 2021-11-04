71d292b3f62846202035aae0cd86f65c408caaf3
benjamin binder <benjamin.binder@artech.at>
2021-11-04 15:49:46 +0100

    Added Facility to Message in RemoteSyslogListener

--- Net/include/Poco/Net/RemoteSyslogListener.h
+++ Net/include/Poco/Net/RemoteSyslogListener.h
@@ -100,6 +100,7 @@ public:
 	static const std::string PROP_THREADS;
 	static const std::string PROP_BUFFER;
 
+	static const std::string LOG_PROP_FACILITY;
 	static const std::string LOG_PROP_APP;
 	static const std::string LOG_PROP_HOST;
 	static const std::string LOG_PROP_STRUCTURED_DATA;
