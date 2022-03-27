852a53e702c8343d4527d0d1315be2feaf07330a
Günter Obiltschnig <guenter.obiltschnig@appinf.com>
2022-03-27 19:37:14 +0200

    #3466: DefinePlatformSpecific.cmake: handle RelWithDebInfo and MinSizeRel configurations

--- cmake/DefinePlatformSpecifc.cmake
+++ cmake/DefinePlatformSpecifc.cmake
@@ -28,9 +28,13 @@ if(MSVC)
             CMAKE_CXX_FLAGS
             CMAKE_CXX_FLAGS_DEBUG
             CMAKE_CXX_FLAGS_RELEASE
+            CMAKE_CXX_FLAGS_RELWITHDEBINFO
+            CMAKE_CXX_FLAGS_MINSIZEREL
             CMAKE_C_FLAGS
             CMAKE_C_FLAGS_DEBUG
             CMAKE_C_FLAGS_RELEASE
+            CMAKE_C_FLAGS_RELWITHDEBINFO
+            CMAKE_C_FLAGS_MINSIZEREL
         )
         foreach(CompilerFlag ${CompilerFlags})
             string(REPLACE "/MD" "/MT" ${CompilerFlag} "${${CompilerFlag}}")
