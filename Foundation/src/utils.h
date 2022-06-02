7035844108ec9ddb97896eaa4d5f9339d82582f0
Alex Fabijanic <alex@pocoproject.org>
2022-06-02 08:11:53 -0500

    fix(double-conversion): riscv defines

--- Foundation/src/utils.h
+++ Foundation/src/utils.h
@@ -140,7 +140,7 @@ int main(int argc, char** argv) {
     defined(__SH4__) || defined(__alpha__) || \
     defined(_MIPS_ARCH_MIPS32R2) || defined(__ARMEB__) ||\
     defined(__AARCH64EL__) || defined(__aarch64__) || defined(__AARCH64EB__) || \
-    defined(__riscv) || defined(__e2k__) || \
+    (defined(__riscv) && defined(__riscv_float_abi_double)) || defined(__e2k__) || \
     defined(__or1k__) || defined(__arc__) || defined(__ARC64__) || \
     defined(__microblaze__) || defined(__XTENSA__) || \
     defined(__EMSCRIPTEN__) || defined(__wasm32__)
