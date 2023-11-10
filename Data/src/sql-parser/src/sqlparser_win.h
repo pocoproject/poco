d55453abda723568df59f5199a228007c3f32bb8
Aleksandar Fabijanic <aleks-f@users.noreply.github.com>
2023-11-10 01:36:55 +0100

    fix(SQLParser): static linking error on windows

--- Data/src/sql-parser/src/sqlparser_win.h
+++ Data/src/sql-parser/src/sqlparser_win.h
@@ -2,17 +2,20 @@
 #define SQLPARSER_SQLPARSER_WIN_H
 
 
-#if defined(_WIN32) || defined(_WIN64)
-	#if defined(_MSC_VER)
-		#define strncasecmp _strnicmp
-		#define strcasecmp _stricmp
+#if defined(_WIN32)
+	#define strncasecmp _strnicmp
+	#define strcasecmp _stricmp
+	#if defined(_USRDLL)
+		#if defined(SQLParser_EXPORTS)
+			#define SQLParser_API __declspec(dllexport)
+		#else
+			#define SQLParser_API __declspec(dllimport)
+		#endif
 	#endif
-	#if defined(SQLParser_EXPORTS)
-		#define SQLParser_API __declspec(dllexport)
-	#else
-		#define SQLParser_API __declspec(dllimport)
-	#endif
-#else
+#endif
+
+
+#if !defined(SQLParser_API)
 	#if defined (__GNUC__) && (__GNUC__ >= 4)
 		#define SQLParser_API __attribute__ ((visibility ("default")))
 	#else
