f745487b8526dc9b98fd4e958b0a073247594541
Friedrich Wilckens <frwilckens@gmail.com>
2023-12-02 20:52:28 -0800

    Data::Session: when parser is not used and autocommit mode is off, assume any SQL statement begins a transaction.

--- Data/src/Statement.cpp
+++ Data/src/Statement.cpp
@@ -235,11 +235,16 @@ void Statement::formatQuery()
 void Statement::checkBeginTransaction()
 {
 	SessionImpl& session = _pImpl->session();
-	if (session.shouldParse() && !session.isTransaction() && !session.isAutocommit())
-	{
-		auto result = parse();
-		if (result.isSpecified() && result.value() && !isSelect().value())
+	if (!session.isTransaction() && !session.isAutocommit()) {
+		if (session.shouldParse())
+		{
+			auto result = parse();
+			if (result.isSpecified() && result.value() && !isSelect().value())
+				session.begin();
+		} else
+		{
 			session.begin();
+		}
 	}
 }
 
