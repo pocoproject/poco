b7151779d24efcb1e35ac59f5032982a47f4f432
Kari Argillander <kari.argillander@fidelix.com>
2023-12-13 09:21:51 +0200

    Use vs160 & vs170 in rebuild all scripts
    I'm not actually sure how much these scripts are even used but let's
    just update these as well to support 160 and 170.

--- ProGen/rebuildall32.bat
+++ ProGen/rebuildall32.bat
@@ -9,11 +9,8 @@ mkdir ProGen\log
 
 call ProGen\progen.bat Win32 >ProGen\log\progen32.log 2>&1
 rd /q /s lib bin     & call buildwin  90 rebuild all both Win32 samples tests devenv  > ProGen\log\90.x32.log 2>&1
-rd /q /s lib bin     & call buildwin 100 rebuild all both Win32 samples tests msbuild > ProGen\log\100.x32.log 2>&1
-rd /q /s lib bin     & call buildwin 110 rebuild all both Win32 samples tests msbuild > ProGen\log\110.x32.log 2>&1
-rd /q /s lib bin     & call buildwin 120 rebuild all both Win32 samples tests msbuild > ProGen\log\120.x32.log 2>&1
-rd /q /s lib bin     & call buildwin 140 rebuild all both Win32 samples tests msbuild > ProGen\log\140.x32.log 2>&1
-rd /q /s lib bin     & call buildwin 150 rebuild all both Win32 samples tests msbuild > ProGen\log\150.x32.log 2>&1
+rd /q /s lib bin     & call buildwin 160 rebuild all both Win32 samples tests msbuild > ProGen\log\140.x32.log 2>&1
+rd /q /s lib bin     & call buildwin 170 rebuild all both Win32 samples tests msbuild > ProGen\log\150.x32.log 2>&1
 popd
 :exit
 endlocal
