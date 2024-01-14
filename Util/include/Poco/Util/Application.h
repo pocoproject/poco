cb431353e9c0ac49609daf2f780dda5b0018d6b7
Matej Kenda <matejken@gmail.com>
2024-01-14 22:03:37 +0100

    mingw compile and link improvements (#4019) (#4391)
    * fix(platform): MinGW Compile and link errors: undefined reference to `WinMain'
    
    * fix(platform): MinGW compile UUID tests (conflicting UUID defined as GUID in rpcdce.h via windows.h)

--- Util/include/Poco/Util/Application.h
+++ Util/include/Poco/Util/Application.h
@@ -504,7 +504,8 @@ inline Poco::Timespan Application::uptime() const
 //
 // Macro to implement main()
 //
-#if defined(_WIN32)
+
+#if defined(_WIN32) && !defined(POCO_COMPILER_MINGW)
 	#define POCO_APP_MAIN(App) \
 	int wmain(int argc, wchar_t** argv)		\
 	{										\
