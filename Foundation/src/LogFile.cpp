fd2134cc4ba715957e2eb56de5b057dea5b5f159
Matej Kenda <matejken@gmail.com>
2024-03-07 09:19:21 +0100

    enh(LogFile): make LogFile based on FileStream (posix). (#4431)

--- Foundation/src/LogFile.cpp
+++ Foundation/src/LogFile.cpp
@@ -13,25 +13,68 @@
 
 
 #include "Poco/LogFile.h"
+#include "Poco/File.h"
+#include "Poco/Exception.h"
 
-
-#if defined(POCO_OS_FAMILY_WINDOWS)
-#include "LogFile_WIN32U.cpp"
-#else
-#include "LogFile_STD.cpp"
-#endif
-
+#include <unistd.h>
 
 namespace Poco {
 
 
-LogFile::LogFile(const std::string& path): LogFileImpl(path)
+LogFile::LogFile(const std::string& path):
+	_path(path),
+	_str(_path, std::ios::app),
+	_size(static_cast<UInt64>(_str.tellp()))
 {
+	if (_size == 0)
+		_creationDate = File(path).getLastModified();
+	else
+		_creationDate = File(path).created();
 }
 
 
 LogFile::~LogFile()
 {
+}
+
+
+void LogFile::write(const std::string& text, bool flush)
+{
+	std::streampos pos = _str.tellp();
+
+	_str << text << '\n';
+
+	if (flush)
+		_str.flushToDisk();
+	else
+		_str.flush();
+
+	if (!_str.good())
+	{
+		_str.clear();
+		_str.seekp(pos);
+		throw WriteFileException(_path);
+	}
+
+	_size = static_cast<UInt64>(_str.tellp());
+}
+
+
+UInt64 LogFile::size() const
+{
+	return _size;
+}
+
+
+Timestamp LogFile::creationDate() const
+{
+	return _creationDate;
+}
+
+
+const std::string& LogFile::path() const
+{
+	return _path;
 }
 
 
