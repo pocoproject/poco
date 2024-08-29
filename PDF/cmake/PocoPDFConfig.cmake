d5b393ffa3728bb678be10b964d284184b5ed044
Dan Rosser <danoli3@gmail.com>
2024-08-29 19:17:42 +1000

    CMake findpackage PNGm added include and link correctly

--- PDF/cmake/PocoPDFConfig.cmake
+++ PDF/cmake/PocoPDFConfig.cmake
@@ -2,7 +2,7 @@ include(CMakeFindDependencyMacro)
 find_dependency(PocoFoundation)
 if(@POCO_UNBUNDLED@)
 	find_dependency(ZLIB REQUIRED)
-	find_dependency(LIBPNG REQUIRED)
+	find_dependency(PNG REQUIRED)
 endif()
 
 include("${CMAKE_CURRENT_LIST_DIR}/PocoPDFTargets.cmake")
