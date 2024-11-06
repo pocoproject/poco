93230d60a2551ef8d14b7e5edce2042b40edadc8
Matej Kenda <matejken@gmail.com>
2024-11-06 17:48:08 +0100

    sample(DBLogger): Save messages from SQL files to an SQL database (works with SQLite) (#4750)

--- Data/samples/DBLogger/src/DBLogger.cpp
+++ Data/samples/DBLogger/src/DBLogger.cpp
@@ -11,9 +11,11 @@
 
 
 #include "Poco/Data/SQLChannel.h"
+#include "Poco/Data/SQLite/Connector.h"
 #include "Poco/Delegate.h"
 #include "Poco/DirectoryIterator.h"
 #include "Poco/DirectoryWatcher.h"
+#include "Poco/FileStream.h"
 #include "Poco/Glob.h"
 #include "Poco/Thread.h"
 #include "Poco/Util/Application.h"
@@ -24,11 +26,14 @@
 
 #include <iostream>
 
+using namespace Poco::Data::Keywords;
 using Poco::Util::Application;
 using Poco::Util::Option;
 using Poco::Util::OptionSet;
 using Poco::Util::HelpFormatter;
 using Poco::Util::OptionCallback;
+
+using namespace std::string_literals;
 
 class DBLogger: public Application
 {
@@ -49,6 +54,21 @@ protected:
 		loadConfiguration(); // load default configuration files, if present
 		Application::initialize(self);
 
+		Poco::Data::SQLite::Connector::registerConnector();
+
+		logger().information("conector: %s, cs: %s", _connector, _connectionString);
+
+		_dataSession = std::make_shared<Poco::Data::Session>(_connector, _connectionString);
+
+		(*_dataSession) << ("DROP TABLE IF EXISTS "s + _tableName), now;
+		const auto create {
+			"CREATE TABLE "s + _tableName +
+			" (Source VARCHAR, Name VARCHAR, ProcessId INTEGER, Thread VARCHAR,"s +
+			" ThreadId INTEGER, Priority INTEGER, Text VARCHAR, DateTime DATE)"s
+		};
+
+		(*_dataSession) << create, now;
+
 		// Initial scan of the directory to pick existing files
 		scanDirectory();
 
@@ -63,7 +83,8 @@ protected:
 
 		// SQL channel to generate SQL files
 		_sqlChannel = new Poco::Data::SQLChannel();
-		_sqlChannel->setProperty("directory", _directory);
+		_sqlChannel->setProperty(Poco::Data::SQLChannel::PROP_DIRECTORY, _directory);
+		_sqlChannel->setProperty(Poco::Data::SQLChannel::PROP_TABLE, _tableName);
 
 		_active = true;
 		_startTime.update();
@@ -78,6 +99,7 @@ protected:
 	void uninitialize()
 	{
 		_sqlSourceThread.join();
+		_dirWatcher->suspendEvents();
 
 		logger().information(
 			"Created %z messages, processed %z messages in %Ld ms.",
@@ -112,8 +134,22 @@ protected:
 			Option("dir", "d", "directory path to scan for SQL log files")
 				.required(true)
 				.repeatable(false)
-				.argument("filebasename")
+				.argument("dir")
 				.callback(OptionCallback<DBLogger>(this, &DBLogger::handleLogDirectory)));
+
+		options.addOption(
+			Option("connector", "c", "database connector")
+				.required(true)
+				.repeatable(false)
+				.argument("connector")
+				.callback(OptionCallback<DBLogger>(this, &DBLogger::handleDbConnector)));
+
+		options.addOption(
+			Option("constring", "s", "database connection string")
+				.required(true)
+				.repeatable(false)
+				.argument("cstr")
+				.callback(OptionCallback<DBLogger>(this, &DBLogger::handleDbConnectionString)));
 
 	}
 
@@ -124,11 +160,25 @@ protected:
 		stopOptionsProcessing();
 	}
 
-	void handleLogDirectory(const std::string& directory, const std::string& value)
+	void handleLogDirectory(const std::string& name, const std::string& value)
 	{
 		if (value.empty())
 			throw Poco::Util::IncompatibleOptionsException("Expected directory name");
 		_directory = value;
+	}
+
+	void handleDbConnector(const std::string& name, const std::string& value)
+	{
+		if (value.empty())
+			throw Poco::Util::IncompatibleOptionsException("Expected database connector name");
+		_connector = value;
+	}
+
+	void handleDbConnectionString(const std::string& name, const std::string& value)
+	{
+		if (value.empty())
+			throw Poco::Util::IncompatibleOptionsException("Expected database connection string");
+		_connectionString = value;
 	}
 
 	void displayHelp()
@@ -173,6 +223,11 @@ protected:
 		// TODO: What to do if the file content is modified with correct content after it is added?
 		//       Ignore if empty and process on modified?
 
+		if (_dataSession == nullptr || !_dataSession->isGood())
+		{
+			return;
+		}
+
 		if (!file.isFile())
 		{
 			logger().trace("Not a file: %s", file.absolutePath());
@@ -191,8 +246,29 @@ protected:
 			logger().information("File does not exist: %s", file.absolutePath());
 			return;
 		}
-		f.remove();
-		++_processed;
+		try
+		{
+			Poco::FileInputStream is(f.absolutePath());
+			std::stringstream buffer;
+			buffer << is.rdbuf();
+			const auto& sql { buffer.str() };
+			if (!sql.empty())
+			{
+				auto& s = (*_dataSession);
+
+				s << sql, now;
+
+				s.isGood();
+
+				f.remove();
+				++_processed;
+			}
+			// TODO: What if sql is empty?
+		}
+		catch (const Poco::Exception& e)
+		{
+			logger().warning("Failed to process %s: %s", f.absolutePath(), e.displayText());
+		}
 	}
 
 	void scanDirectory()
@@ -237,6 +313,12 @@ protected:
 
 private:
 	bool _helpRequested;
+
+	std::string _directory;
+	std::string _connector;
+	std::string _connectionString;
+	const std::string _tableName{"T_POCO_LOG"};
+
 	bool _active {false};
 
 	std::size_t _created{0};
@@ -244,11 +326,11 @@ private:
 
 	Poco::Timestamp _startTime;
 
-	std::string _directory;
 	Poco::Glob _sqlNameGlob;
 	std::shared_ptr<Poco::DirectoryWatcher> _dirWatcher;
 	Poco::AutoPtr<Poco::Data::SQLChannel> _sqlChannel;
 	Poco::Thread _sqlSourceThread;
+	std::shared_ptr<Poco::Data::Session> _dataSession;
 };
 
 
