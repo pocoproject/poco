df0b15acfdcbd457d339c525fe9aaa7c2810ce73
Matej Kenda <matejken@gmail.com>
2024-12-09 21:04:43 +0100

    fix(cmake): Add missing install and find dependency for utf8proc in unbundled build.

--- Foundation/cmake/PocoFoundationConfig.cmake
+++ Foundation/cmake/PocoFoundationConfig.cmake
@@ -3,6 +3,7 @@ if(@POCO_UNBUNDLED@)
 	list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}")
 	find_dependency(ZLIB REQUIRED)
 	find_dependency(PCRE2 REQUIRED)
+	find_dependency(Utf8Proc REQUIRED)
 endif()
 
 include("${CMAKE_CURRENT_LIST_DIR}/PocoFoundationTargets.cmake")
