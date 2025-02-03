eeb31fa1d0346fdb43751c1527844d49db96c157
Matej Kenda <matejken@gmail.com>
2025-02-03 20:53:45 +0100

    fix(Foundation): detection of CXXABI is based on presence of the header file, simplify conditions.

--- Foundation/include/Poco/Types.h
+++ Foundation/include/Poco/Types.h
@@ -21,17 +21,12 @@
 #include "Poco/Foundation.h"
 #include <cstdint>
 #include <type_traits>
-#if defined(__clang__) || (defined (__GNUC__) && (__GNUC__ >= 3))
-#	if (__cplusplus >= 201703L)
-#		if __has_include(<cxxabi.h>)
-#			include <typeinfo>
-#			include <cxxabi.h>
-#			include <cstdlib>
-#			define POCO_HAVE_CXXABI_H
-#		endif // __has_include(<cxxabi.h>)
-#	endif // __cplusplus >= 201703L
-#endif // defined(__clang__) || (defined (__GNUC__) && (__GNUC__ >= 3))
 
+#if defined(POCO_HAVE_CXXABI_H)
+	#include <typeinfo>
+	#include <cxxabi.h>
+	#include <cstdlib>
+#endif
 
 namespace Poco {
 
