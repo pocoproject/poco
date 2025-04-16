4b8f0a36a6df1be9d4bb5c1145dd0e66eade22e0
Ben Wolsieffer <benwolsieffer@gmail.com>
2025-04-16 19:27:45 -0400

    CMake: make utf8proc, PCRE2, zlib and expat private dependencies
    In the unbundled build, utf8proc, PCRE2, zlib and expat were all
    included as part of the CMake link interface, and therefore the CMake
    config file required find_dependency() calls for each.
    
    These libraries are internal implementation details of Poco and are not
    exposed in any public API. Therefore, we can make them private
    dependencies and avoid the find_dependency() calls and their associated
    find scripts.

--- Foundation/cmake/PocoFoundationConfig.cmake
+++ Foundation/cmake/PocoFoundationConfig.cmake
@@ -1,9 +1,1 @@
-if(@POCO_UNBUNDLED@)
-	include(CMakeFindDependencyMacro)
-	list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}")
-	find_dependency(ZLIB REQUIRED)
-	find_dependency(PCRE2 REQUIRED)
-	find_dependency(Utf8Proc REQUIRED)
-endif()
-
 include("${CMAKE_CURRENT_LIST_DIR}/PocoFoundationTargets.cmake")
