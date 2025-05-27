c6fdde12e675f697e01b61f45af7d34b9c54b3a9
Matej Kenda <matejken@gmail.com>
2025-05-27 09:23:40 +0200

    feature(AsyncNotificationCenter): introduce worker pool that dispatches notifications to observers asynchronously.

--- Foundation/include/Poco/AsyncNotificationCenter.h
+++ Foundation/include/Poco/AsyncNotificationCenter.h
@@ -18,17 +18,23 @@
 #ifndef Foundation_AsyncNotificationCenter_INCLUDED
 #define Foundation_AsyncNotificationCenter_INCLUDED
 
-
 #include "Poco/Foundation.h"
 #include "Poco/NotificationCenter.h"
 #include "Poco/Thread.h"
-#include "Poco/Stopwatch.h"
-#include "Poco/Debugger.h"
-#include "Poco/ErrorHandler.h"
-#include "Poco/Format.h"
 #include "Poco/RunnableAdapter.h"
 #include "Poco/NotificationQueue.h"
+#include <condition_variable>
 
+#if (POCO_HAVE_CPP20_COMPILER)
+	#if !defined(__cpp_lib_jthread)
+		#error ("std::jthread is expected but is not available. Please check your compiler version and settings.")
+	#endif
+#endif
+
+#include <thread>
+#include <vector>
+#include <list>
+#include <map>
 
 namespace Poco {
 
@@ -39,7 +45,17 @@ class Foundation_API AsyncNotificationCenter: public NotificationCenter
 	/// handler in a dedicated thread.
 {
 public:
-	AsyncNotificationCenter();
+
+#if (POCO_HAVE_CPP20_COMPILER)
+	enum class AsyncMode { ENQUEUE,	NOTIFY, BOTH };
+		/// ENQUEUE: Notifications are enqueued in a separate thread.
+		/// NOTIFY: Notifications are dispatched to observers from worker threads
+		/// BOTH: Notifications are enqueued and dispatched to observers in worker threads.
+#else
+	enum class AsyncMode { ENQUEUE };
+#endif
+
+	AsyncNotificationCenter(AsyncMode mode = AsyncMode::ENQUEUE, std::size_t workersCount = AsyncNotificationCenter::DEFAULT_WORKERS_COUNT);
 		/// Creates the AsyncNotificationCenter and starts the notifying thread.
 
 	~AsyncNotificationCenter() override;
@@ -56,6 +72,10 @@ public:
 	int backlog() const override;
 	/// Returns the numbner of notifications in the notification queue.
 
+protected:
+
+	void notifyObservers(Notification::Ptr& pNotification) override;
+
 private:
 	void start();
 	void stop();
@@ -63,11 +83,55 @@ private:
 
 	using Adapter = RunnableAdapter<AsyncNotificationCenter>;
 
+	const AsyncMode _mode { AsyncMode::ENQUEUE };
+
+	// Async enqueue for notifications
 	Thread _thread;
 	NotificationQueue _nq;
 	Adapter _ra;
 	std::atomic<bool> _started;
 	std::atomic<bool> _done;
+
+#if (POCO_HAVE_CPP20_COMPILER)
+	// Async notification dispatching
+
+	using NotificationList = std::list<Notification::Ptr>;
+	using ObserversMap = std::map<AbstractObserverPtr, NotificationList>;
+	using NotificationTuple = std::tuple<AbstractObserverPtr, Notification::Ptr>;
+
+	std::optional<NotificationTuple> nextNotification();
+
+	void dispatchNotifications(std::stop_token& stopToken);
+		/// Dispatching function executed by each worker thread.
+
+	constexpr static std::size_t DEFAULT_WORKERS_COUNT { 5 };
+		/// Default number of worker threads to process notifications.
+		/// This can be configured to a different value if needed.
+
+	const std::size_t _workersCount { DEFAULT_WORKERS_COUNT };
+		/// Number of worker threads to process notifications.
+		/// This can be configured to a different value if needed.
+
+	std::vector<std::jthread> _workers;
+		/// Workers pop notifications from the lists and call the observers' notification handlers.
+		/// If an observer is not registered anymore (hasObserver), it is removed from the map.
+		/// Workers pick observers in a round robin fashion.
+
+	ObserversMap _lists;
+		/// Each observer has its own list of pending notifications.
+		/// Observers are identifed by their pointers.
+		/// When adding to the queue, observersToNotify is used to get the observers
+		/// that are registered for such notification.
+
+	ObserversMap::iterator _workerIterator;
+		/// Iterator to the current observer list being processed by the worker threads.
+		/// It is used to ensure that workers process observers in a round robin fashion.
+
+	std::mutex _listsMutex;
+	std::condition_variable	_listsCondition;
+		// Condition variable to notify workers when new notifications are added to lists.
+
+#endif
 };
 
 
--- Foundation/src/AsyncNotificationCenter.cpp
+++ Foundation/src/AsyncNotificationCenter.cpp
@@ -15,13 +15,21 @@
 
 #include "Poco/AsyncNotificationCenter.h"
 
+#include "Poco/AbstractObserver.h"
+#include "Poco/Stopwatch.h"
+#include "Poco/Debugger.h"
+#include "Poco/ErrorHandler.h"
+#include "Poco/Format.h"
 
 namespace Poco {
 
 
-AsyncNotificationCenter::AsyncNotificationCenter(): _ra(*this, &AsyncNotificationCenter::dequeue),
+AsyncNotificationCenter::AsyncNotificationCenter(AsyncMode mode, std::size_t workersCount) :
+	_mode(mode),
+	_ra(*this, &AsyncNotificationCenter::dequeue),
 	_started(false),
-	_done(false)
+	_done(false),
+	_workersCount(workersCount)
 {
 	start();
 }
@@ -35,13 +43,47 @@ AsyncNotificationCenter::~AsyncNotificationCenter()
 
 void AsyncNotificationCenter::postNotification(Notification::Ptr pNotification)
 {
-	_nq.enqueueNotification(pNotification);
+	if (_mode == AsyncMode::ENQUEUE || _mode == AsyncMode::BOTH)
+	{
+		_nq.enqueueNotification(pNotification);
+	}
+	else
+	{
+		// Notification enqueue is synchronous
+		notifyObservers(pNotification);
+	}
 }
 
 
 int AsyncNotificationCenter::backlog() const
 {
 	return _nq.size();
+}
+
+void AsyncNotificationCenter::notifyObservers(Notification::Ptr& pNotification)
+{
+	poco_check_ptr (pNotification);
+
+	if (_mode == AsyncMode::NOTIFY || _mode == AsyncMode::BOTH)
+	{
+		// Notification is asynchronous, add it to the lists
+		// for appropriate observers.
+		std::unique_lock<std::mutex> lock(_listsMutex);
+		auto observers = observersToNotify(pNotification);
+		if (observers.empty())
+			return;
+		for (auto& o : observers)
+		{
+			auto& list = _lists[o];
+			list.push_back(pNotification);
+		}
+		_listsCondition.notify_all();
+	}
+	else
+	{
+		// Notification is synchronous
+		NotificationCenter::notifyObservers(pNotification);
+	}
 }
 
 
@@ -62,6 +104,18 @@ void AsyncNotificationCenter::start()
 			throw Poco::TimeoutException(poco_src_loc);
 		Thread::sleep(100);
 	}
+
+	_workerIterator = _lists.begin();
+
+	auto dispatch = [this](std::stop_token stopToken) {
+		this->dispatchNotifications(stopToken);
+	};
+
+	for (std::size_t i {0}; i < _workersCount; ++i)
+	{
+		auto worker = std::jthread(dispatch);
+		_workers.push_back(std::move(worker));
+	}
 }
 
 
@@ -71,6 +125,11 @@ void AsyncNotificationCenter::stop()
 	_nq.wakeUpAll();
 	while (!_done) Thread::sleep(100);
 	_thread.join();
+
+	for (auto& t: _workers)
+	{
+		t.request_stop();
+	}
 }
 
 
@@ -100,6 +159,60 @@ void AsyncNotificationCenter::dequeue()
 	}
 	_done = true;
 	_started = false;
+}
+
+std::optional<AsyncNotificationCenter::NotificationTuple> AsyncNotificationCenter::nextNotification()
+{
+	std::unique_lock<std::mutex> lock(_listsMutex);
+
+	for (std::size_t i {0}; i < _lists.size(); ++i)
+	{
+		if (_lists.empty())
+			break;
+
+		if (_workerIterator == _lists.end())
+			_workerIterator = _lists.begin();
+
+		auto& o = _workerIterator->first;
+		auto& l = _workerIterator->second;
+		if (l.empty())
+		{
+			++_workerIterator;
+			continue;
+		};
+		if (!hasObserver(*o)) {
+			// Observer is not registered anymore, remove its list
+			_workerIterator = _lists.erase(_workerIterator);
+			continue;
+		}
+		auto n = l.front();
+		l.pop_front();
+		return { {o, n} };
+	}
+	return {};
+}
+
+void AsyncNotificationCenter::dispatchNotifications(std::stop_token& stopToken)
+{
+	while (!stopToken.stop_requested())
+	{
+		// get next observer and notification
+		auto no = nextNotification();
+		if (no.has_value())
+		{
+			auto [a, n] = *no;
+			a->notify(n);
+			continue;
+		}
+		// No notification available, wait for a while
+		std::unique_lock<std::mutex> lock(_listsMutex);
+		_listsCondition.wait_for(
+			lock, 1s,
+			[&stopToken] { return stopToken.stop_requested(); }
+		);
+		if (stopToken.stop_requested())
+			break;
+	}
 }
 
 
