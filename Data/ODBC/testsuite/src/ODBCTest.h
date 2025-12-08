32633b572f3a949fe5ac3d1ddf1e8326f6b0a32d
Alex Fabijanic <alex@pocoproject.org>
2025-12-07 21:02:03 -0600

    chore: revert leftover ODB IP address

--- Data/ODBC/testsuite/src/ODBCTest.h
+++ Data/ODBC/testsuite/src/ODBCTest.h
@@ -25,7 +25,7 @@
 #include "SQLExecutor.h"
 
 
-#define POCO_ODBC_TEST_DATABASE_SERVER "192.168.139.34"
+#define POCO_ODBC_TEST_DATABASE_SERVER "127.0.0.1"
 
 
 class ODBCTest: public CppUnit::TestCase
