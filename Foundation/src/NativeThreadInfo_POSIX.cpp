b4a3bfd3ccb4a79b313e462c20f8a38699c25a0d
Alex Fabijanic <alex@pocoproject.org>
2025-12-21 21:17:49 -0600

    fix(NativeThread): add OS includes #3333

--- Foundation/src/NativeThreadInfo_POSIX.cpp
+++ Foundation/src/NativeThreadInfo_POSIX.cpp
@@ -17,14 +17,38 @@
 #include <unistd.h>
 #include <sys/types.h>
 
-
-#if __GLIBC__ == 2 && __GLIBC_MINOR__ < 30
-#include <sys/syscall.h>
-#ifdef SYS_gettid
-#define gettid() syscall(SYS_gettid)
-#else
-#define gettid() (0)
+#if POCO_OS == POCO_OS_FREE_BSD
+#    include <sys/thr.h>
+#    include <pthread_np.h>
+#    include <osreldate.h>
 #endif
+
+
+#if defined(__sun) && defined(__SVR4)
+#	if !defined(__EXTENSIONS__)
+#		define __EXTENSIONS__
+#	endif
+#endif
+#if POCO_OS == POCO_OS_LINUX || POCO_OS == POCO_OS_ANDROID || POCO_OS == POCO_OS_MAC_OS_X || POCO_OS == POCO_OS_QNX
+#	include <time.h>
+#endif
+
+#if POCO_OS == POCO_OS_QNX
+#	include <sys/neutrino.h>
+#endif
+
+#ifndef POCO_NO_THREADNAME
+	#if POCO_OS == POCO_OS_LINUX || POCO_OS == POCO_OS_ANDROID
+	#	include <sys/prctl.h>
+	#endif
+#endif
+
+#if POCO_OS == POCO_OS_LINUX
+	#ifndef _GNU_SOURCE
+		#define _GNU_SOURCE         /* See feature_test_macros(7) */
+	#endif
+	#include <unistd.h>
+	#include <sys/syscall.h>   /* For SYS_xxx definitions */
 #endif
 
 
