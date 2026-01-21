b4a5659eb96fb3509d4db9d6317347999f1917ed
Günter Obiltschnig <guenter.obiltschnig@appinf.com>
2026-01-21 19:18:26 +0100

    chore: replace deprecated constants

--- RemotingNG/JSONRPC/src/JSONRPCRequestHandler.cpp
+++ RemotingNG/JSONRPC/src/JSONRPCRequestHandler.cpp
@@ -258,7 +258,7 @@ void JSONRPCRequestHandler::handleRequest(Poco::Net::HTTPServerRequest& request,
 	std::string ce = request.get("Content-Encoding"s, ""s);
 	if (!ce.empty() && ce != "gzip")
 	{
-		response.setStatusAndReason(Poco::Net::HTTPResponse::HTTP_UNSUPPORTEDMEDIATYPE);
+		response.setStatusAndReason(Poco::Net::HTTPResponse::HTTP_UNSUPPORTED_MEDIA_TYPE);
 		response.setContentLength(0);
 		response.send();
 		return;
