cd3cbf4d12c14f943cc88684df40a92081031f26
seektechnz <avery@seektech.nz>
2026-03-10 12:07:25 +1300

    Clean up POCO source build artifacts

--- output/x64/Poco/PocoCryptoConfig.cmake
+++
@@ -1,4 +0,0 @@
-include(CMakeFindDependencyMacro)
-find_dependency(PocoFoundation)
-find_dependency(OpenSSL REQUIRED COMPONENTS Crypto)
-include("${CMAKE_CURRENT_LIST_DIR}/PocoCryptoTargets.cmake")
--- output/x64/Poco/PocoJWTConfig.cmake
+++
@@ -1,5 +0,0 @@
-include(CMakeFindDependencyMacro)
-find_dependency(PocoFoundation)
-find_dependency(PocoJSON)
-find_dependency(PocoCrypto)
-include("${CMAKE_CURRENT_LIST_DIR}/PocoJWTTargets.cmake")
