32990e88e23651f43e7df7641183c08e5f2a5044
Matej Kenda <matejken@gmail.com>
2026-09-16 00:07:09 +0200

    fix(RemotingNG): address the CodeQL findings: dead null checks in RemoteGen, MTOM parts not cleared on reset #5447

--- RemotingNG/RemoteGen/src/RemoteObjectGenerator.cpp
+++ RemotingNG/RemoteGen/src/RemoteObjectGenerator.cpp
@@ -71,8 +71,7 @@ void RemoteObjectGenerator::structStart(const Poco::CppParser::Struct* pStruct, 
 	Poco::CppParser::Function* pDestr = new Poco::CppParser::Function(std::string("virtual ~")+_pStruct->name(), _pStruct);
 	pDestr->addDocumentation(	" Destroys the " + _pStruct->name() + ".");
 
-	Poco::CppParser::TypeAlias* pTypeAlias = new Poco::CppParser::TypeAlias("using Ptr = Poco::AutoPtr<" + generateClassName(pStruct) + ">", _pStruct);
-	poco_check_ptr (pTypeAlias); // just avoid unused variable warning
+	[[maybe_unused]] Poco::CppParser::TypeAlias* pTypeAlias = new Poco::CppParser::TypeAlias("using Ptr = Poco::AutoPtr<" + generateClassName(pStruct) + ">", _pStruct); // registers itself with _pStruct
 
 	// adds the member var
 	_cppGen.addIncludeFile("Poco/SharedPtr.h");
