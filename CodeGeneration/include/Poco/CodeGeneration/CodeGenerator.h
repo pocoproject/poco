b7771e1cf8b5b0c4d4998a74a8ba3a25ccb4300f
Günter Obiltschnig <guenter.obiltschnig@appinf.com>
2026-09-15 13:11:44 +0200

    feat(CodeGeneration): add CodeGeneration library from macchina.io #5447
    Flat import of platform/CodeGeneration from macchina.io (state of 2026-09-09); the Applied
    Informatics changes since the common base follow as separate commits.
    
    Co-Authored-By: Aleksandar Fabijanic <aleks-f@users.noreply.github.com>
    Co-Authored-By: Matej Kenda <matejken@gmail.com>

---
+++ CodeGeneration/include/Poco/CodeGeneration/CodeGenerator.h
@@ -0,0 +1,217 @@
+//
+// CodeGenerator.h
+//
+// Library: CodeGeneration
+// Package: CodeGeneration
+// Module:  CodeGenerator
+//
+// Definition of the CodeGenerator class.
+//
+// Copyright (c) 2006-2014, Applied Informatics Software Engineering GmbH.
+// All rights reserved.
+//
+// License: Applied Informatics Software and Source Code License Agreement
+//
+
+
+#ifndef CodeGeneration_CodeGenerator_INCLUDED
+#define CodeGeneration_CodeGenerator_INCLUDED
+
+
+#include "Poco/CodeGeneration/CodeGeneration.h"
+#include "Poco/CppParser/Struct.h"
+#include "Poco/CppParser/Variable.h"
+#include "Poco/CppParser/TypeDef.h"
+#include <set>
+#include <map>
+
+
+namespace Poco {
+namespace CodeGeneration {
+
+
+class CodeGeneration_API CodeGenerator
+	/// A CodeGenerator defines an interface for code generators.
+{
+public:
+	struct FwdDecl
+	{
+		std::string includeFile;
+		std::string className;
+		std::string fullNameSpace;
+		FwdDecl(const std::string& inc, const std::string& cN, const std::string& ns);
+		bool operator<(const FwdDecl& other) const;
+		/// Must guarantee that the primary order is by namespace, then by class
+	};
+
+	typedef std::set<std::string> IncludeSet;
+	typedef std::map<std::string, std::string> Properties;
+	/// Maps a property name to its value.
+	typedef std::set<FwdDecl> FwdDecls;
+
+	CodeGenerator(const std::string& nameSpace, const std::string& libraryName, bool usePocoIncludeStyle, const std::string& copyright);
+		/// Creates the CodeGenerator. The nameSpace declares in which nameSpace the generated class will be,
+		/// the library name contaisn the name of the project
+
+	virtual ~CodeGenerator();
+		/// Destroys the CodeGenerator.
+
+	virtual void addIncludeFile(const std::string& incFile);
+		/// Adds an include file to the CodeGenerator. A call to this method is only meaningful until structStart was not called.
+		/// Duplicate includes will be ignored. Contains the include path only, e.g. "include/MyClass.h" (without enclosing ")
+
+	virtual void addSrcIncludeFile(const std::string& incFile);
+		/// Include file which will be written to an implementation file.
+
+	virtual void addSystemIncludeFile(const std::string& incFile);
+		/// Adds a system include file
+
+	virtual void addFwdDecl(const std::string& inclFile, const std::string& className, const std::string namespacePrefix);
+		/// Adds a fwd decl.
+
+	virtual void writeIncludes();
+		/// Convenience Functions which calls for all include files writeInclude, also writes fwdDecls.
+
+	virtual void writeDefaultHeader(const Poco::CppParser::Struct* pStruct, const std::string& className, const std::string& libraryName, const std::string& package) = 0;
+		/// Writes the default header, e.g. copyright notice, no include namespace open or anything else is written
+		/// This is always the first method that is called!
+
+	virtual void endFile() = 0;
+		/// Closes the file.
+
+	virtual void writeInclude(const std::string& include, bool toHeader) = 0;
+		/// Writes the include either to the header or the src file.
+
+	virtual void writeSystemInclude(const std::string& include) = 0;
+		/// Writes the system include file to the header.
+
+	virtual void writeNameSpaceBegin(const std::string& fullNameSpace) = 0;
+		/// Writes the enclosing namespace
+
+	virtual void writeFwdDecls(const FwdDecls& decl) = 0;
+		/// Writes the fwd decl to the header file and the include file to the src file.
+
+	virtual void writeNameSpaceEnd(const std::string& fullNameSpace) = 0;
+		/// Closes the namespace
+
+	virtual void structStart(const Poco::CppParser::Struct* pStruct, const CodeGenerator::Properties& properties) = 0;
+		/// Writes the class declaration.
+
+	virtual void structEnd() = 0;
+		/// Closes the class declaration.
+
+	virtual void methodStart(const Poco::CppParser::Function* pFunc, const CodeGenerator::Properties& properties) = 0;
+		/// Writes the method header. Properties contains the properties found in the functions docuementation and in the class documentation.
+
+	virtual void writeMethodImplementation(const std::string& code) = 0;
+		/// Only valid when inside a method. Simply forwards the code lines to the implementation file.
+
+	virtual void writeImplementation(const std::string& code) = 0;
+		/// Writes other non-method code.
+
+	virtual void methodEnd(const Poco::CppParser::Function* pFunc, const CodeGenerator::Properties& properties) = 0;
+		/// Closes the method.
+
+	virtual void variablesStart() = 0;
+		/// Indicates that variables wil be written.
+
+	virtual void variable(const Poco::CppParser::Variable* pVar) = 0;
+		/// Writes a single variable.
+
+	virtual void writeTypeDef(const Poco::CppParser::TypeDef* pType) = 0;
+		/// Writes a single typedef
+
+	virtual void writeUsing(const Poco::CppParser::TypeAlias* pType) = 0;
+		/// Writes a single using
+
+	virtual void variablesEnd() = 0;
+		/// Ends variable definitions.
+
+	virtual const CodeGenerator::IncludeSet& includes() const;
+		/// Returns all includes that the CodeGenerator consists of.
+
+	virtual const CodeGenerator::IncludeSet& systemIncludes() const;
+		/// Returns all system includes that the CodeGenerator consists of.
+
+	virtual const std::string& nameSpace() const;
+		/// The namespace of the file
+
+	virtual const std::string& libraryName() const;
+		/// The prefix assigned to the namespace.
+
+	virtual bool usePocoIncludeStyle() const;
+		/// Returns which include style should be used
+
+	virtual const std::string& copyright() const;
+		/// Returns the copyright notice, can be empty.
+
+private:
+	std::string _nameSpace;
+	std::string _libraryName;
+	bool        _usePocoIncludeStyle;
+	std::string _copyright;
+	IncludeSet  _includes;
+	IncludeSet  _systemIncludes;
+	IncludeSet  _srcIncludes;
+	FwdDecls    _fwdDecls;
+};
+
+
+inline void CodeGenerator::addIncludeFile(const std::string& incFile)
+{
+	_includes.insert(incFile);
+}
+
+
+inline void CodeGenerator::addSrcIncludeFile(const std::string& incFile)
+{
+	_srcIncludes.insert(incFile);
+}
+
+
+inline void CodeGenerator::addSystemIncludeFile(const std::string& incFile)
+{
+	_systemIncludes.insert(incFile);
+}
+
+
+inline const CodeGenerator::IncludeSet& CodeGenerator::includes() const
+{
+	return _includes;
+}
+
+
+inline const CodeGenerator::IncludeSet& CodeGenerator::systemIncludes() const
+{
+	return _systemIncludes;
+}
+
+
+inline const std::string& CodeGenerator::nameSpace() const
+{
+	return _nameSpace;
+}
+
+
+inline const std::string& CodeGenerator::libraryName() const
+{
+	return _libraryName;
+}
+
+
+inline bool CodeGenerator::usePocoIncludeStyle() const
+{
+	return _usePocoIncludeStyle;
+}
+
+
+inline const std::string& CodeGenerator::copyright() const
+{
+	return _copyright;
+}
+
+
+} } // namespace Poco::CodeGeneration
+
+
+#endif // CodeGeneration_CodeGenerator_INCLUDED
---
+++ CodeGeneration/src/MethodPropertyFilter.cpp
@@ -0,0 +1,95 @@
+//
+// MethodPropertyFilter.cpp
+//
+// Library: CodeGeneration
+// Package: CodeGeneration
+// Module:  MethodPropertyFilter
+//
+// Copyright (c) 2006-2014, Applied Informatics Software Engineering GmbH.
+// All rights reserved.
+//
+// License: Applied Informatics Software and Source Code License Agreement
+//
+
+
+#include "Poco/CodeGeneration/MethodPropertyFilter.h"
+#include "Poco/CodeGeneration/Utility.h"
+#include "Poco/CppParser/Function.h"
+
+
+namespace Poco {
+namespace CodeGeneration {
+
+
+MethodPropertyFilter::MethodPropertyFilter(CodeGenerator* pGen, const CodeGenerator::Properties& properties):
+	CodeGenerator("","", true, ""),
+	_pGen(pGen),
+	_properties(properties),
+	_forwardMethod(false),
+	_inMethod(false)
+{
+	poco_assert (pGen);
+}
+
+
+MethodPropertyFilter::MethodPropertyFilter(CodeGenerator* pGen, const std::string& prop):
+	CodeGenerator("","", true, ""),
+	_pGen(pGen),
+	_properties(),
+	_forwardMethod(false),
+	_inMethod(false)
+{
+	poco_assert (pGen);
+	_properties.insert(std::make_pair(prop, Utility::VAL_TRUE));
+}
+
+
+MethodPropertyFilter::~MethodPropertyFilter()
+{
+	delete _pGen;
+}
+
+
+void MethodPropertyFilter::methodStart(const Poco::CppParser::Function* pFunc, const CodeGenerator::Properties& properties)
+{
+	_inMethod = true;
+	_forwardMethod = matches(properties) && !(pFunc->isConstructor() || pFunc->isDestructor() || pFunc->isDeleted()) && pFunc->getAccess() == Poco::CppParser::Symbol::ACC_PUBLIC;
+
+	if (_forwardMethod)
+		_pGen->methodStart(pFunc, properties);
+}
+
+
+void MethodPropertyFilter::writeMethodImplementation(const std::string& code)
+{
+	if (_forwardMethod || !_inMethod)
+		_pGen->writeMethodImplementation(code);
+}
+
+
+void MethodPropertyFilter::methodEnd(const Poco::CppParser::Function* pFunc, const CodeGenerator::Properties& properties)
+{
+	if (_forwardMethod)
+		_pGen->methodEnd(pFunc, properties);
+	_inMethod = false;
+	_forwardMethod = false;
+}
+
+
+bool MethodPropertyFilter::matches(const CodeGenerator::Properties& properties) const
+{
+	// look if we find all of the internalproperties in properties
+	CodeGenerator::Properties::const_iterator it = _properties.begin();
+	CodeGenerator::Properties::const_iterator itEnd = _properties.end();
+	bool ok = true;
+	for (; it != itEnd && ok; ++it)
+	{
+		CodeGenerator::Properties::const_iterator itF = properties.find(it->first);
+
+		ok &= (itF != properties.end() && (itF->second == it->second || (it->second == Utility::VAL_TRUE && itF->second.empty())));
+	}
+	return ok;
+}
+
+
+} } // namespace Poco::CodeGeneration
