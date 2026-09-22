ced646ce96f53e6b56f23e24eeda8496527a0652
Matej Kenda <matejken@gmail.com>
2026-09-22 12:53:40 +0200

    fix(NetSSL_OpenSSL): load the built-in DH parameters in the correct byte order with OpenSSL 3
    OSSL_PARAM integers are in native byte order; on little-endian hosts p and g were reversed. DH parameters are loaded only for server contexts.

--- NetSSL_OpenSSL/include/Poco/Net/Context.h
+++ NetSSL_OpenSSL/include/Poco/Net/Context.h
@@ -203,11 +203,18 @@ public:
 
 		std::string dhParamsFile;
 			/// Specifies a file containing Diffie-Hellman parameters.
-			/// If empty, the default parameters are used.
+			/// If empty, the default parameters are used. Only a server uses them.
 
 		KeyDHGroup dhGroup;
-			/// If set to true, will use 2048-bit MODP Group with 256-bit
-			/// prime order subgroup (RFC5114) instead of 1024-bit for DH.
+			/// Selects the built-in Diffie-Hellman parameters (RFC 5114) used if
+			/// dhParamsFile is empty: KEY_DH_GROUP_2048 (default, 2048-bit MODP group
+			/// with 256-bit prime order subgroup) or KEY_DH_GROUP_1024 (1024-bit MODP
+			/// group with 160-bit prime order subgroup). OpenSSL 3.0 and newer reject
+			/// parameters that are too small for the security level (KEY_DH_GROUP_1024 above
+			/// SECURITY_LEVEL_80_BITS, KEY_DH_GROUP_2048 above SECURITY_LEVEL_112_BITS);
+			/// a server Context constructor then throws an SSLContextException. Use
+			/// dhParamsFile for larger parameters. In FIPS mode dhGroup and
+			/// dhParamsFile are ignored.
 
 		std::string ecdhCurve;
 			/// OpenSSL 1.0.1 and earlier:
---
+++ NetSSL_OpenSSL/testsuite/src/ContextTest.h
@@ -0,0 +1,47 @@
+//
+// ContextTest.h
+//
+// Definition of the ContextTest class.
+//
+// Copyright (c) 2026, Applied Informatics Software Engineering GmbH.
+// and Contributors.
+//
+// SPDX-License-Identifier:	BSL-1.0
+//
+
+
+#ifndef ContextTest_INCLUDED
+#define ContextTest_INCLUDED
+
+
+#include "Poco/Net/Net.h"
+#include "Poco/Net/Context.h"
+#include "CppUnit/TestCase.h"
+
+
+class ContextTest: public CppUnit::TestCase
+{
+public:
+	ContextTest(const std::string& name);
+	~ContextTest();
+
+#if POCO_OPENSSL_VERSION_PREREQ(3, 0, 0)
+	void testBuiltInDHParameters();
+#endif
+	void testClientContextIgnoresDHParameters();
+
+	void setUp();
+	void tearDown();
+
+	static CppUnit::Test* suite();
+
+private:
+#if POCO_OPENSSL_VERSION_PREREQ(3, 0, 0)
+	std::string negotiatedDHGroup(Poco::Net::Context::KeyDHGroup dhGroup);
+		/// Returns the OpenSSL name of the DH group that a server Context with
+		/// the given built-in parameters sends in a TLS 1.2 DHE handshake.
+#endif
+};
+
+
+#endif // ContextTest_INCLUDED
